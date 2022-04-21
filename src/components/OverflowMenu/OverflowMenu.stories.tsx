4e4cda944c23b439b271f19cc03ea4e70b716f86
Larisa Markova <lovemirror@rambler.ru>
2022-04-21 12:02:39 +0300

    Add stories example

--- src/components/OverflowMenu/OverflowMenu.stories.tsx
+++ src/components/OverflowMenu/OverflowMenu.stories.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { withDesign } from 'storybook-addon-designs';
+import styled from 'styled-components';
 import { ReactComponent as MinusCircleOutline } from '@admiral-ds/icons/build/service/MinusCircleOutline.svg';
 
 import { OverflowMenu } from '#src/components/OverflowMenu';
 import { DropDownItem } from '#src/components/DropDownItem';
+
+const Separator = styled.div`
+  height: 20px;
+  width: 8px;
+`;
+
+const DisplayBlock = styled.div`
+  display: flex;
+`;
 
 export default {
   title: 'Admiral-2.1/OverflowMenu',
@@ -29,6 +39,9 @@ export default {
       control: { type: 'radio' },
     },
     disabled: {
+      control: { type: 'boolean' },
+    },
+    isVertical: {
       control: { type: 'boolean' },
     },
   },
@@ -141,6 +154,160 @@ const Template2: ComponentStory<typeof OverflowMenu> = (args) => {
   );
 };
 
+const Template3: ComponentStory<typeof OverflowMenu> = (args) => {
+  const items: Array<any> = [
+    {
+      id: '1',
+      display: 'Option one',
+    },
+    {
+      id: '2',
+      display: (
+        <div style={{ display: 'flex', width: '115px', justifyContent: 'space-between', alignItems: 'center' }}>
+          <MinusCircleOutline width={20} height={20} />
+          Option two
+        </div>
+      ),
+    },
+    {
+      id: '3',
+      display: 'Option three',
+      disabled: true,
+    },
+    {
+      id: '4',
+      display: 'Option four',
+    },
+  ];
+
+  const [selected, setSelected] = React.useState<string | null>(null);
+  return (
+    <>
+      <DisplayBlock>
+        <OverflowMenu
+          {...args}
+          selected={selected}
+          onChange={(id) => setSelected(id)}
+          onOpen={() => console.log('open menu')}
+          onClose={() => console.log('close menu')}
+          aria-label="Overflow Menu component L"
+        >
+          {items.map((item) => {
+            return (
+              <DropDownItem role="option" key={item.id} id={item.id} disabled={item.disabled}>
+                {item.display}
+              </DropDownItem>
+            );
+          })}
+        </OverflowMenu>
+        <Separator />
+        <OverflowMenu
+          {...args}
+          dimension="m"
+          selected={selected}
+          onChange={(id) => setSelected(id)}
+          onOpen={() => console.log('open menu')}
+          onClose={() => console.log('close menu')}
+          aria-label="Overflow Menu component M"
+        >
+          {items.map((item) => {
+            return (
+              <DropDownItem role="option" key={item.id} id={item.id} disabled={item.disabled}>
+                {item.display}
+              </DropDownItem>
+            );
+          })}
+        </OverflowMenu>
+        <Separator />
+        <OverflowMenu
+          {...args}
+          dimension="s"
+          selected={selected}
+          onChange={(id) => setSelected(id)}
+          onOpen={() => console.log('open menu')}
+          onClose={() => console.log('close menu')}
+          aria-label="Overflow Menu component S"
+        >
+          {items.map((item) => {
+            return (
+              <DropDownItem role="option" key={item.id} id={item.id} disabled={item.disabled}>
+                {item.display}
+              </DropDownItem>
+            );
+          })}
+        </OverflowMenu>
+      </DisplayBlock>
+      <Separator />
+      <DisplayBlock>
+        <OverflowMenu
+          {...args}
+          isVertical
+          selected={selected}
+          onChange={(id) => {
+            console.log(`onChange('${id}')`);
+            setSelected(id);
+          }}
+          onOpen={() => console.log('open menu')}
+          onClose={() => console.log('close menu')}
+          aria-label="Overflow Menu component vertical L"
+        >
+          {items.map((item) => {
+            return (
+              <DropDownItem role="option" key={item.id} id={item.id}>
+                {item.display}
+              </DropDownItem>
+            );
+          })}
+        </OverflowMenu>
+        <Separator />
+        <OverflowMenu
+          {...args}
+          dimension="m"
+          isVertical
+          selected={selected}
+          onChange={(id) => {
+            console.log(`onChange('${id}')`);
+            setSelected(id);
+          }}
+          onOpen={() => console.log('open menu')}
+          onClose={() => console.log('close menu')}
+          aria-label="Overflow Menu component vertical M"
+        >
+          {items.map((item) => {
+            return (
+              <DropDownItem role="option" key={item.id} id={item.id}>
+                {item.display}
+              </DropDownItem>
+            );
+          })}
+        </OverflowMenu>
+        <Separator />
+        <OverflowMenu
+          {...args}
+          dimension="s"
+          isVertical
+          selected={selected}
+          onChange={(id) => {
+            console.log(`onChange('${id}')`);
+            setSelected(id);
+          }}
+          onOpen={() => console.log('open menu')}
+          onClose={() => console.log('close menu')}
+          aria-label="Overflow Menu component vertical S"
+        >
+          {items.map((item) => {
+            return (
+              <DropDownItem role="option" key={item.id} id={item.id}>
+                {item.display}
+              </DropDownItem>
+            );
+          })}
+        </OverflowMenu>
+      </DisplayBlock>
+    </>
+  );
+};
+
 export const OverflowMenuBase = Template1.bind({});
 OverflowMenuBase.args = {};
 OverflowMenuBase.storyName = 'OverflowMenu. Базовый пример.';
@@ -156,6 +323,17 @@ export const OverflowMenuOptions = Template2.bind({});
 OverflowMenuOptions.args = {};
 OverflowMenuOptions.storyName = 'OverflowMenu. Задизейбленные и кастомизированные опции.';
 OverflowMenuOptions.parameters = {
+  docs: {
+    source: {
+      type: 'code',
+    },
+  },
+};
+
+export const OverflowMenuSizesOrientation = Template3.bind({});
+OverflowMenuSizesOrientation.args = {};
+OverflowMenuSizesOrientation.storyName = 'OverflowMenu. Размеры и ориентация.';
+OverflowMenuSizesOrientation.parameters = {
   docs: {
     source: {
       type: 'code',
