62a7e7b6056b92c0215d2d7b43a8cfd59833e96d
Larisa Markova <lovemirror@rambler.ru>
2022-04-22 13:46:12 +0300

    feature(Tags): add dimension

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -15,18 +15,21 @@ const TagsWrapper = styled.div`
   }
 `;
 
+type Dimension = 'm' | 's';
 type Kind = 'neutral' | 'green' | 'blue' | 'red' | 'orange';
 
 export interface TagsProps extends HTMLAttributes<HTMLDivElement> {
   /** Ширина тэгов */
   width?: number | string;
+  /** Размер тегов (высота) */
+  dimension?: Dimension;
   /** Тип тэгов */
   kind?: Kind;
   /** Обработчик клика по тэгам */
   onClick?: (event: MouseEvent<HTMLElement> | KeyboardEvent<HTMLElement>) => void;
 }
 
-export const Tags: FC<TagsProps> = ({ children, width, kind, onClick }) => {
+export const Tags: FC<TagsProps> = ({ children, width, dimension, kind, onClick }) => {
   return (
     <TagsWrapper>
       {Children.map(children, (child: ReactNode) => {
@@ -36,6 +39,7 @@ export const Tags: FC<TagsProps> = ({ children, width, kind, onClick }) => {
         return cloneElement(child, {
           onClick: child.props.onClick || onClick,
           width: child.props.width || width,
+          dimension: child.props.dimension || dimension,
           kind: child.props.kind || kind,
           ...child.props,
         });
