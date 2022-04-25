d82790691697a6d13f998602b91be332ccca4a37
Larisa Markova <lovemirror@rambler.ru>
2022-04-25 12:47:28 +0300

    Remove redundant svg icons

--- src/components/OverflowMenu/Button.tsx
+++ src/components/OverflowMenu/Button.tsx
@@ -2,9 +2,6 @@ import type { ButtonHTMLAttributes, ReactNode } from 'react';
 import * as React from 'react';
 import styled, { css } from 'styled-components';
 
-import { ReactComponent as ButtonL } from '#src/components/OverflowMenu/svgs/ButtonL.svg';
-import { ReactComponent as ButtonM } from '#src/components/OverflowMenu/svgs/ButtonM.svg';
-import { ReactComponent as ButtonS } from '#src/components/OverflowMenu/svgs/ButtonS.svg';
 import { ReactComponent as MoreHorizontalOutline } from '@admiral-ds/icons/build/system/MoreHorizontalOutline.svg';
 
 export type Dimension = 'l' | 'm' | 's';
