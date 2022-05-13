e879975240434721be6f3bfaff5fe8c7e4dd28f2
Larisa Markova <lovemirror@rambler.ru>
2022-05-13 14:56:58 +0300

    Add spinner size and inverse

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -92,6 +92,18 @@ const ButtonContent = styled.span<{ dimension?: Dimension; loading?: boolean }>`
   }
 `;
 
+const spinnerDimension = (dimension: Dimension) => {
+  return dimension === 's' ? 's' : 'm';
+};
+
+const spinnerInverse = (appearance: Appearance) => {
+  /* return appearance === ('primary' || 'danger' || 'success' || 'white'); */
+  if (appearance === 'primary' || appearance === 'danger' || appearance === 'success' || appearance === 'white') {
+    return true;
+  }
+  return false;
+};
+
 const StyledSpinner = styled(Spinner)<{ dimension?: Dimension }>`
   position: absolute;
   left: 50%;
@@ -103,7 +115,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ appearance = 'primary', dimension = 'xl', type = 'button', loading = false, children, ...props }, ref) => {
     return (
       <StyledButton ref={ref} appearance={appearance} dimension={dimension} type={type} loading={loading} {...props}>
-        {loading && <StyledSpinner dimension={dimension === 's' ? 's' : 'm'} />}
+        {loading && <StyledSpinner dimension={spinnerDimension(dimension)} inverse={spinnerInverse(appearance)} />}
         <ButtonContent loading={loading}>
           {React.Children.toArray(children).map((child) =>
             typeof child === 'string' ? <span key={uid()}>{child}</span> : child,
