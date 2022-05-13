ee15ad7d375cd827d3a8f424fc4276c4a21b6761
Larisa Markova <lovemirror@rambler.ru>
2022-05-14 01:01:20 +0300

    Fix passing prop to styled button

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,2 +1,9 @@
 export type Appearance = 'primary' | 'secondary' | 'ghost' | 'white' | 'danger' | 'success';
 export type Dimension = 'xl' | 'l' | 'm' | 's';
+
+export interface StyledButtonProps {
+  appearance?: Appearance;
+  dimension?: Dimension;
+  displayAsDisabled?: boolean;
+  $loading?: boolean;
+}
