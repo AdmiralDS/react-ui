5d99ee4bd28ac2a786c3a3e4532cf276b9e511a3
Larisa Markova <lovemirror@rambler.ru>
2022-06-03 14:53:17 +0300

    Move borderRadius functions to separate file

---
+++ src/components/common/utils/borderRadius.ts
@@ -0,0 +1,44 @@
+import { Shape } from '#src/components/themes/common';
+
+export function smallGroupBorderRadius(shape: Shape): string {
+  switch (shape.borderRadiusKind) {
+    case 'Border radius 0':
+      return 'none';
+    case 'Border radius 2':
+      return '2px';
+    case 'Border radius 4':
+      return '4px';
+    case 'Border radius 8':
+      return '4px';
+    default:
+      return '4px';
+  }
+}
+export function mediumGroupBorderRadius(shape: Shape): string {
+  switch (shape.borderRadiusKind) {
+    case 'Border radius 0':
+      return 'none';
+    case 'Border radius 2':
+      return '2px';
+    case 'Border radius 4':
+      return '4px';
+    case 'Border radius 8':
+      return '8px';
+    default:
+      return '4px';
+  }
+}
+export function largeGroupBorderRadius(shape: Shape): string {
+  switch (shape.borderRadiusKind) {
+    case 'Border radius 0':
+      return 'none';
+    case 'Border radius 2':
+      return '4px';
+    case 'Border radius 4':
+      return '8px';
+    case 'Border radius 8':
+      return '16px';
+    default:
+      return '8px';
+  }
+}
