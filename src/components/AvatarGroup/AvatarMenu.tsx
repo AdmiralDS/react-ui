98c7b4a9da79ae349a40e7bd2574c3fd3c1bc472
Larisa Markova <lovemirror@rambler.ru>
2022-06-24 13:50:48 +0300

    Remove redundant code

--- src/components/AvatarGroup/AvatarMenu.tsx
+++ src/components/AvatarGroup/AvatarMenu.tsx
@@ -1,27 +1,9 @@
 import * as React from 'react';
-import { refSetter } from '#src/components/common/utils/refSetter';
-import { Dropdown } from '#src/components/Dropdown';
 import styled from 'styled-components';
 import type { AvatarProps } from '#src/components/Avatar';
 import { Avatar, AvatarInternalProps } from '#src/components/Avatar';
 import { DropMenu } from '#src/components/DropMenu';
 import { ItemProps } from '#src/components/MenuItem';
-
-const StyledDropDown = styled(Dropdown)<{ alignDropdown?: string; dropMaxHeight: string | number }>`
-  padding: 8px 0;
-  overflow-x: hidden;
-  overflow-y: auto;
-  ${(p) => (p.alignDropdown ? `align-self: ${p.alignDropdown}` : '')};
-  max-height: ${(p) => p.dropMaxHeight};
-  min-width: 100%;
-`;
-
-const SelectWrapper = styled.div`
-  box-sizing: border-box;
-  position: relative;
-  display: flex;
-  cursor: pointer;
-`;
 
 const MenuAvatar = styled(Avatar)`
   cursor: pointer;
@@ -67,42 +49,6 @@ export const AvatarMenu = React.forwardRef<HTMLButtonElement, SelectProps>(
     },
     ref,
   ) => {
-    const [open, setOpen] = React.useState(false);
-    const refWrapper = React.useRef<HTMLInputElement>(null);
-
-    const handleKeyDownItem = (e: React.KeyboardEvent<HTMLElement>, onKeyDown: (e: any) => void) => {
-      e.preventDefault();
-      if (e.key === 'Enter' || e.key === ' ') {
-        onKeyDown?.(e);
-        onAvatarSelect?.(e);
-        setOpen(false);
-        (refWrapper.current as HTMLElement).focus();
-      } else if (e.key === 'Escape' || e.key === 'Tab') {
-        setOpen(false);
-        (refWrapper.current as HTMLElement).focus();
-      }
-    };
-
-    const handleClickItem = (e: React.MouseEvent<HTMLElement>, onClick: (e: any) => void) => {
-      refWrapper.current?.focus();
-      onClick?.(e);
-      onAvatarSelect?.(e);
-    };
-
-    const renderChildrenDropDown = () => {
-      return React.Children.map(children, (child: React.ReactNode) => {
-        if (!React.isValidElement(child)) {
-          return null;
-        }
-
-        return React.cloneElement(child, {
-          ...child.props,
-          onClick: (e: React.MouseEvent<HTMLElement>) => handleClickItem(e, child.props.onClick),
-          onKeyDown: (e: React.KeyboardEvent<HTMLElement>) => handleKeyDownItem(e, child.props.onKeyDown),
-        });
-      });
-    };
-
     const menuDimension = dimension === 'xs' ? 's' : dimension === 'xl' ? 'l' : dimension;
 
     return (
@@ -116,7 +62,7 @@ export const AvatarMenu = React.forwardRef<HTMLButtonElement, SelectProps>(
         renderContentProp={({ buttonRef, handleKeyDown, handleClick }) => {
           return (
             <MenuAvatarButton
-              ref={buttonRef}
+              ref={buttonRef as React.Ref<HTMLButtonElement>}
               userName={'+' + items.length}
               isMenuAvatar
               appearance={appearance}
