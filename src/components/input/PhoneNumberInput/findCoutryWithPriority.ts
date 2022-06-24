b8b2b0c909a6a40a7e2d17c9620a0a4d30c84293
Александр Сталоверов <AStaloverov@inno.tech>
2022-06-24 16:18:49 +0300

    fix(PhoneNumberInput) add missing country codes

--- src/components/input/PhoneNumberInput/findCoutryWithPriority.ts
+++ src/components/input/PhoneNumberInput/findCoutryWithPriority.ts
@@ -1,8 +1,8 @@
-import { CountryIso3Code } from '#src/components/input/PhoneNumberInput/constants';
+import type { CountryAlpha3Code } from '@admiral-ds/flags';
 
 export interface CountryPhoneCode {
   code: string;
-  iso3: CountryIso3Code;
+  iso3: CountryAlpha3Code;
 }
 
 export type findCountryFunction = (inputValue: string) => CountryPhoneCode | null;
