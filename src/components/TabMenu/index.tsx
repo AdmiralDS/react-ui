1d533442dfa18e9f173ee6a3e3e762049c893c6f
Larisa Markova <lovemirror@rambler.ru>
2022-07-04 15:22:15 +0300

    Fix hiding underline in first render

--- src/components/TabMenu/index.tsx
+++ src/components/TabMenu/index.tsx
@@ -65,10 +65,10 @@ export const TabMenu: React.FC<TabMenuProps> = ({
   const tablistRef = React.useRef<HTMLDivElement | null>(null);
   const underlineRef = React.useRef<HTMLDivElement | null>(null);
   const [visibilityMap, setVisibilityMap] = React.useState<{ [index: number | string]: boolean }>({ 0: true });
-  const activeTabVisible = React.useMemo(() => {
+  const activeTabVisible = () => {
     const activeTabIndex = tabsWithRef.findIndex((item) => item.id === activeTab);
     return visibilityMap[activeTabIndex];
-  }, [activeTab, tabsWithRef, visibilityMap]);
+  };
 
   const modelAllTabs = React.useMemo(() => {
     return tabsWithRef.map((item) => ({
