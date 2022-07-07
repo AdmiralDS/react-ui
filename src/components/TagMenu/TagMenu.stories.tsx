ab24539da806cfaf0864348c165cad34a8ee6ce5
Larisa Markova <lovemirror@rambler.ru>
2022-07-07 18:08:47 +0300

    Add stoies

---
+++ src/components/TagMenu/TagMenu.stories.tsx
@@ -0,0 +1,140 @@
+import { withDesign } from 'storybook-addon-designs';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import { TagMenu } from '#src/components/TagMenu/index';
+import styled, { ThemeProvider } from 'styled-components';
+import { Tag, TagProps } from '#src/components/Tag';
+import { Theme } from '#src/components/themes';
+import { MenuItem, RenderOptionProps } from '#src/components/MenuItem';
+
+const Desc = styled.div`
+  font-family: 'VTB Group UI';
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+const Description = () => (
+  <Desc>
+    Тег — это метка, размечающая и каталогизирующая информацию для облегчения процесса поиска. При нажатии на тэг
+    загружаются все элементы имеющие эту метку.
+  </Desc>
+);
+
+export default {
+  title: 'Admiral-2.1/TagMenu',
+  decorators: [withDesign],
+  component: TagMenu,
+  parameters: {
+    componentSubtitle: <Description />,
+    layout: 'centered',
+    design: [
+      {
+        type: 'figma',
+        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=37%3A18375',
+      },
+      {
+        type: 'figma',
+        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=37%3A18530',
+      },
+    ],
+  },
+  argTypes: {
+    kind: {
+      options: ['neutral', 'blue', 'green', 'red', 'orange'],
+      control: { type: 'radio' },
+    },
+    dimension: {
+      options: ['m', 's'],
+      control: { type: 'radio' },
+    },
+    width: {
+      control: { type: 'text' },
+    },
+    statusViaBackground: {
+      control: { type: 'boolean' },
+    },
+    as: {
+      options: ['div', 'span', 'button'],
+      control: { type: 'radio' },
+    },
+    themeBorderKind: {
+      control: {
+        type: 'radio',
+        options: ['Border radius 0', 'Border radius 2', 'Border radius 4', 'Border radius 8'],
+      },
+    },
+  },
+} as ComponentMeta<typeof TagMenu>;
+
+const items = [
+  {
+    id: '1',
+    display: 'Option one',
+  },
+  {
+    id: '2',
+    display: 'Option two',
+  },
+  {
+    id: '3',
+    display: 'Option three',
+  },
+  {
+    id: '4',
+    display: 'Option four',
+  },
+  {
+    id: '5',
+    display: 'Option five',
+    disabled: true,
+  },
+  {
+    id: '6',
+    display: 'Option six',
+  },
+  {
+    id: '7',
+    display: 'Option seven',
+  },
+];
+
+const Template0: ComponentStory<typeof Tag> = (args: TagProps) => {
+  const [selected, setSelected] = React.useState<string | undefined>(undefined);
+  const model = React.useMemo(() => {
+    return items.map((item) => ({
+      id: item.id,
+      render: (options: RenderOptionProps) => (
+        <MenuItem dimension={args.dimension} {...options} key={item.id}>
+          {item.display}
+        </MenuItem>
+      ),
+      disabled: item.disabled,
+    }));
+  }, [args.dimension]);
+  function swapBorder(theme: Theme): Theme {
+    theme.shape.borderRadiusKind = (args as any).themeBorderKind || theme.shape.borderRadiusKind;
+    return theme;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={swapBorder}>
+        <TagMenu
+          items={model}
+          selected={selected}
+          onChange={(id) => {
+            console.log(`selected: ${id}`);
+            setSelected(id);
+          }}
+          {...args}
+        >
+          Playground
+        </TagMenu>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export const Playground = Template0.bind({});
+Playground.args = {};
+Playground.storyName = 'TagMenu. Playground.';
