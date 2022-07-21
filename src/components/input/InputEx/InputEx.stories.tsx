55d43c2774c2bca73c3c20d353310b90a3ff89ad
Andrey Litvinov <drune.email@gmail.com>
2022-07-21 19:24:57 +0300

    chore(InputEx): add initial component

---
+++ src/components/input/InputEx/InputEx.stories.tsx
@@ -0,0 +1,109 @@
+import React, { ChangeEvent, useState } from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { InputEx } from './index';
+import { withDesign } from 'storybook-addon-designs';
+import { INPUT_DIMENSIONS_VALUES } from '#src/components/input/types';
+import { Theme } from '#src/components/themes';
+import { ThemeProvider } from 'styled-components';
+
+export default {
+  title: 'Admiral-2.1/Input/InputEx',
+  component: InputEx,
+  decorators: [withDesign],
+  parameters: {
+    design: [
+      {
+        type: 'figma',
+        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=39%3A60376',
+      },
+      {
+        type: 'figma',
+        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=39%3A60982',
+      },
+      {
+        type: 'figma',
+        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=39%3A61046',
+      },
+    ],
+  },
+  argTypes: {
+    dimension: {
+      options: INPUT_DIMENSIONS_VALUES,
+      control: { type: 'radio' },
+    },
+
+    disabled: {
+      control: { type: 'boolean' },
+    },
+    readOnly: {
+      control: { type: 'boolean' },
+    },
+    disableCopying: {
+      control: { type: 'boolean' },
+    },
+    displayStatusIcon: {
+      control: { type: 'boolean' },
+    },
+    displayClearIcon: {
+      control: { type: 'boolean' },
+    },
+    placeholder: {
+      type: 'string',
+    },
+    prefix: {
+      type: 'string',
+    },
+    onChange: {
+      action: 'onChange',
+    },
+    icons: {
+      control: false,
+    },
+    containerRef: {
+      control: false,
+    },
+    handleInput: {
+      control: false,
+    },
+    themeBorderKind: {
+      control: {
+        type: 'radio',
+        options: ['Border radius 0', 'Border radius 2', 'Border radius 4', 'Border radius 8'],
+      },
+    },
+  },
+} as ComponentMeta<typeof InputEx>;
+
+const Template: ComponentStory<typeof InputEx> = (props) => {
+  const cleanProps = (Object.keys(props) as Array<keyof typeof props>).reduce((acc, key) => {
+    if (props[key] !== undefined) acc[key] = props[key];
+
+    return acc;
+  }, {} as Record<any, any>);
+
+  const [localValue, setValue] = useState<string>(String(props.value) ?? '');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.currentTarget.value;
+    setValue(inputValue);
+    props.onChange?.(e);
+  };
+
+  function swapBorder(theme: Theme): Theme {
+    theme.shape.borderRadiusKind = (props as any).themeBorderKind || theme.shape.borderRadiusKind;
+    return theme;
+  }
+
+  return (
+    <ThemeProvider theme={swapBorder}>
+      <InputEx {...cleanProps} value={localValue} onChange={handleChange} />
+    </ThemeProvider>
+  );
+};
+
+export const InputExStory = Template.bind({});
+InputExStory.args = {
+  value: 'Привет!',
+  placeholder: 'Placeholder',
+};
+InputExStory.storyName = 'Базовый input компонент';
--- src/components/input/InputEx/index.tsx
+++ src/components/input/InputEx/index.tsx
@@ -1,50 +1,13 @@
-import {
-  changeInputData,
-  CustomInputHandler,
-  InputData,
-  isInputDataDifferent,
-} from '#src/components/common/dom/changeInputData';
-import { refSetter } from '#src/components/common/utils/refSetter';
-import { Container } from '../Container';
-import type { ComponentDimension, ExtraProps, InputStatus } from '#src/components/input/types';
-import { typography } from '#src/components/Typography';
-import { ReactComponent as CloseOutlineSvg } from '@admiral-ds/icons/build/service/CloseOutline.svg';
-import { ReactComponent as EyeCloseOutlineSvg } from '@admiral-ds/icons/build/service/EyeCloseOutline.svg';
-import { ReactComponent as EyeOutlineSvg } from '@admiral-ds/icons/build/service/EyeOutline.svg';
-import type { ForwardedRef, InputHTMLAttributes } from 'react';
 import * as React from 'react';
+import { ForwardedRef, InputHTMLAttributes } from 'react';
+import { ReactComponent as CloseOutlineSvg } from '@admiral-ds/icons/build/service/CloseOutline.svg';
+import { ComponentDimension, containerHeights, ExtraProps, InputStatus } from '#src/components/input';
 import styled, { css } from 'styled-components';
-import { StatusIcon } from '../StatusIcon';
-import { mediumGroupBorderRadius } from '#src/components/themes/borderRadius';
+import { typography } from '#src/components/Typography';
+import { changeInputData } from '#src/components/common/dom/changeInputData';
 import { InputIconButton } from '#src/components/InputIconButton';
-
-const EyeCloseIcon = styled(EyeCloseOutlineSvg)`
-  & *[fill^='#'] {
-    fill: ${(props) => props.theme.color['Neutral/Neutral 50']};
-  }
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  &:hover *[fill^='#'] {
-    fill: ${(props) => props.theme.color['Primary/Primary 70']};
-  }
-`;
-
-const EyeIcon = styled(EyeOutlineSvg)`
-  & *[fill^='#'] {
-    fill: ${(props) => props.theme.color['Neutral/Neutral 50']};
-  }
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  &:hover *[fill^='#'] {
-    fill: ${(props) => props.theme.color['Primary/Primary 70']};
-  }
-`;
+import { refSetter } from '#src/components/common/utils/refSetter';
+import { mediumGroupBorderRadius } from '#src/components/themes/borderRadius';
 
 const iconSizeValue = (props: { dimension?: ComponentDimension }) => {
   switch (props.dimension) {
@@ -72,11 +35,6 @@ const extraPadding = css<ExtraProps>`
     horizontalPaddingValue(props) +
     (iconSizeValue(props) + 8) * (props.iconCount ?? 0) -
     8 * (props.iconCount ? 1 : 0)}px;
-`;
-
-const disabledColors = css`
-  background-color: ${(props) => props.theme.color['Neutral/Neutral 10']};
-  border-color: transparent;
 `;
 
 const BorderedDiv = styled.div`
@@ -108,8 +66,6 @@ const BorderedDiv = styled.div`
 `;
 
 const colorsBorderAndBackground = css<{ disabled?: boolean }>`
-  background-color: ${(props) => props.theme.color['Neutral/Neutral 00']};
-
   &:focus + ${BorderedDiv} {
     border: 2px solid ${(props) => props.theme.color['Primary/Primary 60 Main']};
   }
@@ -132,11 +88,6 @@ const colorsBorderAndBackground = css<{ disabled?: boolean }>`
 
   [data-status='success'] &:hover + ${BorderedDiv}, [data-status='success'] &:focus + ${BorderedDiv} {
     border: 2px solid ${(props) => props.theme.color['Success/Success 50 Main']};
-  }
-
-  [data-read-only] &,
-  &:disabled {
-    ${disabledColors}
   }
 
   &:disabled {
@@ -166,7 +117,7 @@ const Input = styled.input<ExtraProps>`
   border: none;
   background: transparent;
   text-overflow: ellipsis;
-  padding: 0 ${horizontalPaddingValue}px;
+  padding: 0;
 
   color: ${(props) => props.theme.color['Neutral/Neutral 90']};
 
@@ -187,6 +138,16 @@ const Input = styled.input<ExtraProps>`
   ${colorsBorderAndBackground}
   ${extraPadding}
   ${ieFixes}
+`;
+
+const PrefixContainer = styled.div<{ disabled?: boolean; dimension?: ComponentDimension }>`
+  align-self: center;
+  border-right: solid 1px ${(props) => props.theme.color['Neutral/Neutral 20']};
+  padding-right: 8px;
+  margin-right: 8px;
+  ${(props) => (props.dimension === 's' ? typography['Body/Body 2 Long'] : typography['Body/Body 1 Long'])};
+  color: ${(props) =>
+    props.disabled ? props.theme.color['Neutral/Neutral 30'] : props.theme.color['Neutral/Neutral 50']};
 `;
 
 const IconPanel = styled.div<{ disabled?: boolean; dimension?: ComponentDimension }>`
@@ -220,21 +181,29 @@ const IconPanel = styled.div<{ disabled?: boolean; dimension?: ComponentDimensio
   }
 `;
 
-function defaultHandleInput(newInputData: InputData | null): InputData {
-  return newInputData || {};
-}
+const preventDefault = (e: React.MouseEvent) => e.preventDefault();
 
-const stopEvent = (e: React.MouseEvent) => e.preventDefault();
+const Container = styled.div<{ disabled?: boolean; dimension?: ComponentDimension }>`
+  position: relative;
+  display: flex;
+  align-items: stretch;
+  border: none;
+  border-radius: ${(p) => mediumGroupBorderRadius(p.theme.shape)};
+  padding: 0 ${horizontalPaddingValue}px;
+  background-color: ${(props) => props.theme.color['Neutral/Neutral 00']};
+  &[data-read-only],
+  &[data-disabled] {
+    background-color: ${(props) => props.theme.color['Neutral/Neutral 10']};
+  }
+  ${containerHeights}
+`;
 
-export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface InputExProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'prefix'> {
   /** Делает высоту компонента больше или меньше обычной */
   dimension?: ComponentDimension;
 
   /** Иконки для отображения в правом углу поля */
   icons?: React.ReactNode;
-
-  /** Отображать иконку статуса */
-  displayStatusIcon?: boolean;
 
   /** Отображать иконку очистки поля */
   displayClearIcon?: boolean;
@@ -245,56 +214,20 @@ export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   /** Ref контейнера компонента */
   containerRef?: ForwardedRef<HTMLDivElement>;
 
-  /**
-   * Дает возможность изменить значение поля ввода и позицию курсора до момента отображения при следующем цикле рендеринга.
-   * Используется для создания масок ввода
-   */
-  handleInput?: CustomInputHandler;
-
   /**  Наличие этого атрибута отключает возможность выделения и копирования значения поля */
   disableCopying?: boolean;
+
+  prefix?: React.ReactNode;
 }
 
-export const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
+export const InputEx = React.forwardRef<HTMLInputElement, InputExProps>(
   (
-    {
-      type,
-
-      displayStatusIcon,
-      displayClearIcon,
-      status,
-      handleInput = defaultHandleInput,
-      containerRef,
-      icons,
-      children,
-      className,
-      style,
-      placeholder,
-      ...props
-    },
+    { type, displayClearIcon, status, containerRef, icons, children, className, style, placeholder, prefix, ...props },
     ref,
   ) => {
     const inputRef = React.useRef<HTMLInputElement>(null);
 
     const iconArray = React.Children.toArray(icons);
-
-    const [isPasswordVisible, setPasswordVisible] = React.useState(false);
-    if (type === 'password') {
-      const Icon = isPasswordVisible ? EyeIcon : EyeCloseIcon;
-      iconArray.push(
-        <Icon
-          key="eye-icon"
-          aria-hidden
-          onClick={() => {
-            setPasswordVisible(!isPasswordVisible);
-          }}
-        />,
-      );
-    }
-
-    if (displayStatusIcon) {
-      iconArray.push(<StatusIcon key="status-icon" status={status} aria-hidden />);
-    }
 
     if (!props.readOnly && displayClearIcon) {
       iconArray.unshift(
@@ -313,64 +246,21 @@ export const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
 
     const iconCount = iconArray.length;
 
-    // const inputData = value !== undefined && value !== null ? handleInput({ value: String(value) }) : {};
-
-    React.useLayoutEffect(() => {
-      const nullHandledValue = handleInput(null);
-
-      function oninput(this: HTMLInputElement) {
-        const { value, selectionStart, selectionEnd } = this;
-        const currentInputData = { value, selectionStart, selectionEnd };
-
-        const inputData = handleInput(currentInputData);
-
-        if (placeholder && !isInputDataDifferent(nullHandledValue, inputData)) {
-          changeInputData(this, { ...inputData, value: '' });
-        } else {
-          changeInputData(this, inputData);
-        }
-      }
-
-      // Чтение selectionStart в Safari при type='date' вызывает ошибку
-      if (type !== 'date' && inputRef.current) {
-        const node = inputRef.current;
-        node.addEventListener('input', oninput);
-
-        const { value, selectionStart, selectionEnd } = node;
-        const currentInputData = { value, selectionStart, selectionEnd };
-        const inputData = handleInput(currentInputData);
-
-        if (placeholder && !isInputDataDifferent(nullHandledValue, inputData)) {
-          changeInputData(node, { ...inputData, value: '' });
-        } else {
-          changeInputData(node, inputData);
-        }
-
-        return () => {
-          node.removeEventListener('input', oninput);
-        };
-      }
-    }, [inputRef.current, handleInput, placeholder]);
     return (
       <Container
         className={className}
         style={style}
-        disabled={props.disabled}
+        data-disabled={props.disabled ? true : undefined}
         dimension={props.dimension}
         ref={containerRef}
         data-read-only={props.readOnly ? true : undefined}
         data-status={status}
         {...(props.disableCopying && {
-          onMouseDown: stopEvent,
+          onMouseDown: preventDefault,
         })}
       >
-        <Input
-          ref={refSetter(ref, inputRef)}
-          {...props}
-          placeholder={placeholder}
-          iconCount={iconCount}
-          type={type === 'password' && isPasswordVisible ? 'text' : type}
-        />
+        {!!prefix && <PrefixContainer dimension={props.dimension}>{prefix}</PrefixContainer>}
+        <Input ref={refSetter(ref, inputRef)} {...props} placeholder={placeholder} iconCount={iconCount} />
         <BorderedDiv />
         {iconCount > 0 && (
           <IconPanel disabled={props.disabled} dimension={props.dimension}>
@@ -383,7 +273,9 @@ export const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
   },
 );
 
-TextInput.defaultProps = {
+InputEx.defaultProps = {
   dimension: 'm',
+  prefix: 'Prefix',
 } as const;
-TextInput.displayName = 'TextInput';
+
+InputEx.displayName = 'InputEx';
