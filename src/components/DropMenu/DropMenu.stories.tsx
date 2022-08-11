edd33fc5064ea526835c3dbd83601baab4227676
Larisa Markova <lovemirror@rambler.ru>
2022-08-11 15:38:56 +0300

    Add examples with Checkbox and RadioButton

--- src/components/DropMenu/DropMenu.stories.tsx
+++ src/components/DropMenu/DropMenu.stories.tsx
@@ -9,6 +9,8 @@ import { Button } from '#src/components/Button';
 import { typography } from '#src/components/Typography';
 import { ReactComponent as CardSolid } from '@admiral-ds/icons/build/finance/CardSolid.svg';
 import { Tooltip } from '#src/components/Tooltip';
+import { CheckboxField } from '#src/components/form';
+import { RadioButton } from '#src/components/RadioButton';
 
 const Desc = styled.div`
   font-family: 'VTB Group UI';
@@ -86,17 +88,22 @@ const items = [
     id: '4',
     label: 'Option four',
     value: 4,
+    disabled: true,
   },
   {
     id: '5',
     label: 'Option five',
     value: 5,
   },
-  { id: '6', label: 'Option six', value: 7 },
+  {
+    id: '6',
+    label: 'Option six',
+    value: 6,
+  },
   {
     id: '7',
     label: 'Option seven',
-    value: 6,
+    value: 7,
   },
 ];
 
@@ -370,10 +377,129 @@ const DropMenuTooltipTemplate: ComponentStory<typeof DropMenu> = (args) => {
   );
 };
 
+const TemplateWithCheckbox: ComponentStory<typeof DropMenu> = (args) => {
+  const [selected, setSelected] = React.useState<string | undefined>(undefined);
+  const [checkedState, setCheckedState] = React.useState(
+    items.map((item) => {
+      return { id: item.id, checked: false };
+    }),
+  );
+  const model = React.useMemo(() => {
+    return items.map((item, index) => ({
+      id: item.id,
+      render: (options: RenderOptionProps) => (
+        <MenuItem dimension={args.dimension || 's'} {...options} key={item.id}>
+          <CheckboxField disabled={item.disabled} readOnly={true} key={item.id} checked={checkedState[index].checked}>
+            {item.label}
+          </CheckboxField>
+        </MenuItem>
+      ),
+      disabled: item.disabled,
+    }));
+  }, [args.dimension, checkedState]);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <DropMenu
+        {...args}
+        items={model}
+        onChange={(id) => {
+          console.log(`selected: ${id}`);
+          const newCheckedState = checkedState.map((item) => {
+            const newItem = { id: item.id, checked: item.checked };
+            if (newItem.id === id) {
+              newItem.checked = !newItem.checked;
+            }
+            return newItem;
+          });
+          setCheckedState(newCheckedState);
+          setSelected(id);
+        }}
+        onOpen={onOpenMenu}
+        onClose={onCloseMenu}
+        dimension={args.dimension}
+        disabled={args.disabled}
+        selected={selected}
+        renderContentProp={({ buttonRef, handleKeyDown, handleClick, statusIcon }) => {
+          return (
+            <Button ref={buttonRef as React.Ref<HTMLButtonElement>} onKeyDown={handleKeyDown} onClick={handleClick}>
+              Нажми
+              {statusIcon}
+            </Button>
+          );
+        }}
+      />
+    </div>
+  );
+};
+
+const TemplateWithRadiobutton: ComponentStory<typeof DropMenu> = (args) => {
+  const [selected, setSelected] = React.useState<string | undefined>(undefined);
+  const [checkedState, setCheckedState] = React.useState(
+    items.map((item) => {
+      return { id: item.id, checked: false };
+    }),
+  );
+  const model = React.useMemo(() => {
+    return items.map((item, index) => ({
+      id: item.id,
+      render: (options: RenderOptionProps) => (
+        <MenuItem dimension={args.dimension || 's'} {...options} key={item.id}>
+          <RadioButton disabled={item.disabled} key={item.id} readOnly={true} checked={checkedState[index].checked}>
+            {item.label}
+          </RadioButton>
+        </MenuItem>
+      ),
+      disabled: item.disabled,
+    }));
+  }, [args.dimension, checkedState]);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <DropMenu
+        {...args}
+        items={model}
+        onChange={(id) => {
+          console.log(`selected: ${id}`);
+          const newCheckedState = checkedState.map((item) => {
+            const newItem = { id: item.id, checked: item.checked };
+            if (newItem.id === id) {
+              newItem.checked = !newItem.checked;
+            }
+            if (newItem.id === selected) {
+              newItem.checked = !newItem.checked;
+            }
+            return newItem;
+          });
+          setCheckedState(newCheckedState);
+          setSelected(id);
+        }}
+        onOpen={onOpenMenu}
+        onClose={onCloseMenu}
+        dimension={args.dimension}
+        disabled={args.disabled}
+        selected={selected}
+        renderContentProp={({ buttonRef, handleKeyDown, handleClick, statusIcon }) => {
+          return (
+            <Button ref={buttonRef as React.Ref<HTMLButtonElement>} onKeyDown={handleKeyDown} onClick={handleClick}>
+              Нажми
+              {statusIcon}
+            </Button>
+          );
+        }}
+      />
+    </div>
+  );
+};
+
 export const Simple = SimpleTemplate.bind({});
 export const Category = TemplateWithCards.bind({});
 export const DropMenuTooltip = DropMenuTooltipTemplate.bind({});
+export const DropMenuCheckbox = TemplateWithCheckbox.bind({});
+export const DropMenuRadiobutton = TemplateWithRadiobutton.bind({});
 
 Simple.storyName = 'Базовый пример';
 Category.storyName = 'Пример с группами';
 DropMenuTooltip.storyName = 'Пример с Tooltip';
+DropMenuCheckbox.storyName = 'Пример с Checkbox';
+DropMenuRadiobutton.storyName = 'Пример с Radiobutton';
