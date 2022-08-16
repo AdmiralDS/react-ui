a4de402f9393fdbb79774b2332ec0bd61d7d2302
Kutepova Ekaterina <eovetelina@gmail.com>
2022-08-16 16:20:27 +0300

    fix(Tag): remove Tooltip Wrapper from Tag and fix some styles so that Tag can be correctly embeded in other components

--- src/components/TooltipHOC/TooltipHOC.stories.tsx
+++ src/components/TooltipHOC/TooltipHOC.stories.tsx
@@ -31,76 +31,46 @@ const Description = () => (
   </Desc>
 );
 
-export default {
-  title: 'Admiral-2.1/TooltipHOC',
-  decorators: [withDesign],
-  component: TooltipHOC as unknown,
-  parameters: {
-    design: [
-      {
-        type: 'figma',
-        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=37%3A31354',
-      },
-      {
-        type: 'figma',
-        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=37%3A31490',
-      },
-    ],
-    componentSubtitle: <Description />,
-    layout: 'centered',
-    docs: {
-      source: {
-        type: 'code',
-      },
-    },
-  },
-} as any;
+// export default {
+//   title: 'Admiral-2.1/TooltipHOC',
+//   decorators: [withDesign],
+//   component: TooltipHOC as unknown,
+//   parameters: {
+//     design: [
+//       {
+//         type: 'figma',
+//         url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=37%3A31354',
+//       },
+//       {
+//         type: 'figma',
+//         url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=37%3A31490',
+//       },
+//     ],
+//     componentSubtitle: <Description />,
+//     layout: 'centered',
+//     docs: {
+//       source: {
+//         type: 'code',
+//       },
+//     },
+//   },
+// } as any;
 
 const Template1: ComponentStory<any> = (args) => {
   function swapBorder(theme: Theme): Theme {
     theme.shape.borderRadiusKind = (args as any).themeBorderKind || theme.shape.borderRadiusKind;
     return theme;
   }
-  const [visible, setVisible] = React.useState(false);
 
-  const Input = ({ innerRef, ...props }: TextInputProps & WrappedComponentProps) => {
-    return <TextInput ref={innerRef} {...props} />;
-  };
+  const Input = React.forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
+    return <TextInput ref={ref} {...props} />;
+  });
 
   const TooltipedInput = TooltipHOC(Input);
 
   return (
     <ThemeProvider theme={swapBorder}>
-      <TooltipedInput
-        renderContent={() => 'use FC component in TooltipHoc'}
-        visible={visible}
-        handleVisibilityChange={(visible: boolean) => setVisible(visible)}
-      />
-    </ThemeProvider>
-  );
-};
-
-const Template2: ComponentStory<any> = (args) => {
-  function swapBorder(theme: Theme): Theme {
-    theme.shape.borderRadiusKind = (args as any).themeBorderKind || theme.shape.borderRadiusKind;
-    return theme;
-  }
-  const [visible, setVisible] = React.useState(false);
-
-  class Input extends React.Component<TextInputProps & WrappedComponentProps> {
-    render() {
-      return <TextInput ref={this.props.innerRef} {...this.props} />;
-    }
-  }
-
-  const TooltipedInput = TooltipHOC(Input);
-  return (
-    <ThemeProvider theme={swapBorder}>
-      <TooltipedInput
-        renderContent={() => 'use class component in TooltipHoc'}
-        visible={visible}
-        handleVisibilityChange={(visible: boolean) => setVisible(visible)}
-      />
+      <TooltipedInput renderContent={() => 'use FC component in TooltipHoc'} />
     </ThemeProvider>
   );
 };
@@ -110,11 +80,10 @@ const Template3: ComponentStory<any> = (args) => {
     theme.shape.borderRadiusKind = (args as any).themeBorderKind || theme.shape.borderRadiusKind;
     return theme;
   }
-  const [visible, setVisible] = React.useState(false);
 
-  const Input = ({ innerRef, ...props }: TextInputProps & WrappedComponentProps) => {
-    return <TextInput ref={innerRef} {...props} />;
-  };
+  const Input = React.forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
+    return <TextInput ref={ref} {...props} />;
+  });
 
   const TooltipedInput = TooltipHOC(Input);
   const inputRef = React.useRef<HTMLInputElement | null>(null);
@@ -124,12 +93,7 @@ const Template3: ComponentStory<any> = (args) => {
 
   return (
     <ThemeProvider theme={swapBorder}>
-      <TooltipedInput
-        ref={inputRef}
-        renderContent={() => 'set ref on TooltipHoc result'}
-        visible={visible}
-        handleVisibilityChange={(visible: boolean) => setVisible(visible)}
-      />
+      <TooltipedInput ref={inputRef} renderContent={() => 'set ref on TooltipHoc result'} />
     </ThemeProvider>
   );
 };
@@ -140,82 +104,36 @@ const Template4: ComponentStory<any> = (args) => {
     return theme;
   }
 
-  const Input = ({ innerRef, ...props }: TextInputProps & WrappedComponentProps) => {
+  const Input = React.forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
     const inputRef = React.useRef<HTMLInputElement | null>(null);
     React.useEffect(() => {
       if (inputRef.current) {
         inputRef.current.style.border = '4px solid red';
       }
     }, []);
+    return <TextInput ref={refSetter(ref, inputRef)} {...props} />;
+  });
 
-    return <TextInput ref={refSetter(innerRef, inputRef)} {...props} />;
-  };
-
-  class Input2 extends React.Component<TextInputProps & WrappedComponentProps> {
-    private inputField: React.RefObject<HTMLInputElement>;
-    constructor(props: any) {
-      super(props);
-      this.inputField = React.createRef();
-    }
-    componentDidMount() {
-      if (this.inputField.current) {
-        this.inputField.current.style.border = '4px solid blue';
-      }
-    }
-
-    render() {
-      return <TextInput ref={refSetter(this.props.innerRef, this.inputField)} {...this.props} />;
-    }
-  }
-  const [visible1, setVisible1] = React.useState(false);
-  const [visible2, setVisible2] = React.useState(false);
   const TooltipedInput = TooltipHOC(Input);
-  const TooltipedInput2 = TooltipHOC(Input2);
   return (
     <ThemeProvider theme={swapBorder}>
-      <TooltipedInput
-        renderContent={() => 'merge refs in FC component'}
-        visible={visible1}
-        handleVisibilityChange={(visible: boolean) => setVisible1(visible)}
-      />
-      <TooltipedInput2
-        renderContent={() => 'merge refs in class component'}
-        visible={visible2}
-        handleVisibilityChange={(visible: boolean) => setVisible2(visible)}
-      />
+      <TooltipedInput renderContent={() => 'merge refs in FC component'} />
     </ThemeProvider>
   );
 };
 
-export const TooltipFC = Template1.bind({});
-TooltipFC.args = {};
-TooltipFC.storyName = 'TooltipHoc. Вызов с функциональным компонентом.';
+// export const TooltipFC = Template1.bind({});
+// TooltipFC.args = {};
+// TooltipFC.storyName = 'TooltipHoc. Вызов с функциональным компонентом.';
 
-export const TooltipClass = Template2.bind({});
-TooltipClass.args = {};
-TooltipClass.storyName = 'TooltipHoc. Вызов с классовым компонентом.';
+// export const TooltipClass = Template2.bind({});
+// TooltipClass.args = {};
+// TooltipClass.storyName = 'TooltipHoc. Вызов с классовым компонентом.';
 
-export const TooltipRef = Template3.bind({});
-TooltipRef.args = {};
-TooltipRef.storyName = 'TooltipHoc. Прокидывание ref на результат вызова TooltipHoc.';
+// export const TooltipRef = Template3.bind({});
+// TooltipRef.args = {};
+// TooltipRef.storyName = 'TooltipHoc. Прокидывание ref на результат вызова TooltipHoc.';
 
-export const TooltipRefSetter = Template4.bind({});
-TooltipRefSetter.args = {};
-TooltipRefSetter.storyName = 'TooltipHoc. Merge inputRef с ref6 созданным внутри компонента.';
-
-const Counter = React.forwardRef((props, ref: any) => {
-  class Counter extends React.Component {
-    render() {
-      return (
-        <div>
-          <button ref={ref}>button</button>
-        </div>
-      );
-    }
-  }
-  return <Counter />;
-});
-
-const Button = React.forwardRef((props, ref: any) => {
-  return <button ref={ref}>{props.children}</button>;
-});
+// export const TooltipRefSetter = Template4.bind({});
+// TooltipRefSetter.args = {};
+// TooltipRefSetter.storyName = 'TooltipHoc. Merge inputRef с ref6 созданным внутри компонента.';
--- src/components/TooltipHOC/index.tsx
+++ src/components/TooltipHOC/index.tsx
@@ -1,19 +1,11 @@
 import * as React from 'react';
 import type { RefCallback, RefObject } from '#src/components/common/utils/handleRef';
-import { handleRef } from '#src/components/common/utils/handleRef';
-import { getScrollableParents } from '#src/components/common/utils/getScrollableParents';
-import { getScrollbarSize } from '#src/components/common/dom/scrollbarUtil';
 import { refSetter } from '#src/components/common/utils/refSetter';
+import { Tooltip } from '../TooltipRefactor';
 
-import type { TooltipPositionType } from './utils';
-import { getTooltipDirection } from './utils';
-import { FakeTarget, Portal, TooltipContainer, TooltipWrapper } from './style';
-
-const TOOLTIP_DELAY = 1500;
+type TooltipPositionType = 'bottom' | 'top' | 'left' | 'right' | 'bottomPageCenter' | 'topPageCenter';
 
 export interface TooltipHocProps {
-  visible: boolean;
-  handleVisibilityChange: (visible: boolean) => void;
   renderContent: () => React.ReactNode;
   container?: Element | null;
   withDelay?: boolean;
@@ -27,163 +19,20 @@ export type WrappedComponentProps = {
 
 export function TooltipHOC<P extends React.ComponentPropsWithRef<any>>(Component: React.ComponentType<P>) {
   const WrappedComponent = (props: P & TooltipHocProps & WrappedComponentProps) => {
-    const {
-      innerRef,
-      visible,
-      handleVisibilityChange,
-      renderContent,
-      container: userContainer,
-      withDelay,
-      tooltipRef,
-      tooltipPosition,
-      ...wrappedOnlyProps
-    } = props;
+    const { innerRef, renderContent, container, withDelay, tooltipRef, tooltipPosition, ...wrappedOnlyProps } = props;
     const anchorElementRef = React.useRef<any>(null);
-    const tooltipElementRef = React.useRef<HTMLDivElement | null>(null);
-    const container: Element = userContainer || document.body;
-    let scrollableParents: Array<Element> | undefined = undefined;
-    let showTooltipTimer: any;
-
-    const [portalFlexDirection, setPortalFlexDirection] = React.useState('');
-    const [portalFullWidth, setPortalFullWidth] = React.useState(false);
-
-    const hideTooltip = () => handleVisibilityChange(false);
-
-    const manageTooltip = (scrollbarSize: number) => {
-      if (anchorElementRef.current && tooltipElementRef.current) {
-        const direction = getTooltipDirection(
-          anchorElementRef.current,
-          tooltipElementRef.current,
-          scrollbarSize,
-          tooltipPosition,
-        );
-        const tooltip = tooltipElementRef.current;
-        switch (direction) {
-          case 'topPageCenter':
-            setPortalFlexDirection('column-reverse');
-            setPortalFullWidth(true);
-            tooltip.style.margin = '0 0 8px 0';
-            break;
-          case 'bottomPageCenter':
-            setPortalFlexDirection('column');
-            setPortalFullWidth(true);
-            tooltip.style.margin = '8px 0 0 0';
-            break;
-          case 'left':
-            setPortalFlexDirection('row-reverse');
-            setPortalFullWidth(false);
-            tooltip.style.margin = '0 8px 0 0';
-            break;
-          case 'right':
-            setPortalFlexDirection('row');
-            setPortalFullWidth(false);
-            tooltip.style.margin = '0 0 0 8px';
-            break;
-          case 'top':
-            setPortalFlexDirection('column-reverse');
-            setPortalFullWidth(false);
-            tooltip.style.margin = '0 0 8px 0';
-            break;
-          case 'bottom':
-          default:
-            setPortalFlexDirection('column');
-            setPortalFullWidth(false);
-            tooltip.style.margin = '8px 0 0 0';
-        }
-      }
-    };
-
-    const attachRef = (node: HTMLDivElement) => handleRef(node, tooltipRef, tooltipElementRef);
-
-    React.useEffect(() => {
-      window.addEventListener('resize', hideTooltip);
-      window.addEventListener('scroll', hideTooltip);
-
-      /**  если у anchorElement есть родительский элемент, который имеет собственный скролл,
-       * необходимо повесить на этого родителя обработчик скролла */
-      if (!scrollableParents && anchorElementRef.current) {
-        scrollableParents = getScrollableParents(anchorElementRef.current);
-        scrollableParents?.forEach((el) => el.addEventListener('scroll', hideTooltip));
-      }
-      return () => {
-        clearTimeout(showTooltipTimer);
-        window.removeEventListener('resize', hideTooltip);
-        window.removeEventListener('scroll', hideTooltip);
-        scrollableParents?.forEach((el) => el.removeEventListener('scroll', hideTooltip));
-      };
-    });
-
-    React.useEffect(() => {
-      const scrollbarSize = getScrollbarSize();
-      manageTooltip(scrollbarSize);
-    }, [renderContent(), anchorElementRef, tooltipPosition, container]);
-
-    // First container render always happens downward and transparent,
-    // after size and position settled transparency returns to normal
-    React.useEffect(() => {
-      if (tooltipElementRef.current) {
-        tooltipElementRef.current.style.opacity = '1';
-      }
-    }, [tooltipElementRef.current, visible]);
-
-    /** Вешаю обработчик на mouseenter через addEventListener,
-     * React SyntheticEvent onMouseEnter отрабатывает некорректно в случае,
-     * если мышь была наведена на задизейбленный элемент, а потом была передвинута на AnchorWrapper
-     * https://github.com/facebook/react/issues/19419#:~:text=mouseenter%20does%20not%20fire%20because,element%20of%20the%20opposing%20event.
-     */
-    React.useEffect(() => {
-      anchorElementRef.current?.addEventListener('mouseenter', handleMouseEnter);
-      anchorElementRef.current?.addEventListener('mouseleave', handleMouseLeave);
-      return () => {
-        anchorElementRef.current?.removeEventListener('mouseenter', handleMouseEnter);
-        anchorElementRef.current?.removeEventListener('mouseleave', handleMouseLeave);
-      };
-    }, [anchorElementRef.current]);
-
-    const handleMouseEnter = () => {
-      showTooltipTimer = window.setTimeout(
-        () => {
-          (handleVisibilityChange as any)(true);
-          manageTooltip(getScrollbarSize());
-        },
-        withDelay ? TOOLTIP_DELAY : 0,
-      );
-    };
-
-    const handleMouseLeave = () => {
-      clearTimeout(showTooltipTimer);
-      hideTooltip();
-    };
 
     return (
       <>
-        <Component
-          {...(wrappedOnlyProps as any)}
-          innerRef={refSetter(innerRef, anchorElementRef)}
-          onFocus={(e: FocusEvent) => {
-            handleMouseEnter();
-            (props as any)?.onFocus?.(e);
-          }}
-          onBlur={(e: FocusEvent) => {
-            handleMouseLeave();
-            (props as any)?.onBlur?.(e);
-          }}
+        <Component {...(wrappedOnlyProps as any)} ref={refSetter(innerRef, anchorElementRef)} />
+        <Tooltip
+          targetRef={anchorElementRef}
+          renderContent={renderContent}
+          container={container}
+          withDelay={withDelay}
+          tooltipPosition={tooltipPosition}
+          tooltipRef={tooltipRef}
         />
-        {visible && (
-          <Portal
-            targetRef={anchorElementRef}
-            container={container}
-            flexDirection={portalFlexDirection}
-            fullContainerWidth={portalFullWidth}
-          >
-            <FakeTarget />
-            <TooltipWrapper ref={attachRef}>
-              <TooltipContainer role="tooltip" {...props}>
-                {renderContent()}
-              </TooltipContainer>
-            </TooltipWrapper>
-          </Portal>
-        )}
       </>
     );
   };
