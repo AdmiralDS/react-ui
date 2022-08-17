665c39350db7ef952458152baba3b07395f2c1d4
Larisa Markova <lovemirror@rambler.ru>
2022-08-17 14:08:55 +0300

    Add splitDataAttributes function

---
+++ src/components/common/utils/splitDataAttributes.ts
@@ -0,0 +1,15 @@
+const FORM_FIELD_DATA_ATTRIBUTE = 'data-container';
+
+export function splitDataAttributes(initialProps: any, containerProps: any) {
+  (Object.keys(initialProps) as Array<keyof typeof initialProps>).forEach((key) => {
+    if (typeof key === 'string' && key.startsWith(FORM_FIELD_DATA_ATTRIBUTE)) {
+      const internalKey = 'data' + key.slice(FORM_FIELD_DATA_ATTRIBUTE.length);
+      const descriptor = Object.getOwnPropertyDescriptor(initialProps, key);
+      containerProps[key] = initialProps[key];
+      if (typeof descriptor !== 'undefined') {
+        Object.defineProperty(initialProps, internalKey, descriptor);
+      }
+      delete initialProps[key];
+    }
+  });
+}
