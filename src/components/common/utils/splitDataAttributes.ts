ca2e7e6abec3e0f0fd46311dbcae8834343dfecf
Larisa Markova <lovemirror@rambler.ru>
2022-08-22 23:49:26 +0300

    Remove duplicating data attributes

--- src/components/common/utils/splitDataAttributes.ts
+++ src/components/common/utils/splitDataAttributes.ts
@@ -4,29 +4,19 @@ export const DROPDOWN_DATA_ATTRIBUTE = 'data-dropdown-container';
 export function splitFormFieldDataAttributes(initialProps: any, containerProps: any) {
   (Object.keys(initialProps) as Array<keyof typeof initialProps>).forEach((key) => {
     if (typeof key === 'string' && key.startsWith(FORM_FIELD_DATA_ATTRIBUTE)) {
-      const internalKey = 'data' + key.slice(FORM_FIELD_DATA_ATTRIBUTE.length);
-      const descriptor = Object.getOwnPropertyDescriptor(initialProps, key);
       containerProps[key] = initialProps[key];
-      if (typeof descriptor !== 'undefined') {
-        Object.defineProperty(initialProps, internalKey, descriptor);
-      }
       delete initialProps[key];
     }
   });
 }
 
-export function splitDropdownDataAttributes(initialProps: any): [Record<string, any>, Record<string, any>] {
+export function splitDropdownDataAttributes(initialProps: any): Record<string, any> {
   const dropMenuProps = {} as Record<string, any>;
-  const containerProps = {} as Record<string, any>;
   (Object.keys(initialProps) as Array<keyof typeof initialProps>).forEach((key) => {
     if (typeof key === 'string' && key.startsWith(DROPDOWN_DATA_ATTRIBUTE)) {
-      const dropMenuKey = 'data-dropdown' + key.slice(DROPDOWN_DATA_ATTRIBUTE.length);
-      dropMenuProps[dropMenuKey] = initialProps[key];
-      if (initialProps !== containerProps) {
-        containerProps[key] = initialProps[key];
-        delete initialProps[key];
-      }
+      dropMenuProps[key] = initialProps[key];
+      delete initialProps[key];
     }
   });
-  return [dropMenuProps, containerProps];
+  return [dropMenuProps];
 }
