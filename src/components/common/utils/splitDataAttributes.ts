8a831dcf75434c7e908b170d6a48c7303254efbe
Larisa Markova <lovemirror@rambler.ru>
2022-08-22 23:54:21 +0300

    Add function passDataAttributes

--- src/components/common/utils/splitDataAttributes.ts
+++ src/components/common/utils/splitDataAttributes.ts
@@ -2,21 +2,20 @@ export const FORM_FIELD_DATA_ATTRIBUTE = 'data-container';
 export const DROPDOWN_DATA_ATTRIBUTE = 'data-dropdown-container';
 
 export function splitFormFieldDataAttributes(initialProps: any, containerProps: any) {
-  (Object.keys(initialProps) as Array<keyof typeof initialProps>).forEach((key) => {
-    if (typeof key === 'string' && key.startsWith(FORM_FIELD_DATA_ATTRIBUTE)) {
-      containerProps[key] = initialProps[key];
-      delete initialProps[key];
-    }
-  });
+  passDataAttributes(initialProps, containerProps, FORM_FIELD_DATA_ATTRIBUTE);
 }
 
 export function splitDropdownDataAttributes(initialProps: any): Record<string, any> {
   const dropMenuProps = {} as Record<string, any>;
-  (Object.keys(initialProps) as Array<keyof typeof initialProps>).forEach((key) => {
-    if (typeof key === 'string' && key.startsWith(DROPDOWN_DATA_ATTRIBUTE)) {
-      dropMenuProps[key] = initialProps[key];
-      delete initialProps[key];
+  passDataAttributes(initialProps, dropMenuProps, DROPDOWN_DATA_ATTRIBUTE);
+  return [dropMenuProps];
+}
+
+function passDataAttributes(sourceProps: any, destinationProps: any, dataAttributesStart: string) {
+  (Object.keys(sourceProps) as Array<keyof typeof sourceProps>).forEach((key) => {
+    if (typeof key === 'string' && key.startsWith(dataAttributesStart)) {
+      destinationProps[key] = sourceProps[key];
+      delete sourceProps[key];
     }
   });
-  return [dropMenuProps];
 }
