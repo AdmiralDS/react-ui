8136839a7505ac071501ed538b25718d67b2bd7b
Александр Сталоверов <AStaloverov@inno.tech>
2022-09-05 10:29:19 +0300

    fix(DateInput): change default input handler

--- src/components/input/DateInput/defaultDateInputHandle.ts
+++ src/components/input/DateInput/defaultDateInputHandle.ts
@@ -3,6 +3,26 @@ import { splice } from '#src/components/common/utils/splice';
 
 export const MASK_VALUE = '__.__.____';
 
+export const formatDate = (value: string) => {
+  const DD = value.substring(0, 2).replace(/\D/g, '_').padEnd(2, '_');
+  const MM = value.substring(2, 4).replace(/\D/g, '_').padEnd(2, '_');
+  const YYYY = value.substring(4, 8).replace(/\D/g, '_').padEnd(4, '_');
+
+  return `${DD}.${MM}.${YYYY}`;
+};
+
+const calcCursorPosition = (inputValue: string, selectionStart: number) => {
+  const underlinePos = inputValue.indexOf('_');
+  // selectionStart должен быть не менее 1, иначе маска не будет отображаться
+  const valueLength = underlinePos === -1 ? inputValue.length : underlinePos || 1;
+
+  if (selectionStart < valueLength) {
+    return inputValue.charAt(selectionStart - 1) === '.' ? selectionStart + 1 : selectionStart;
+  }
+
+  return valueLength;
+};
+
 export function defaultDateInputHandle(inputData: InputData | null): InputData {
   if (inputData === null) {
     return { value: MASK_VALUE, selectionStart: 0, selectionEnd: 0 };
@@ -20,35 +40,15 @@ export function defaultDateInputHandle(inputData: InputData | null): InputData {
     return { ...inputData, value: inputValue };
   }
 
-  let addCount = 0;
-  let moveCursor = 0;
+  const clearValue = inputValue.replace(/\D/g, '');
+  inputValue = formatDate(clearValue);
 
-  if (inputValue.charAt(selectionStart - 1) === '.') {
-    addCount = -1;
-    moveCursor = 0;
-  }
-  if (inputValue.charAt(selectionStart) === '.') {
-    moveCursor = 1;
-    if (/\d/.test(inputValue.charAt(selectionStart - 1))) {
-      addCount = 1;
-    } else {
-      addCount = -1;
-      moveCursor = 0;
-    }
-  }
+  const cursorPos = calcCursorPosition(inputValue, selectionStart);
 
-  const spliced = splice(inputValue, selectionStart + addCount, lengthDifference, '').replace(/\./g, '');
-
-  inputValue = `${spliced.substr(0, 2).replace(/\D/g, '_')}.${spliced.substr(2, 2).replace(/\D/g, '_')}.${spliced
-    .substr(4, 4)
-    .replace(/\D/g, '_')}`;
-
-  const result = {
+  return {
     ...inputData,
     value: inputValue,
-    selectionStart: selectionStart + moveCursor,
-    selectionEnd: selectionStart + moveCursor,
+    selectionStart: cursorPos,
+    selectionEnd: cursorPos,
   };
-
-  return result;
 }
--- src/components/input/DateInput/defaultDateRangeInputHandle.ts
+++ src/components/input/DateInput/defaultDateRangeInputHandle.ts
@@ -1,8 +1,21 @@
 import { InputData } from '#src/components/common/dom/changeInputData';
-import { defaultDateInputHandle, MASK_VALUE as DATE_MASK_VALUE } from './defaultDateInputHandle';
+import { formatDate, MASK_VALUE as DATE_MASK_VALUE } from './defaultDateInputHandle';
 
 const DATE_SEPARATOR = ' - ';
 const MASK_VALUE = DATE_MASK_VALUE + DATE_SEPARATOR + DATE_MASK_VALUE;
+
+const calcCursorPosition = (inputValue: string, selectionStart: number) => {
+  const underlinePos = inputValue.indexOf('_');
+  // selectionStart должен быть не менее 1, иначе маска не будет отображаться
+  const valueLength = underlinePos === -1 ? inputValue.length : underlinePos || 1;
+
+  if (selectionStart < valueLength) {
+    if (selectionStart > 10 && selectionStart < 14) return 14;
+    return inputValue.charAt(selectionStart - 1) === '.' ? selectionStart + 1 : selectionStart;
+  }
+
+  return valueLength;
+};
 
 export function defaultDateRangeInputHandle(inputData: InputData | null): InputData {
   if (!inputData || !inputData.value) {
@@ -15,62 +28,19 @@ export function defaultDateRangeInputHandle(inputData: InputData | null): InputD
   // if entered correct masked data
   if (inputValue.replace(/\d/g, '_') === MASK_VALUE) return inputData;
 
+  const clearValue = inputValue.replace(/\D/g, '');
+
+  const leftPart = formatDate(clearValue.substring(0, 8));
+  const rightPart = formatDate(clearValue.substring(8, 16));
+
   const selectionStart = inputData.selectionStart || 0;
-  const symbolsChanged = inputValue.length - MASK_VALUE.length;
+  const formattedValue = leftPart + DATE_SEPARATOR + rightPart;
 
-  // Is cursor move to separator
-  if (DATE_MASK_VALUE.length < selectionStart + 1 && DATE_MASK_VALUE.length + DATE_SEPARATOR.length > selectionStart) {
-    const selectionPos =
-      inputValue.length < MASK_VALUE.length ? DATE_MASK_VALUE.length : DATE_MASK_VALUE.length + DATE_SEPARATOR.length;
+  const cursorPos = calcCursorPosition(formattedValue, selectionStart);
 
-    const handledData = defaultDateInputHandle({
-      ...inputData,
-      value: inputValue.substring(0, DATE_MASK_VALUE.length),
-    });
-
-    const ret = {
-      value: handledData.value + DATE_SEPARATOR + inputValue.slice(-10),
-      selectionStart: selectionPos,
-      selectionEnd: selectionPos,
-    };
-
-    return ret;
-  }
-
-  // cursor on the left side
-  if (selectionStart <= DATE_MASK_VALUE.length) {
-    const handledData = defaultDateInputHandle({
-      ...inputData,
-      value:
-        inputValue.length > DATE_MASK_VALUE.length
-          ? inputValue.substring(0, DATE_MASK_VALUE.length + Math.max(symbolsChanged, -DATE_MASK_VALUE.length))
-          : inputValue,
-    });
-    const rightHandleData = defaultDateInputHandle({
-      value:
-        inputValue.length > DATE_MASK_VALUE.length
-          ? inputValue.substring(inputValue.length - DATE_MASK_VALUE.length, inputValue.length)
-          : '',
-    });
-    const ret = {
-      value: handledData.value + DATE_SEPARATOR + rightHandleData.value,
-      selectionStart: handledData.selectionStart,
-      selectionEnd: handledData.selectionEnd,
-    };
-    return ret;
-  }
-
-  //cursor on the right side
-  const handledData = defaultDateInputHandle({
-    value: inputValue.slice(-10 - symbolsChanged),
-    selectionStart: selectionStart - (DATE_MASK_VALUE.length + DATE_SEPARATOR.length),
-    selectionEnd: selectionStart - (DATE_MASK_VALUE.length + DATE_SEPARATOR.length),
-  });
-
-  const ret = {
-    value: inputValue.substring(0, DATE_MASK_VALUE.length) + DATE_SEPARATOR + handledData.value,
-    selectionStart: (handledData.selectionStart || 0) + DATE_MASK_VALUE.length + DATE_SEPARATOR.length,
-    selectionEnd: (handledData.selectionEnd || 0) + DATE_MASK_VALUE.length + DATE_SEPARATOR.length,
+  return {
+    value: formattedValue,
+    selectionStart: cursorPos,
+    selectionEnd: cursorPos,
   };
-  return ret;
 }
