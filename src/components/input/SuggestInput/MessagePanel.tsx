0b6bb8886de9d08e93aee8319087617345425246
Larisa Markova <lovemirror@rambler.ru>
2022-09-28 14:40:26 +0300

    Use MenuItem

--- src/components/input/SuggestInput/MessagePanel.tsx
+++ src/components/input/SuggestInput/MessagePanel.tsx
@@ -1,23 +1,16 @@
-import { typography } from '#src/components/Typography';
 import styled from 'styled-components';
+import { MenuItem } from '#src/components/Menu/MenuItem';
 
-export const MessagePanel = styled.div`
+export const MessagePanel = styled(MenuItem)`
   margin: 8px 0;
-  height: 40px;
-  ${typography['Body/Body 1 Long']}
-  line-height: 40px;
-  padding: 0 16px;
   color: ${(p) => p.theme.color['Neutral/Neutral 50']};
+  pointer-events: none;
 
-  [data-dimension='xl'] & {
-    height: 48px;
-    line-height: 48px;
-  }
-
-  [data-dimension='s'] & {
-    height: 32px;
-    ${typography['Body/Body 2 Long']}
-    line-height: 32px;
-    padding: 0 12px;
+  &&[data-disabled='true'] {
+    background-color: ${(p) => p.theme.color['Special/Elevated BG']};
+    color: ${(p) => p.theme.color['Neutral/Neutral 50']};
+    && *[fill^='#'] {
+      fill: ${(p) => p.theme.color['Neutral/Neutral 50']};
+    }
   }
 `;
