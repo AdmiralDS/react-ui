9f06f8cffe7ee86e206241493603e9de9d6d04f3
Andrey Litvinov <drune.email@gmail.com>
2022-10-13 17:29:39 +0300

    fix:  fix TagMenu  export

--- src/index.ts
+++ src/index.ts
@@ -68,6 +68,7 @@ export * from './components/InputIconButton';
 export * from './components/GroupActionsPane';
 export * from './components/skeleton/animation';
 export * from './components/common/utils/refSetter';
+export * from './components/TagMenu';
 
 export { FontsVTBGroup } from './components/FontsVTBGroup';
 export { DropdownProvider } from './components/DropdownProvider';
