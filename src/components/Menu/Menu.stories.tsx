1793930562c757e45ab292604411a8f777da97d0
Larisa Markova <lovemirror@rambler.ru>
2022-10-17 14:00:05 +0300

    feat(Menu): add example with CheckboxGroup

--- src/components/Menu/Menu.stories.tsx
+++ src/components/Menu/Menu.stories.tsx
@@ -1,7 +1,7 @@
-import React, { ChangeEvent, HTMLAttributes, useContext, useEffect, useMemo, useState } from 'react';
+import React, { ChangeEvent, forwardRef, HTMLAttributes, useContext, useEffect, useMemo, useState } from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Menu } from '#src/components/Menu';
-import { MenuItem, RenderOptionProps } from '#src/components/Menu/MenuItem';
+import { ItemProps, MenuItem, RenderOptionProps } from '#src/components/Menu/MenuItem';
 import styled, { css, ThemeContext, ThemeProvider } from 'styled-components';
 import { typography } from '#src/components/Typography';
 import { ReactComponent as CardSolid } from '@admiral-ds/icons/build/finance/CardSolid.svg';
@@ -18,6 +18,7 @@ import { ReactComponent as PlusOutline } from '@admiral-ds/icons/build/service/P
 import { uid } from '#src/components/common/uid';
 import { keyboardKey } from '#src/components/common/keyboardKey';
 import { mediumGroupBorderRadius } from '#src/components/themes/borderRadius';
+import { ItemDimension } from '#src/components/Menu/menuItemMixins';
 
 const Desc = styled.div`
   font-family: 'VTB Group UI';
@@ -674,6 +675,197 @@ const MenuActionsAddUserValueTemplate: ComponentStory<typeof Menu> = (props) => 
   );
 };
 
+const paddingLeft = css<{ level?: number; dimension?: ItemDimension }>`
+  padding-left: ${({ dimension, level }) => {
+    switch (dimension) {
+      case 's':
+        return 12 + 28 * (level ? level : 0);
+      case 'm':
+      case 'l':
+      default:
+        return 16 + 32 * (level ? level : 0);
+    }
+  }}px;
+`;
+const CheckboxGroupMenuItem = styled(MenuItem)<{ level?: number; dimension?: ItemDimension }>`
+  ${paddingLeft}
+`;
+
+const itemsCheckboxGroup: Array<CheckboxGroupItemProps> = [
+  {
+    id: '1',
+    label: 'Опция 1',
+    checked: false,
+    children: [
+      {
+        id: '1.1',
+        label: 'Опция 1.1',
+        checked: false,
+      },
+      {
+        id: '1.2',
+        label: 'Опция 1.2',
+        checked: false,
+        children: [
+          {
+            id: '1.2.1',
+            label: 'Опция 1.2.1',
+            checked: false,
+          },
+          {
+            id: '1.2.2',
+            label: 'Опция 1.2.2',
+            checked: false,
+          },
+          {
+            id: '1.2.3',
+            label: 'Опция 1.2.3',
+            checked: false,
+          },
+        ],
+      },
+      {
+        id: '1.3',
+        label: 'Опция 1.3',
+        checked: false,
+      },
+    ],
+  },
+  {
+    id: '2',
+    label: 'Опция 2',
+    checked: false,
+  },
+  {
+    id: '3',
+    label: 'Опция 3',
+    checked: false,
+  },
+];
+interface CheckboxGroupItemProps {
+  id: string;
+  label: React.ReactNode;
+  checked: boolean;
+  disabled?: boolean;
+  children?: Array<CheckboxGroupItemProps>;
+}
+
+type CheckboxNodesMapItem = {
+  dependencies?: Array<string>;
+  level: number;
+  node: CheckboxGroupItemProps;
+};
+
+const treeToMap = (
+  source: Array<CheckboxGroupItemProps>,
+  level = 0,
+  dependencies?: Array<Array<string>>,
+): Map<string, CheckboxNodesMapItem> => {
+  return source.reduce((acc: Map<string, CheckboxNodesMapItem>, item) => {
+    const key = item.id.toString();
+    const currentNode: CheckboxNodesMapItem = { level, node: item };
+    acc.set(key, currentNode);
+
+    if (dependencies) {
+      dependencies.forEach((dependency) => dependency.push(key));
+    }
+    if (item.children) {
+      const allDependencies = dependencies ? [...dependencies] : [];
+      const itemDependencies: Array<string> = [];
+      currentNode.dependencies = itemDependencies;
+      acc.set(key, currentNode);
+      allDependencies.push(itemDependencies);
+      const map = treeToMap(item.children, level + 1, allDependencies);
+      return new Map([...acc, ...map]);
+    }
+
+    return acc;
+  }, new Map<string, CheckboxNodesMapItem>());
+};
+const MenuCheckboxGroupTemplate: ComponentStory<typeof Menu> = (args) => {
+  const [internalModel, setInternalModel] = useState<Array<CheckboxGroupItemProps>>([...itemsCheckboxGroup]);
+
+  const map = useMemo(() => {
+    return treeToMap(internalModel);
+  }, [internalModel]);
+
+  const setChecked = (id: string, value: boolean) => {
+    const mapItem = map.get(id);
+    if (mapItem?.node.disabled) return;
+    if (mapItem) {
+      mapItem.node.checked = value;
+    }
+
+    if (mapItem?.dependencies?.length) {
+      mapItem?.dependencies?.forEach((depId: string) => setChecked(depId, value));
+    }
+  };
+
+  const toggleCheck = (id: string) => {
+    const item = map.get(id);
+    const hasChildren = !!item?.node.children;
+
+    const indeterminate =
+      item?.dependencies?.some((depId: string) => map.get(depId)?.node.checked) &&
+      item?.dependencies?.some((depId: string) => !map.get(depId)?.node.checked);
+
+    const checked = hasChildren
+      ? indeterminate
+        ? true
+        : item?.dependencies?.every((depId: string) => map.get(depId)?.node.checked)
+      : item?.node.checked;
+
+    setChecked(id, !checked);
+
+    setInternalModel([...internalModel]);
+  };
+
+  const model = useMemo(() => {
+    const model: ItemProps[] = [];
+    map.forEach((item) => {
+      const node = item.node;
+      const hasChildren = !!node.children;
+      const indeterminate =
+        item.dependencies?.some((depId: string) => map.get(depId)?.node.checked) &&
+        item.dependencies?.some((depId: string) => !map.get(depId)?.node.checked);
+      const checked = hasChildren
+        ? item.dependencies?.every((depId: string) => map.get(depId)?.node.checked)
+        : node.checked;
+      model.push({
+        id: node.id,
+        render: (options: RenderOptionProps) => (
+          <CheckboxGroupMenuItem dimension={args.dimension || 's'} {...options} level={item.level} key={node.id}>
+            <CheckboxField
+              dimension={args.dimension !== 's' ? 'm' : args.dimension}
+              checked={checked}
+              indeterminate={indeterminate}
+              onChange={() => toggleCheck(node.id)}
+            >
+              {node.label}
+            </CheckboxField>
+          </CheckboxGroupMenuItem>
+        ),
+        disabled: node.disabled,
+      });
+    });
+
+    return model;
+  }, [args.dimension, map]);
+
+  function swapBorder(theme: Theme): Theme {
+    theme.shape.borderRadiusKind = (args as any).themeBorderKind || theme.shape.borderRadiusKind;
+    return theme;
+  }
+
+  return (
+    <ThemeProvider theme={swapBorder}>
+      <Wrapper style={{ width: 'fit-content' }}>
+        <Menu {...args} model={model} />
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
+
 export const Simple = SimpleTemplate.bind({});
 export const Category = TemplateWithCards.bind({});
 export const CustomItems = CustomItemTemplate.bind({});
@@ -683,6 +875,7 @@ export const MenuTooltip = MenuTooltipTemplate.bind({});
 export const MultiLineMenu = MultiLineMenuTemplate.bind({});
 export const MenuActionsTwoButtons = MenuActionsTwoButtonsTemplate.bind({});
 export const MenuActionsAddUserValue = MenuActionsAddUserValueTemplate.bind({});
+export const MenuCheckboxGroup = MenuCheckboxGroupTemplate.bind({});
 
 Simple.storyName = 'Базовый пример';
 Category.storyName = 'Пример с группами';
@@ -693,3 +886,4 @@ MenuTooltip.storyName = 'Пример с Tooltip';
 MultiLineMenu.storyName = 'Пример с многострочными пунктами';
 MenuActionsTwoButtons.storyName = 'Пример с Actions с двумя кнопками';
 MenuActionsAddUserValue.storyName = 'Пример с Actions и Search';
+MenuCheckboxGroup.storyName = 'Пример с CheckboxGroup';
