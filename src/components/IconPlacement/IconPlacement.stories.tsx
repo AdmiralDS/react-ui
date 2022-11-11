2a63e7e1c1305116cf9fd400512ec06782c50502
Larisa Markova <lovemirror@rambler.ru>
2022-11-11 18:29:21 +0300

    feat(IconPlacement): add new component

---
+++ src/components/IconPlacement/IconPlacement.stories.tsx
@@ -0,0 +1,141 @@
+import * as React from 'react';
+import { withDesign } from 'storybook-addon-designs';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ReactComponent as CloseOutline } from '@admiral-ds/icons/build/service/CloseOutline.svg';
+import styled from 'styled-components';
+import { T } from '#src/components/T';
+import { IconPlacement } from '#src/components/IconPlacement/index';
+
+export default {
+  title: 'Admiral-2.1/IconPlacement',
+  decorators: [withDesign],
+  component: IconPlacement,
+  parameters: {
+    docs: {
+      source: {
+        code: null,
+      },
+    },
+    layout: 'centered',
+    design: [
+      {
+        type: 'figma',
+        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=133774%3A104360',
+      },
+      {
+        type: 'figma',
+        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=133774%3A104658',
+      },
+      {
+        type: 'figma',
+        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=133799%3A104398',
+      },
+      {
+        type: 'figma',
+        url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=133799%3A104512',
+      },
+    ],
+  },
+  argTypes: {
+    dimension: {
+      control: false,
+    },
+    disabled: {
+      control: false,
+    },
+  },
+} as ComponentMeta<typeof IconPlacement>;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 20px;
+`;
+
+const WrapperVertical = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 20px;
+`;
+
+const handleClick = () => console.log('IconPlacement clicked');
+const IconPlacementDemoTemplate: ComponentStory<typeof IconPlacement> = (args) => {
+  return (
+    <WrapperVertical>
+      <T font="Body/Body 1 Short" as="div">
+        Icon Placement - вспомогательный компонент, обычно используется в составе других компонентов или организмов. По
+        сути это любая иконка с заранее заданными состояниями Default, Hover, Press, Focus, Disable.
+      </T>
+      <T font="Body/Body 1 Short" as="div">
+        Рекомендуется в качестве отдельно стоящих иконок использовать компонент Icon Button, а Icon Placement применять
+        только в случаях, когда это действительно необходимо, как правило, это ограниченное пространство внутри
+        компонентов.
+      </T>
+      <T font="Body/Body 1 Short" as="div">
+        У компонента есть три размера иконки: M 24, S 20 и XS 16. Причем, у размеров M и S два размера Hover-круга для
+        различных ситуаций, что бы вписываться внутрь небольших компонентов. БОльшие размеры круга обозначены в названии
+        вариантов компонента как Big. Размер Hover-круга не учитывается в размере компонента и выходит за его границы.
+      </T>
+      <Wrapper>
+        <WrapperVertical>
+          <T font="Body/Body 1 Long" as="div">
+            Dimension - m
+          </T>
+          <IconPlacement dimension="m" onClick={handleClick}>
+            <CloseOutline />
+          </IconPlacement>
+          <IconPlacement dimension="m" onClick={handleClick} disabled={true}>
+            <CloseOutline />
+          </IconPlacement>
+          <T font="Body/Body 1 Long" as="div">
+            Dimension - m Big
+          </T>
+          <IconPlacement dimension="mBig" onClick={handleClick}>
+            <CloseOutline />
+          </IconPlacement>
+          <IconPlacement dimension="mBig" disabled={true} onClick={handleClick}>
+            <CloseOutline />
+          </IconPlacement>
+        </WrapperVertical>
+        <WrapperVertical>
+          <T font="Body/Body 1 Long" as="div">
+            Dimension - s
+          </T>
+          <IconPlacement dimension="s" onClick={handleClick}>
+            <CloseOutline />
+          </IconPlacement>
+          <IconPlacement dimension="s" onClick={handleClick} disabled={true}>
+            <CloseOutline />
+          </IconPlacement>
+          <T font="Body/Body 1 Long" as="div">
+            Dimension - s Big
+          </T>
+          <IconPlacement dimension="sBig" onClick={handleClick}>
+            <CloseOutline />
+          </IconPlacement>
+          <IconPlacement dimension="sBig" disabled={true} onClick={handleClick}>
+            <CloseOutline />
+          </IconPlacement>
+        </WrapperVertical>
+        <WrapperVertical>
+          <T font="Body/Body 1 Long" as="div">
+            Dimension - xs
+          </T>
+          <IconPlacement dimension="xs" onClick={handleClick}>
+            <CloseOutline />
+          </IconPlacement>
+          <IconPlacement dimension="xs" disabled={true} onClick={handleClick}>
+            <CloseOutline />
+          </IconPlacement>
+        </WrapperVertical>
+      </Wrapper>
+    </WrapperVertical>
+  );
+};
+
+export const IconPlacementDemo = IconPlacementDemoTemplate.bind({});
+IconPlacementDemo.storyName = 'IconPlacement. Размеры';
---
+++ src/components/IconPlacement/index.tsx
@@ -0,0 +1,120 @@
+import * as React from 'react';
+import { ButtonHTMLAttributes } from 'react';
+import styled, { css } from 'styled-components';
+
+const IconSizeM = 24;
+const IconSizeS = 20;
+const IconSizeXS = 16;
+
+export type IconPlacementDimension = 'mBig' | 'm' | 'sBig' | 's' | 'xs';
+
+function getIconSize(dimension?: IconPlacementDimension) {
+  switch (dimension) {
+    case 'm':
+    case 'mBig':
+      return IconSizeM;
+    case 's':
+    case 'sBig':
+      return IconSizeS;
+    case 'xs':
+      return IconSizeXS;
+    default:
+      return IconSizeM;
+  }
+}
+
+function getHoverSize(dimension?: IconPlacementDimension) {
+  switch (dimension) {
+    case 'mBig':
+    case 'sBig':
+      return getIconSize(dimension) + 12;
+    case 'm':
+    case 's':
+    case 'xs':
+      return getIconSize(dimension) + 8;
+    default:
+      return getIconSize(dimension) + 8;
+  }
+}
+
+export interface IconPlacementProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  /** Размер кнопки */
+  dimension?: IconPlacementDimension;
+  /** Отключение кнопки */
+  disabled?: boolean;
+}
+
+const hoverStyle = css<{ dimension?: IconPlacementDimension }>`
+  &::before {
+    content: '';
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+    width: ${(p) => getHoverSize(p.dimension)}px;
+    height: ${(p) => getHoverSize(p.dimension)}px;
+    background-color: ${({ theme }) => theme.color['Opacity/Hover']};
+  }
+`;
+const StyledButton = styled.button<{ dimension?: IconPlacementDimension }>`
+  position: relative;
+  padding: 0;
+  box-sizing: border-box;
+  border: none;
+  background-color: transparent;
+  appearance: none;
+  height: ${(p) => getIconSize(p.dimension)}px;
+  width: ${(p) => getIconSize(p.dimension)}px;
+
+  cursor: pointer;
+
+  &:disabled {
+    cursor: default;
+    pointer-events: none;
+    & *[fill^='#'] {
+      fill: ${({ theme }) => theme.color['Neutral/Neutral 30']};
+    }
+  }
+
+  &:focus:not(:disabled) {
+    ${hoverStyle}
+    &::before {
+      background-color: ${({ theme }) => theme.color['Opacity/Focus']};
+    }
+  }
+
+  &:hover:not(:disabled) {
+    ${hoverStyle}
+  }
+
+  &:active:not(:disabled) {
+    ${hoverStyle}
+    &::before {
+      background-color: ${({ theme }) => theme.color['Opacity/Press']};
+    }
+  }
+`;
+
+const IconPlacementContent = styled.div<{ dimension?: IconPlacementDimension }>`
+  height: 100%;
+
+  & *[fill^='#'] {
+    fill: ${(p) => p.theme.color['Neutral/Neutral 50']};
+  }
+
+  & > svg {
+    height: ${(p) => getIconSize(p.dimension)}px;
+    width: ${(p) => getIconSize(p.dimension)}px;
+  }
+`;
+
+export const IconPlacement = React.forwardRef<HTMLButtonElement, IconPlacementProps>(
+  ({ type = 'button', dimension = 'm', disabled = false, children, ...props }, ref) => {
+    return (
+      <StyledButton ref={ref} type={type} dimension={dimension} disabled={disabled} {...props}>
+        <IconPlacementContent dimension={dimension}>{children}</IconPlacementContent>
+      </StyledButton>
+    );
+  },
+);
