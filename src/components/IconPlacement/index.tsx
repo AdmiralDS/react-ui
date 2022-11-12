d21c87b972480ebd4d6b55a645eef606246d51f8
Larisa Markova <lovemirror@rambler.ru>
2022-11-12 14:29:31 +0300

    Use separate container for effects instead of ::before

--- src/components/IconPlacement/index.tsx
+++ src/components/IconPlacement/index.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { ButtonHTMLAttributes } from 'react';
 import styled, { css } from 'styled-components';
 import { smallGroupBorderRadius } from '#src/components/themes/borderRadius';
+import { refSetter } from '#src/components/common/utils/refSetter';
 
 const IconSizeM = 24;
 const IconSizeS = 20;
 const IconSizeXS = 16;
 
 export type IconPlacementDimension = 'mBig' | 'm' | 'sBig' | 's' | 'xs';
+export type ContainerRole = 'hover' | 'active' | 'focus';
 
 function getIconSize(dimension?: IconPlacementDimension) {
   switch (dimension) {
@@ -24,17 +26,31 @@ function getIconSize(dimension?: IconPlacementDimension) {
   }
 }
 
+function getHoverOffset(dimension?: IconPlacementDimension) {
+  switch (dimension) {
+    case 'mBig':
+    case 'sBig':
+      return 6;
+    case 'm':
+    case 's':
+    case 'xs':
+      return 4;
+    default:
+      return 4;
+  }
+}
+
 function getHoverSize(dimension?: IconPlacementDimension) {
   switch (dimension) {
     case 'mBig':
     case 'sBig':
-      return getIconSize(dimension) + 12;
+      return getIconSize(dimension) + getHoverOffset(dimension) * 2;
     case 'm':
     case 's':
     case 'xs':
-      return getIconSize(dimension) + 8;
+      return getIconSize(dimension) + getHoverOffset(dimension) * 2;
     default:
-      return getIconSize(dimension) + 8;
+      return getIconSize(dimension) + getHoverOffset(dimension) * 2;
   }
 }
 
@@ -44,6 +60,24 @@ export interface IconPlacementProps extends ButtonHTMLAttributes<HTMLButtonEleme
   /** Отключение кнопки */
   disabled?: boolean;
 }
+
+/*  &:focus:not(:disabled) {
+    ${hoverStyle}
+    &::before {
+      background-color: ${({ theme }) => theme.color['Opacity/Focus']};
+    }
+  }
+
+  &:hover:not(:disabled) {
+    ${hoverStyle}
+  }
+
+  &:active:not(:disabled) {
+    ${hoverStyle}
+    &::before {
+      background-color: ${({ theme }) => theme.color['Opacity/Press']};
+    }
+  }*/
 
 const hoverStyle = css<{ dimension?: IconPlacementDimension }>`
   &::before {
@@ -79,24 +113,6 @@ const StyledButton = styled.button<{ dimension?: IconPlacementDimension }>`
     }
   }
 
-  &:focus:not(:disabled) {
-    ${hoverStyle}
-    &::before {
-      background-color: ${({ theme }) => theme.color['Opacity/Focus']};
-    }
-  }
-
-  &:hover:not(:disabled) {
-    ${hoverStyle}
-  }
-
-  &:active:not(:disabled) {
-    ${hoverStyle}
-    &::before {
-      background-color: ${({ theme }) => theme.color['Opacity/Press']};
-    }
-  }
-
   &:focus-visible {
     outline-offset: 2px;
     outline: ${(p) => p.theme.color['Primary/Primary 60 Main']} solid 2px;
@@ -116,12 +132,102 @@ const IconPlacementContent = styled.div<{ dimension?: IconPlacementDimension }>`
   }
 `;
 
+const getContainerColor = (containerRole?: ContainerRole) => {
+  switch (containerRole) {
+    case 'hover':
+      return 'Opacity/Hover';
+    case 'active':
+      return 'Opacity/Press';
+    //return 'Error/Error 20';
+    case 'focus':
+      return 'Opacity/Focus';
+    //return 'Primary/Primary 70';
+    default:
+      return 'Opacity/Hover';
+  }
+};
+
+const Container = styled.div<{ dimension?: IconPlacementDimension; containerRole?: ContainerRole }>`
+  width: ${(p) => getHoverSize(p.dimension)}px;
+  height: ${(p) => getHoverSize(p.dimension)}px;
+  border-radius: 50%;
+  background-color: ${(p) => (p.containerRole ? p.theme.color[getContainerColor(p.containerRole)] : 'transparent')};
+  pointer-events: none;
+  top: -${(p) => getHoverOffset(p.dimension)}px;
+  left: -${(p) => getHoverOffset(p.dimension)}px;
+  position: absolute;
+`;
+
+const Wrapper = styled.div<{ dimension?: IconPlacementDimension }>`
+  height: ${(p) => getIconSize(p.dimension)}px;
+  width: ${(p) => getIconSize(p.dimension)}px;
+  overflow: visible;
+  position: relative;
+`;
+
 export const IconPlacement = React.forwardRef<HTMLButtonElement, IconPlacementProps>(
   ({ type = 'button', dimension = 'm', disabled = false, children, ...props }, ref) => {
+    const buttonRef = React.useRef<HTMLButtonElement>(null);
+    const [visible, setVisible] = React.useState<boolean>(false);
+    const [containerRole, setContainerRole] = React.useState<ContainerRole | undefined>(undefined);
+
+    React.useEffect(() => {
+      const button = buttonRef.current;
+      function hover() {
+        if (containerRole === undefined) {
+          setContainerRole('hover');
+          setVisible(true);
+        }
+      }
+      function blur() {
+        if (containerRole === 'focus') {
+          setContainerRole(undefined);
+          setVisible(false);
+        }
+      }
+      function hide() {
+        if (containerRole === 'hover') {
+          setContainerRole(undefined);
+          setVisible(false);
+        }
+      }
+      function active() {
+        setContainerRole('active');
+        setVisible(true);
+      }
+      function focus() {
+        setContainerRole('focus');
+        setVisible(true);
+      }
+      if (button) {
+        /** Рекомендуется использовать именно addEventListener, так как React SyntheticEvent onMouseEnter
+         * отрабатывает некорректно в случае, если мышь была наведена на задизейбленный элемент,
+         * а затем была наведена на target элемент
+         * https://github.com/facebook/react/issues/19419 */
+        button.addEventListener('mouseenter', hover);
+        button.addEventListener('mouseleave', hide);
+        button.addEventListener('blur', blur);
+        button.addEventListener('mousedown', active);
+        button.addEventListener('mouseup', focus);
+        //button.addEventListener('focus', focus);
+        return () => {
+          button.removeEventListener('mouseenter', hover);
+          button.removeEventListener('mouseleave', hide);
+          button.removeEventListener('blur', blur);
+          button.removeEventListener('mousedown', active);
+          button.removeEventListener('mouseup', focus);
+          //button.removeEventListener('focus', focus);
+        };
+      }
+    }, [buttonRef.current, visible, containerRole]);
+
     return (
-      <StyledButton ref={ref} type={type} dimension={dimension} disabled={disabled} {...props}>
-        <IconPlacementContent dimension={dimension}>{children}</IconPlacementContent>
-      </StyledButton>
+      <Wrapper dimension={dimension}>
+        {visible && <Container containerRole={containerRole} dimension={dimension} />}
+        <StyledButton ref={refSetter(ref, buttonRef)} type={type} dimension={dimension} disabled={disabled} {...props}>
+          <IconPlacementContent dimension={dimension}>{children}</IconPlacementContent>
+        </StyledButton>
+      </Wrapper>
     );
   },
 );
