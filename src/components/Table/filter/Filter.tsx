ca3460dfc6e7c2c9c08c7ae3c602017af571eac8
Kutepova Ekaterina <eovetelina@gmail.com>
2022-12-06 13:31:08 +0300

    feat(Table): add column argument to renderFilter callback, fix stories

--- src/components/Table/filter/Filter.tsx
+++ src/components/Table/filter/Filter.tsx
@@ -3,116 +3,85 @@ import styled from 'styled-components';
 import { keyboardKey } from '#src/components/common/keyboardKey';
 import { refSetter } from '#src/components/common/utils/refSetter';
 import { StyledDropdownContainer } from '#src/components/DropdownContainer';
+import type { TableProps, Column } from '#src/components/Table';
+
 import { Button } from './Button';
 
 const FilterDropdownContainer = styled(StyledDropdownContainer)`
   background-color: ${(p) => p.theme.color['Special/Elevated BG']};
 `;
 
-type Dimension = 'xl' | 'l' | 'm' | 's';
-type FilterProps = {
-  /** Функция закрытия фильтра */
-  closeMenu: () => void;
-  /** Функция установки состояния фильтра (активный/неактивный).
-   * Необходимо для окрашивания иконки фильтра в синий цвет при активном фильтре и в серый при неактивном фильтре.
-   */
-  setFilterActive: (isActive: boolean) => void;
-};
-
 export interface FilterCompProps extends Omit<React.HTMLAttributes<HTMLButtonElement>, 'onChange'> {
+  column: Column;
   /** Размер компонента */
-  dimension?: Dimension;
-  /** Функция отрисовки содержимого фильтра (выпадающего меню фильтра). Если её не передать, значок фильтра отображаться не будет */
-  renderFilter: (obj: FilterProps) => React.ReactNode;
-  /** Функция отрисовки иконки фильтра. По умолчанию в качестве иконки фильтра применяется OverflowIcon (троеточие) */
-  renderFilterIcon?: () => React.ReactNode;
-  /** Колбек на клик вне меню фильтра */
-  onFilterMenuClickOutside?: (obj: FilterProps, event: Event) => void;
-  /** Колбек на открытие меню фильтра */
-  onFilterMenuOpen?: () => void;
-  /** Колбек на закрытие меню фильтра */
-  onFilterMenuClose?: () => void;
-  /** Выравнивание контента ячеек столбца по левому или правому краю. По умолчанию left */
-  cellAlign?: 'left' | 'right';
+  dimension?: TableProps['dimension'];
   /** Ref на элемент, относительно которого будет позиционироваться меню фильтра */
   targetRef: React.RefObject<HTMLElement>;
 }
 
-export const Filter = React.forwardRef<HTMLButtonElement, FilterCompProps>(
-  (
-    {
-      renderFilter,
-      renderFilterIcon,
-      onFilterMenuClickOutside,
-      onFilterMenuOpen,
-      onFilterMenuClose,
-      cellAlign,
-      targetRef,
-      ...props
-    },
-    ref,
-  ) => {
-    const [menuOpened, setMenuOpened] = React.useState<boolean>(false);
-    const [isFilterActive, setFilterActive] = React.useState<boolean>(false);
-    const btnRef = React.useRef<HTMLButtonElement>(null);
+export const Filter = React.forwardRef<HTMLButtonElement, FilterCompProps>(({ column, targetRef, ...props }, ref) => {
+  const { renderFilter, renderFilterIcon, onFilterMenuClickOutside, onFilterMenuOpen, onFilterMenuClose, cellAlign } =
+    column;
+  const [menuOpened, setMenuOpened] = React.useState<boolean>(false);
+  const [isFilterActive, setFilterActive] = React.useState<boolean>(false);
+  const btnRef = React.useRef<HTMLButtonElement>(null);
 
-    const reverseMenu = () => {
-      setMenuOpened((prevOpened) => {
-        prevOpened ? onFilterMenuClose?.() : onFilterMenuOpen?.();
-        return !prevOpened;
-      });
-    };
-    const closeMenu = () => {
-      setMenuOpened(false);
-      onFilterMenuClose?.();
-      btnRef.current?.focus();
-    };
-    const clickOutside = (e: Event) => {
-      if (e.target && btnRef.current?.contains(e.target as Node)) {
-        return;
-      }
-      onFilterMenuClickOutside?.({ closeMenu, setFilterActive }, e);
-    };
+  const reverseMenu = () => {
+    setMenuOpened((prevOpened) => {
+      prevOpened ? onFilterMenuClose?.() : onFilterMenuOpen?.();
+      return !prevOpened;
+    });
+  };
+  const closeMenu = () => {
+    setMenuOpened(false);
+    onFilterMenuClose?.();
+    btnRef.current?.focus();
+  };
+  const clickOutside = (e: Event) => {
+    if (e.target && btnRef.current?.contains(e.target as Node)) {
+      return;
+    }
+    onFilterMenuClickOutside?.({ closeMenu, setFilterActive }, e);
+  };
 
-    const handleBtnKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-      const code = keyboardKey.getCode(e);
-      if (code === keyboardKey.ArrowDown || code === keyboardKey.Enter || code === keyboardKey[' ']) {
-        setMenuOpened(true);
-        onFilterMenuOpen?.();
-        e.preventDefault();
-      }
-    };
+  const handleBtnKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+    const code = keyboardKey.getCode(e);
+    if (code === keyboardKey.ArrowDown || code === keyboardKey.Enter || code === keyboardKey[' ']) {
+      setMenuOpened(true);
+      onFilterMenuOpen?.();
+      e.preventDefault();
+    }
+  };
 
-    const handleMenuKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-      const code = keyboardKey.getCode(e);
-      if (code === keyboardKey.Escape || code === keyboardKey.Tab) {
-        closeMenu();
-      }
-    };
-    return (
-      <>
-        <Button
-          ref={refSetter(ref, btnRef)}
-          onKeyDown={handleBtnKeyDown}
-          onClick={reverseMenu}
-          aria-expanded={menuOpened}
-          aria-haspopup={menuOpened}
-          isFilterActive={isFilterActive}
-          renderFilterIcon={renderFilterIcon}
-          {...props}
-        />
-        {menuOpened && (
-          <FilterDropdownContainer
-            role="listbox"
-            targetRef={targetRef}
-            alignSelf={cellAlign === 'left' ? 'flex-end' : 'flex-start'}
-            onClickOutside={clickOutside}
-            onKeyDown={handleMenuKeyDown}
-          >
-            {renderFilter({ closeMenu, setFilterActive })}
-          </FilterDropdownContainer>
-        )}
-      </>
-    );
-  },
-);
+  const handleMenuKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    const code = keyboardKey.getCode(e);
+    if (code === keyboardKey.Escape || code === keyboardKey.Tab) {
+      closeMenu();
+    }
+  };
+  return (
+    <>
+      <Button
+        ref={refSetter(ref, btnRef)}
+        onKeyDown={handleBtnKeyDown}
+        onClick={reverseMenu}
+        aria-expanded={menuOpened}
+        aria-haspopup={menuOpened}
+        isFilterActive={isFilterActive}
+        renderFilterIcon={renderFilterIcon}
+        {...props}
+      />
+      {menuOpened && (
+        <FilterDropdownContainer
+          role="listbox"
+          targetRef={targetRef}
+          alignSelf={cellAlign === 'left' ? 'flex-end' : 'flex-start'}
+          onClickOutside={clickOutside}
+          onKeyDown={handleMenuKeyDown}
+        >
+          {renderFilter?.({ closeMenu, setFilterActive }, column)}
+        </FilterDropdownContainer>
+      )}
+    </>
+  );
+});
