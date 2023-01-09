8a7e9f3a90d81cbe8381e8dbaa144d5382e1dac0
Andrey Litvinov <drune.email@gmail.com>
2023-01-09 16:42:09 +0300

    fix: linter and snapshots

--- src/components/input/types.ts
+++ src/components/input/types.ts
@@ -1,9 +1,9 @@
 export const INPUT_DIMENSIONS_VALUES = ['xl', 'm', 's'] as const;
 /** Размеры компонента относительно начального */
-export type ComponentDimension = typeof INPUT_DIMENSIONS_VALUES[number];
+export type ComponentDimension = (typeof INPUT_DIMENSIONS_VALUES)[number];
 
 export const INPUT_STATUS_VALUES = ['error', 'success'] as const;
-export type InputStatus = typeof INPUT_STATUS_VALUES[number];
+export type InputStatus = (typeof INPUT_STATUS_VALUES)[number];
 
 export interface ExtraProps {
   /** Делает высоту компонента больше или меньше обычной */
