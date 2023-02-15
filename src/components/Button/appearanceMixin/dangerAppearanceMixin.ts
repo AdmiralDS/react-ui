d3f6b92d77bfc6beb39b107766961c54960a4d9c
Larisa Markova <lovemirror@rambler.ru>
2023-02-15 12:39:50 +0300

     fix(Button): remove focus style after click

--- src/components/Button/appearanceMixin/dangerAppearanceMixin.ts
+++ src/components/Button/appearanceMixin/dangerAppearanceMixin.ts
@@ -8,7 +8,6 @@ export const dangerAppearanceMixin = css`
     fill: ${({ theme }) => theme.color['Special/Static White']};
   }
 
-  &:focus,
   &:hover {
     background-color: ${({ theme }) => theme.color['Error/Error 70']};
     border-color: ${({ theme }) => theme.color['Error/Error 70']};
