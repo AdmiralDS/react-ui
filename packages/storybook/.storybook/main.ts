5b80a8c547579d9930b6d9844d22fbb142bec315
Andrey Litvinov <drune.email@gmail.com>
2023-03-30 15:12:15 +0300

    chore: update deps

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -1,18 +1,18 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-styling"
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-styling',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
 };
 export default config;
--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -1,17 +1,21 @@
-import type { Preview, ReactRenderer } from "@storybook/react";
-import type { DecoratorFunction } from "@storybook/types"
-import { DARK_THEME, DropdownProvider, FontsVTBGroup, LIGHT_THEME } from "@admiral-ds/react-ui";
-import * as React from "react";
+import type { Preview, ReactRenderer } from '@storybook/react';
+import type { DecoratorFunction } from '@storybook/types';
+import {
+  DARK_THEME,
+  DropdownProvider,
+  FontsVTBGroup,
+  LIGHT_THEME,
+} from '@admiral-ds/react-ui';
+import * as React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { withThemeFromJSXProvider } from '@storybook/addon-styling';
-
 
 const GlobalStyles = createGlobalStyle`
 `;
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -21,11 +25,11 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => (
-      <ThemeProvider theme={LIGHT_THEME} >
-      <DropdownProvider>
-        <FontsVTBGroup />
-        <Story/>
-      </DropdownProvider>
+      <ThemeProvider theme={LIGHT_THEME}>
+        <DropdownProvider>
+          <FontsVTBGroup />
+          <Story />
+        </DropdownProvider>
       </ThemeProvider>
     ),
 
