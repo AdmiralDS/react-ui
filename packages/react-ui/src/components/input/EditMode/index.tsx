77396ae73bc98441fd645b227d7e148fb7d9d6e3
Александр Сталоверов <AStaloverov@inno.tech>
2023-04-03 15:14:42 +0300

    feat(EditMode): add multiline view mode

--- packages/react-ui/src/components/input/EditMode/index.tsx
+++ packages/react-ui/src/components/input/EditMode/index.tsx
@@ -14,27 +14,36 @@ import { ReactComponent as CloseOutline } from '@admiral-ds/icons/build/service/
 import { Tooltip } from '#src/components/Tooltip';
 import { checkOverflow } from '#src/components/common/utils/checkOverflow';
 
+const TypographyMixin = css`
+  [data-dimension='s'] & {
+    ${typography['Body/Body 2 Long']}
+  }
+
+  [data-dimension='s-bold'] & {
+    ${typography['Subtitle/Subtitle 3']}
+  }
+
+  [data-dimension='m'] & {
+    ${typography['Body/Body 1 Long']}
+  }
+
+  [data-dimension='m-bold'] & {
+    ${typography['Subtitle/Subtitle 2']}
+  }
+
+  [data-dimension='xl'] & {
+    ${typography['Header/H5']}
+  }
+
+  [data-dimension='xxl'] & {
+    ${typography['Header/H4']}
+  }
+`;
+
 const EditInput = styled(TextInput)`
   flex: 1 1 auto;
   & input {
-    [data-dimension='s'] & {
-      ${typography['Body/Body 2 Long']}
-    }
-    [data-dimension='s-bold'] & {
-      ${typography['Subtitle/Subtitle 3']}
-    }
-    [data-dimension='m'] & {
-      ${typography['Body/Body 1 Long']}
-    }
-    [data-dimension='m-bold'] & {
-      ${typography['Subtitle/Subtitle 2']}
-    }
-    [data-dimension='xl'] & {
-      ${typography['Header/H5']}
-    }
-    [data-dimension='xxl'] & {
-      ${typography['Header/H4']}
-    }
+    ${TypographyMixin}
   }
 `;
 
@@ -43,13 +52,35 @@ const EditButton = styled(Button)`
   flex-shrink: 0;
 `;
 
+const multilineIcon = css`
+  align-self: flex-start;
+
+  [data-dimension='s-bold'] &&&,
+  [data-dimension='s'] &&& {
+    margin-top: 6px;
+  }
+
+  [data-dimension='m'] &&&,
+  [data-dimension='m-bold'] &&& {
+    margin-top: 8px;
+  }
+
+  [data-dimension='xl'] &&& {
+    margin-top: 14px;
+  }
+
+  [data-dimension='xxl'] &&& {
+    margin-top: 12px;
+  }
+`;
+
 const iconStyle = css`
   display: flex;
   flex-shrink: 0;
   cursor: pointer;
 `;
 
-const EditIcon = styled(EditSolid)`
+const EditIcon = styled(EditSolid)<{ $multiline: boolean }>`
   ${iconStyle}
   & *[fill^='#'] {
     fill: ${({ theme }) => theme.color['Neutral/Neutral 50']};
@@ -69,11 +100,14 @@ const EditIcon = styled(EditSolid)`
       }
     }
   }
+
+  ${(p) => p.$multiline && multilineIcon}
 `;
 
 const CheckIcon = styled(CheckClearOutline)`
   ${iconStyle}
 `;
+
 const ClearIcon = styled(CloseOutline)`
   ${iconStyle}
 `;
@@ -81,56 +115,93 @@ const ClearIcon = styled(CloseOutline)`
 const Wrapper = styled.div<{ cssMixin?: FlattenInterpolation<ThemeProps<DefaultTheme>> }>`
   display: flex;
   align-items: center;
-  ${({ cssMixin }) => cssMixin || ''}
+  ${({ cssMixin }) => cssMixin || ''};
 `;
 
-const Text = styled.div`
-  display: block;
+const MultilineMixin = css`
+  word-wrap: break-word;
+  height: auto;
+  white-space: normal;
+
+  [data-dimension='s-bold'] &&&,
+  [data-dimension='s'] &&& {
+    padding-top: 6px;
+    padding-bottom: 6px;
+    padding-left: 12px;
+  }
+
+  [data-dimension='m'] &&&,
+  [data-dimension='m-bold'] &&& {
+    padding-top: 8px;
+    padding-bottom: 8px;
+  }
+
+  [data-dimension='xl'] &&& {
+    padding-top: 14px;
+    padding-bottom: 14px;
+  }
+
+  [data-dimension='xxl'] &&& {
+    padding-top: 12px;
+    padding-bottom: 12px;
+  }
+`;
+
+const SingleMixin = css`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+
+  [data-dimension='s'] &&& {
+    height: 32px;
+    line-height: 32px;
+    padding-left: 12px;
+  }
+
+  [data-dimension='s-bold'] &&& {
+    height: 32px;
+    line-height: 32px;
+    padding-left: 12px;
+  }
+
+  [data-dimension='m'] &&& {
+    height: 40px;
+    line-height: 40px;
+  }
+
+  [data-dimension='m-bold'] &&& {
+    height: 40px;
+    line-height: 40px;
+  }
+
+  [data-dimension='xl'] &&& {
+    height: 56px;
+    line-height: 56px;
+  }
+
+  [data-dimension='xxl'] &&& {
+    height: 56px;
+    line-height: 56px;
+  }
+`;
+
+const Text = styled.div<{ $multiline?: boolean }>`
+  display: block;
+
   align-items: center;
   cursor: text;
   margin-right: 12px;
   padding-left: 16px;
   color: ${({ theme }) => theme.color['Neutral/Neutral 90']};
-  [data-dimension='s'] & {
-    ${typography['Body/Body 2 Long']}
-    height: 32px;
-    line-height: 32px;
-    padding-left: 12px;
-  }
-  [data-dimension='s-bold'] & {
-    ${typography['Subtitle/Subtitle 3']}
-    height: 32px;
-    line-height: 32px;
-    padding-left: 12px;
-  }
-  [data-dimension='m'] & {
-    ${typography['Body/Body 1 Long']}
-    height: 40px;
-    line-height: 40px;
-  }
-  [data-dimension='m-bold'] & {
-    ${typography['Subtitle/Subtitle 2']}
-    height: 40px;
-    line-height: 40px;
-  }
-  [data-dimension='xl'] & {
-    ${typography['Header/H5']}
-    height: 56px;
-    line-height: 56px;
-  }
-  [data-dimension='xxl'] & {
-    ${typography['Header/H4']}
-    height: 56px;
-    line-height: 56px;
-  }
+
+  ${TypographyMixin};
+  ${(p) => (p.$multiline ? MultilineMixin : SingleMixin)}
 
   [data-disabled='true'] & {
     cursor: default;
     pointer-events: none;
   }
+
   [data-disabled='false'] & {
     &:hover {
       & + ${EditIcon} *[fill^='#'] {
@@ -168,6 +239,8 @@ export interface EditModeProps extends Omit<TextInputProps, 'dimension'> {
   onClear?: () => void;
   /** Отображение тултипа, по умолчанию true */
   showTooltip?: boolean;
+  /** Отображение тултипа, по умолчанию true */
+  multilineView?: boolean;
 }
 
 export const EditMode = React.forwardRef<HTMLInputElement, EditModeProps>(
@@ -182,6 +255,7 @@ export const EditMode = React.forwardRef<HTMLInputElement, EditModeProps>(
       onClear,
       value,
       showTooltip = true,
+      multilineView = false,
       ...props
     },
     ref,
@@ -218,7 +292,7 @@ export const EditMode = React.forwardRef<HTMLInputElement, EditModeProps>(
       if (textRef.current && checkOverflow(textRef.current) !== overflowActive) {
         setOverflowActive(checkOverflow(textRef.current));
       }
-    }, [tooltipVisible, setOverflowActive]);
+    }, [tooltipVisible, textRef.current, setOverflowActive]);
 
     React.useEffect(() => {
       if (!localVal && value) {
@@ -245,6 +319,7 @@ export const EditMode = React.forwardRef<HTMLInputElement, EditModeProps>(
       }
     };
     const editDimension = dimension === 'xxl' ? 'xl' : dimension;
+
     return (
       <Wrapper
         data-dimension={`${dimension}${bold && editDimension !== 'xl' ? '-bold' : ''}`}
@@ -285,13 +360,20 @@ export const EditMode = React.forwardRef<HTMLInputElement, EditModeProps>(
           )
         ) : (
           <>
-            <Text ref={refSetter(textRef, setNode)} onClick={!props.readOnly ? enableEdit : undefined}>
+            <Text
+              className={'my-text-class'}
+              ref={refSetter(textRef, setNode)}
+              $multiline={multilineView}
+              onClick={!props.readOnly ? enableEdit : undefined}
+            >
               {value}
             </Text>
             {showTooltip && tooltipVisible && overflowActive && (
               <Tooltip renderContent={() => value} targetRef={textRef} />
             )}
-            {!props.readOnly && <EditIcon height={iconSize} width={iconSize} onClick={enableEdit} />}
+            {!props.readOnly && (
+              <EditIcon $multiline={multilineView} height={iconSize} width={iconSize} onClick={enableEdit} />
+            )}
           </>
         )}
       </Wrapper>
