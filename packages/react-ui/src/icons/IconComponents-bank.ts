6f0bc0f4c5bdbcfb259807729a308f75d3c84178
Andrey Litvinov <drune.email@gmail.com>
2023-04-17 14:08:07 +0300

    chore: icons dependency update (require `npm upgrade` after merge in local brunch)

---
+++ packages/react-ui/src/icons/IconComponents-bank.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as AlphaBank } from '@admiral-ds/icons/build/bank/AlphaBank.svg';
+export { ReactComponent as Raif } from '@admiral-ds/icons/build/bank/Raif.svg';
+export { ReactComponent as SBP } from '@admiral-ds/icons/build/bank/SBP.svg';
+export { ReactComponent as Sber } from '@admiral-ds/icons/build/bank/Sber.svg';
