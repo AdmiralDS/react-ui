deffb607f56344377a0cdb42984a97afc86ad5f9
Larisa Markova <lovemirror@rambler.ru>
2023-05-02 12:51:46 +0300

    Move to mixin

--- packages/react-ui/src/components/CalendarTry/CalendarContent/DayCell.tsx
+++ packages/react-ui/src/components/CalendarTry/CalendarContent/DayCell.tsx
@@ -3,7 +3,7 @@ import type { MouseEvent } from 'react';
 import dayjs from 'dayjs';
 import type { Dayjs } from 'dayjs';
 import type { DefaultTheme, FlattenInterpolation, ThemeProps } from 'styled-components';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { typography } from '#src/components/Typography';
 import { mediumGroupBorderRadius } from '#src/components/themes';
 
@@ -23,6 +23,58 @@ export interface DayCellProps {
   highlightSpecialDay?: (date: Dayjs) => FlattenInterpolation<ThemeProps<DefaultTheme>> | undefined;
   onMouseEnter: (date: Dayjs, event: MouseEvent<HTMLDivElement>) => void;
 }
+
+const rangeHighlightMixin = css<{
+  disabled?: boolean;
+  inRange?: boolean;
+  inSelectingRange?: boolean;
+  isWeekStart: boolean;
+  isWeekEnd: boolean;
+  isRangeStart: boolean;
+  isRangeEnd: boolean;
+}>`
+  ${({ disabled, inRange, inSelectingRange, isRangeStart, isRangeEnd, isWeekStart, isWeekEnd, theme }) =>
+    !disabled &&
+    (inRange || inSelectingRange) &&
+    `
+      &:before {
+        z-index: -1;
+        content: '';
+        position: absolute;
+        width: 100%;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        background: ${inSelectingRange ? theme.color['Opacity/Focus'] : theme.color['Opacity/Hover']};
+        ${
+          (isRangeStart && isWeekStart) || (isRangeStart && isWeekEnd) || (isRangeStart && !isWeekEnd && !isWeekStart)
+            ? `border-top-left-radius: 50%;
+               border-bottom-left-radius: 50%;`
+            : ''
+        };
+        ${
+          (isRangeEnd && isWeekEnd) || (isRangeEnd && isWeekStart) || (isRangeEnd && !isWeekEnd && !isWeekStart)
+            ? `border-top-right-radius: 50%;
+               border-bottom-right-radius: 50%;`
+            : ''
+        };        
+        
+        ${
+          isWeekStart && !isRangeStart
+            ? `border-top-left-radius: ${mediumGroupBorderRadius(theme.shape)};
+               border-bottom-left-radius: ${mediumGroupBorderRadius(theme.shape)};`
+            : ''
+        };
+        ${
+          isWeekEnd && !isRangeEnd
+            ? `border-top-right-radius: ${mediumGroupBorderRadius(theme.shape)};
+               border-bottom-right-radius: ${mediumGroupBorderRadius(theme.shape)};`
+            : ''
+        };
+      }
+    `}
+`;
 
 export const DayCellWrapper = styled.div<{
   today?: boolean;
@@ -113,47 +165,7 @@ export const DayCellWrapper = styled.div<{
     `}
 
   // подсветка диапазона
-  ${({ disabled, inRange, inSelectingRange, theme, isWeekStart, isWeekEnd, isRangeStart, isRangeEnd }) =>
-    !disabled &&
-    (inRange || inSelectingRange) &&
-    `
-      &:before {
-        z-index: -1;
-        content: '';
-        position: absolute;
-        width: 100%;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        background: ${inSelectingRange ? theme.color['Opacity/Focus'] : theme.color['Opacity/Hover']};
-        ${
-          (isRangeStart && isWeekStart) || (isRangeStart && isWeekEnd) || (isRangeStart && !isWeekEnd && !isWeekStart)
-            ? `border-top-left-radius: 50%;
-               border-bottom-left-radius: 50%;`
-            : ''
-        };
-        ${
-          (isRangeEnd && isWeekEnd) || (isRangeEnd && isWeekStart) || (isRangeEnd && !isWeekEnd && !isWeekStart)
-            ? `border-top-right-radius: 50%;
-               border-bottom-right-radius: 50%;`
-            : ''
-        };        
-        
-        ${
-          isWeekStart && !isRangeStart
-            ? `border-top-left-radius: ${mediumGroupBorderRadius(theme.shape)};
-               border-bottom-left-radius: ${mediumGroupBorderRadius(theme.shape)};`
-            : ''
-        };
-        ${
-          isWeekEnd && !isRangeEnd
-            ? `border-top-right-radius: ${mediumGroupBorderRadius(theme.shape)};
-               border-bottom-right-radius: ${mediumGroupBorderRadius(theme.shape)};`
-            : ''
-        };
-      }
-    `}
+  ${rangeHighlightMixin}
 `;
 
 export const DayCell = ({
