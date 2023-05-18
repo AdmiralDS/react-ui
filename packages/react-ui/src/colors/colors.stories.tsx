bc59e4912d2e9fcdc8e3e74e1c97ecdd844f6880
Kutepova Ekaterina <eovetelina@gmail.com>
2023-05-18 15:11:01 +0300

    feat(Colors): add colors section in storybook

--- packages/react-ui/src/colors/colors.stories.tsx
+++ packages/react-ui/src/colors/colors.stories.tsx
@@ -1,66 +1,46 @@
-import { TooltipHoc } from '#src/components/TooltipHOC';
-import { typography } from '#src/components/Typography';
 import { withDesign } from 'storybook-addon-designs';
 import * as React from 'react';
 import type { Meta, Story } from '@storybook/react';
+import { useDarkMode } from 'storybook-dark-mode';
 import styled, { ThemeContext, ThemeProvider } from 'styled-components';
 import { ReactComponent as CopyOutline } from '@admiral-ds/icons/build/documents/CopyOutline.svg';
-import { COLOR } from '#src/components/themes/light/color';
-import type { Color } from '../components/themes/common';
+import { TooltipHoc } from '#src/components/TooltipHOC';
+import { typography } from '#src/components/Typography';
+import type { Color } from '#src/components/themes/common';
 import { LIGHT_THEME, DARK_THEME } from '#src/components/themes';
-import { COLORS, VERSIONS_ROWS, VERSION_COLUMNS } from './colorsDescription';
-import { useDarkMode } from 'storybook-dark-mode';
-import { Table } from '..';
+import { Table } from '#src/components/Table';
+import { COLORS, COLORS_DESC, VERSIONS_ROWS, VERSION_COLUMNS, ColorCircle } from './colorsDescription';
 
+const Wrapper = styled.div`
+  min-width: fit-content;
+`;
 const Title = styled.div`
   ${typography['Header/H4']}
-  color: ${({ theme }) => theme.color['Neutral/Neutral 90']};
+`;
+const SubTitle = styled.div`
   margin-bottom: 20px;
+  max-width: 800px;
 `;
-
-const IconsWrapper = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  gap: 8px;
-  background: ${({ theme }) => theme.color['Neutral/Neutral 05']};
-  padding: 32px 0;
-  border-radius: 8px;
-  & svg {
-    flex-shrink: 0;
-    *[fill^='#62'],
-    *[fill^='#2B'] {
-      fill: ${({ theme }) => theme.color['Neutral/Neutral 50']};
-    }
-  }
-  justify-content: center;
-`;
-
 const CategoryWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 40px;
-`;
-
-const CopyOutlineWrapper = styled.div`
-  display: inline-flex;
-  cursor: pointer;
-`;
-
-const ColorRow = styled.div`
-  display: flex;
-`;
-
-const ColorComponent = styled.div`
-  display: flex;
-  flex: 0 0 auto;
-  width: 800px;
-  box-sizing: border-box;
-  padding: 40px 32px;
+  padding-bottom: 40px;
+  background: ${({ theme }) => theme.color['Neutral/Neutral 00']};
   color: ${({ theme }) => theme.color['Neutral/Neutral 90']};
   ${typography['Body/Body 2 Long']}
-  background: ${({ theme }) => theme.color['Neutral/Neutral 00']};
 `;
-
+const ColorRow = styled.div`
+  display: flex;
+  & > * {
+    background: ${({ theme }) => theme.color['Neutral/Neutral 00']};
+    color: ${({ theme }) => theme.color['Neutral/Neutral 90']};
+  }
+`;
+const ColorComponent = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  padding: 40px 32px;
+`;
 const ColorDescription = styled.div`
   display: flex;
   flex: 0 0 auto;
@@ -68,22 +48,19 @@ const ColorDescription = styled.div`
   width: 280px;
   margin-right: 104px;
 `;
-
 const ColorProps = styled.div`
   color: ${({ theme }) => theme.color['Neutral/Neutral 50']};
   margin-right: 16px;
   white-space: nowrap;
 `;
-
-const ColorCircle = styled.div<{ $color: keyof Color; border?: boolean }>`
+const ColorProp = styled.div`
   display: flex;
-  flex: 0 0 auto;
-  width: 56px;
-  height: 56px;
-  border-radius: 50%;
-  background: ${({ theme, $color }) => theme.color[$color]};
-  ${({ border, theme }) => border && `border: 1px solid ${theme.color['Neutral/Neutral 20']};`}
-  margin-right: 48px;
+  gap: 8px;
+  align-items: center;
+`;
+const CopyOutlineWrapper = styled.div`
+  display: inline-flex;
+  cursor: pointer;
 `;
 
 const CopyIcon = React.forwardRef<HTMLDivElement, { text: string }>(({ text }, ref) => {
@@ -106,86 +83,89 @@ const CopyIcon = React.forwardRef<HTMLDivElement, { text: string }>(({ text }, r
 });
 const CopyButton = TooltipHoc(CopyIcon);
 
-const ColorProperties = () => (
-  <ColorProps>
-    Token:
-    <br />
-    Name:
-    <br />
-    Hex:
-    <br />
-    Contrast:
-  </ColorProps>
-);
-
 const Category = ({ categoryName }: { categoryName: string }) => {
   const theme = React.useContext(ThemeContext) || LIGHT_THEME;
-  const colors = getColors(categoryName);
+  const colors = [...Object.keys(COLORS[categoryName])] as Array<keyof Color>;
   return (
     <CategoryWrapper>
-      <Title>{categoryName}</Title>
+      <Title>{categoryName == 'Opacity' ? 'Overlay opacity' : categoryName}</Title>
+      <SubTitle>{COLORS_DESC[categoryName]}</SubTitle>
       {colors.map((colorName: keyof Color) => {
+        const contrastMode = useDarkMode() ? 'dark' : 'light';
         return (
-          <ColorRow>
-            <ColorComponent>
-              <ColorCircle
-                $color={colorName}
-                border={theme.color[colorName] === '#FFFFFF' || theme.color[colorName] === '#121316'}
-              />
-              <ColorDescription>{COLORS[categoryName][colorName].description}</ColorDescription>
-              <ColorProps>
-                Token:
-                <br />
-                Name:
-                <br />
-                Hex:
-                <br />
-                Contrast:
-              </ColorProps>
-              <ColorProps>
-                {colorName}
-                <br />
-                {COLORS[categoryName][colorName].name}
-                <br />
-                {theme.color[colorName]}
-                <br />
-                {COLORS[categoryName][colorName].contrast}
-              </ColorProps>
-            </ColorComponent>
-          </ColorRow>
+          <ThemeProvider
+            theme={
+              colorName === 'Opacity/Dark Static Hover' ||
+              colorName === 'Opacity/Dark Static Focus' ||
+              colorName === 'Opacity/Dark Static Press'
+                ? DARK_THEME
+                : theme
+            }
+            key={colorName}
+          >
+            <ColorRow>
+              <ColorComponent>
+                <ColorCircle
+                  $size={56}
+                  $color={colorName}
+                  border={theme.color[colorName] === '#FFFFFF' || theme.color[colorName] === '#121316'}
+                  style={{ marginRight: '48px' }}
+                />
+                <ColorDescription>{COLORS[categoryName][colorName].description}</ColorDescription>
+                <ColorProps>
+                  <ColorProp>Token:</ColorProp>
+                  <ColorProp>Name:</ColorProp>
+                  <ColorProp>Hex:</ColorProp>
+                  <ColorProp>Contrast:</ColorProp>
+                </ColorProps>
+                <ColorProps style={{ minWidth: '200px' }}>
+                  <ColorProp>
+                    {colorName}
+                    <CopyButton renderContent={() => 'Копировать цветовой токен'} text={colorName} />
+                  </ColorProp>
+                  <ColorProp>{COLORS[categoryName][colorName].name}</ColorProp>
+                  <ColorProp>{theme.color[colorName]}</ColorProp>
+                  <ColorProp>{COLORS[categoryName][colorName].contrast[contrastMode]}</ColorProp>
+                </ColorProps>
+              </ColorComponent>
+            </ColorRow>
+          </ThemeProvider>
         );
       })}
     </CategoryWrapper>
   );
 };
 
-const getColorCategories = () => {
-  const result = Object.keys(COLOR).map(function (item) {
-    const slashIndex = item.indexOf('/');
-    const categoryName = item.slice(0, slashIndex);
-    return categoryName;
-  });
-  return [...new Set(result)];
-};
+const Desc = styled.div`
+  ${typography['Body/Body 1 Long']}
+`;
+const Separator = styled.div`
+  height: 20px;
+`;
 
-const getColors = (match: string): any => {
-  return Object.keys(COLOR).filter(function (item) {
-    return item.indexOf(match) === 0;
-  });
-};
-
-const CATEGORIES = getColorCategories();
+const Description = () => (
+  <Desc>
+    В данной таблице представлена вся цветовая палитра дизайн-системы Адмирал 2.1. Все цвета делятся на следующие
+    группы: Базовые (Neutral и Primary), Специальные (статичные цвета), Status (статустные цвета), Opacity (прозрачности
+    оверлеев) и Additional Colors (цвета для чартов и графики). В таблице обозначены наиболее частые сценарии
+    использования оттенков. Цвета с редким использованием могут не использоваться в компонентах, но присутствуют в
+    палитре для возможного использования в кастомных решениях, будущих компонентах или графики.
+    <Separator />
+    Цветовая палитра дизайн-системы Адмирал состоит из двух тем – Light и Dark. Для переключения тем используйте иконку
+    месяца в хедере страницы.
+  </Desc>
+);
 
 export default {
   title: 'Colors/Colors',
   decorators: [withDesign],
-  component: CopyOutline,
   parameters: {
     docs: {
       source: {
         code: null,
       },
     },
+    componentSubtitle: <Description />,
     design: [
       {
         type: 'figma',
@@ -196,24 +176,19 @@ export default {
 } as Meta;
 
 const Template: Story = () => (
-  <ThemeProvider theme={useDarkMode() ? DARK_THEME : LIGHT_THEME}>
-    <IconsWrapper>В описании цвета указаны наиболее частые сценарии его использования</IconsWrapper>
-    {CATEGORIES.map((item) => {
+  <Wrapper>
+    {[...Object.keys(COLORS)].map((item) => {
       return <Category key={item} categoryName={item} />;
     })}
-  </ThemeProvider>
+  </Wrapper>
 );
 
-const Template2: Story = () => (
-  <>
-    <Table rowList={VERSIONS_ROWS} columnList={VERSION_COLUMNS} greyHeader />
-  </>
-);
+const Template2: Story = () => <Table rowList={VERSIONS_ROWS} columnList={VERSION_COLUMNS} greyHeader />;
 
-export const ColorsThemes = Template.bind({});
-ColorsThemes.args = {};
-ColorsThemes.storyName = 'Список цветов';
+export const Colors = Template.bind({});
+Colors.args = {};
+Colors.storyName = 'Группы цветов и их назначение';
 
-export const ColorsThemes2 = Template2.bind({});
-ColorsThemes2.args = {};
-ColorsThemes2.storyName = 'Сопоставление стилей цветов 2.0 и 2.1';
+export const Themes = Template2.bind({});
+Themes.args = {};
+Themes.storyName = 'Сопоставление стилей цветов 2.0 и 2.1';
