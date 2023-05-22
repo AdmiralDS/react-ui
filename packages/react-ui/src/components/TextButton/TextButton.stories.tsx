54f454dbed5d54a746ff57eedc23ed2c7cd0ac37
Larisa Markova <lovemirror@rambler.ru>
2023-05-22 10:58:35 +0300

    docs(Storybook): change imports in stories for TextButton component

--- packages/react-ui/src/components/TextButton/TextButton.stories.tsx
+++ packages/react-ui/src/components/TextButton/TextButton.stories.tsx
@@ -3,10 +3,8 @@ import styled, { ThemeProvider } from 'styled-components';
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import { withDesign } from 'storybook-addon-designs';
 import { ReactComponent as AttachFileOutline } from '@admiral-ds/icons/build/system/AttachFileOutline.svg';
-import { TextButton } from './index';
-import { T } from '#src/components/T';
-import type { Theme } from '#src/components/themes';
-import { ALL_BORDER_RADIUS_VALUES } from '#src/components/themes/borderRadius';
+import { TextButton, T, ALL_BORDER_RADIUS_VALUES } from '@admiral-ds/react-ui';
+import type { Theme } from '@admiral-ds/react-ui';
 import { cleanUpProps } from '#src/components/common/utils/cleanUpStoriesProps';
 
 const StyledText = styled(T)`
