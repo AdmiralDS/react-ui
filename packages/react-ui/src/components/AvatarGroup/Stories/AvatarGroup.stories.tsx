722676983fc74b0f607cb2817e311d1e3364a060
Larisa Markova <lovemirror@rambler.ru>
2023-06-06 10:19:28 +0300

    docs(Storybook): refactor AvatarGroupWidth story in AvatarGroup component

--- packages/react-ui/src/components/AvatarGroup/Stories/AvatarGroup.stories.tsx
+++ packages/react-ui/src/components/AvatarGroup/Stories/AvatarGroup.stories.tsx
@@ -3,24 +3,21 @@ import * as React from 'react';
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import styled from 'styled-components';
 import type { AvatarGroupProps } from '@admiral-ds/react-ui';
-import { AvatarGroup, DefaultFontColorName } from '@admiral-ds/react-ui';
+import { AvatarGroup } from '@admiral-ds/react-ui';
 import { ReactComponent as PersonSolid } from '@admiral-ds/icons/build/system/PersonSolid.svg';
-import { cleanUpProps } from '#src/components/common/utils/cleanUpStoriesProps';
 
+import { AvatarGroupWidthTemplate } from './Templates';
+
+import { cleanUpProps } from '#src/components/common/utils/cleanUpStoriesProps';
 const imageURL = 'https://github.com/DrUNE.png?size=100';
+
+// Imports of text sources
+import AvatarGroupWidthRaw from '!!raw-loader!./Templates/AvatarGroupWidth';
 
 const Desc = styled.div`
   font-family: 'VTB Group UI';
   font-size: 16px;
   line-height: 24px;
-`;
-
-const Text = styled.div`
-  font-family: 'VTB Group UI';
-  font-size: 16px;
-  line-height: 24px;
-  margin-bottom: 8px;
-  color: ${({ theme }) => theme.color[DefaultFontColorName]};
 `;
 
 export default {
@@ -109,74 +106,23 @@ const Template: ComponentStory<typeof AvatarGroup> = ({ onAvatarSelect, ...args 
   );
 };
 
-const items2: AvatarGroupProps['items'] = [
-  { userName: 'Lena Ivanova', icon: <PersonSolid />, id: '1' },
-  { userName: 'Petr Lesov', icon: <PersonSolid />, id: '2' },
-  { userName: 'Agata Petrova', icon: <PersonSolid />, id: '3' },
-  { userName: 'Arina Leskova', icon: <PersonSolid />, id: '4' },
-  { userName: 'Rita', appearance: 'dark', id: '5' },
-  { userName: 'Drune', icon: <PersonSolid />, href: imageURL, id: '6' },
-  { userName: 'Lisa Kotova', icon: <PersonSolid />, id: '7' },
-  { userName: 'Ирина Глушко', icon: <PersonSolid />, id: '8' },
-  { userName: 'Rosa Farrel', icon: <PersonSolid />, id: '9' },
-  { userName: 'Tom Hidlton', appearance: 'dark', id: '10' },
-];
-
-const items3: AvatarGroupProps['items'] = [
-  {
-    userName: 'Lena Ivanova',
-    icon: <PersonSolid />,
-    id: '1',
-    appearance: { background: '#3F7DFE', icon: '#001157' },
-  },
-  { userName: 'Petr Lesov', icon: <PersonSolid />, id: '2' },
-  { userName: 'Agata Petrova', id: '3', appearance: { background: '#D92020', text: '#FFFFFF' } },
-  { userName: 'Arina Leskova', icon: <PersonSolid />, id: '4' },
-  { userName: 'Rita', appearance: 'light', id: '5' },
-  { userName: 'Drune', icon: <PersonSolid />, href: imageURL, id: '6' },
-  { userName: 'Lisa Kotova', icon: <PersonSolid />, id: '7' },
-  { userName: 'Ирина Глушко', icon: <PersonSolid />, id: '8' },
-  { userName: 'Rosa Farrel', icon: <PersonSolid />, id: '9' },
-  { userName: 'Tom Hidlton', appearance: 'light', id: '10' },
-];
-
-const Template2: ComponentStory<typeof AvatarGroup> = (props) => {
-  const clearProps = cleanUpProps(props);
-
-  return (
-    <>
-      <Text>Пример AvatarGroup с единым для всех аватаров внешним видом (appearance)</Text>
-      <AvatarGroup
-        style={{ width: '300px' }}
-        items={items2}
-        onAvatarSelect={onSelectAvatar}
-        dropContainerClassName="dropContainerClass"
-        {...clearProps}
-      />
-      <div style={{ height: '40px' }} />
-      <Text>Пример AvatarGroup с различными по внешнему виду (appearance) аватарами</Text>
-      <AvatarGroup
-        style={{ width: '300px' }}
-        items={items3}
-        onAvatarSelect={onSelectAvatar}
-        appearance="dark"
-        dropContainerClassName="dropContainerClass"
-        dropContainerStyle={{ width: '250px' }}
-        {...clearProps}
-      />
-    </>
-  );
-};
-
 export const Playground = Template.bind({});
 Playground.args = {};
 Playground.storyName = 'Playground';
 
-export const Width = Template2.bind({});
-Width.args = {};
-Width.storyName = 'Пример с ограниченной шириной';
-Width.parameters = {
+//<editor-fold desc="Пример с ограниченной шириной">
+const AvatarGroupWidthStory: ComponentStory<typeof AvatarGroup> = (props) => (
+  <AvatarGroupWidthTemplate items={[]} {...cleanUpProps(props)} />
+);
+
+export const AvatarGroupWidth = AvatarGroupWidthStory.bind({});
+AvatarGroupWidth.args = {};
+AvatarGroupWidth.storyName = 'Пример с ограниченной шириной';
+AvatarGroupWidth.parameters = {
   docs: {
+    source: {
+      code: AvatarGroupWidthRaw,
+    },
     description: {
       story: `При достижении условного максимума отображаемых аватаров, последним ставится аватар с отображением количества
       скрытых элементов. Внешний вид такого аватара (цвет заливки и текста) контролируется с помощью параметра appearance, переданного в 
@@ -187,3 +133,4 @@ Width.parameters = {
     },
   },
 };
+//</editor-fold>
