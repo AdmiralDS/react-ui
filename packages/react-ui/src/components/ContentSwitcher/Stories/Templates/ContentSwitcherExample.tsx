c53a1d1f164c149e4242896a1d83722b0fc04201
Larisa Markova <lovemirror@rambler.ru>
2023-06-07 09:47:36 +0300

    docs(Storybook): refactor stories in ContentSwitcher component

---
+++ packages/react-ui/src/components/ContentSwitcher/Stories/Templates/ContentSwitcherExample.tsx
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+import { ContentSwitcher, ContentSwitcherItem, T } from '@admiral-ds/react-ui';
+
+const Separator = styled.div`
+  height: 20px;
+`;
+
+const MobileWrapper = styled.div`
+  width: 320px;
+  height: 100px;
+  padding-top: 16px;
+  box-sizing: border-box;
+  border: ${(p) => `8px solid ${p.theme.color['Neutral/Neutral 50']}`};
+  border-bottom-style: none;
+  background-color: ${(p) => p.theme.color['Neutral/Neutral 00']};
+  border-top-left-radius: 24px;
+  border-top-right-radius: 24px;
+`;
+
+const SwitcherWrapper = styled.div`
+  overflow-x: auto;
+  width: 100%;
+  padding: 0 8px;
+  box-sizing: border-box;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const list = [
+  { title: 'Active button', content: '1' },
+  { title: 'Default button', content: '2' },
+  { title: 'Disabled button', content: '3', disabled: true },
+];
+
+export const ContentSwitcherExampleTemplate = () => {
+  return (
+    <>
+      <T font="Body/Body 1 Long" as="div">
+        Dimension - l
+      </T>
+      <Separator />
+      <ContentSwitcher dimension={'l'}>
+        {list.map((item, index) => {
+          return (
+            <ContentSwitcherItem key={index} active={index === 0} disabled={item.disabled}>
+              {item.title}
+            </ContentSwitcherItem>
+          );
+        })}
+      </ContentSwitcher>
+      <Separator />
+      <ContentSwitcher dimension={'l'} appearance="primary">
+        {list.map((item, index) => {
+          return (
+            <ContentSwitcherItem key={index} active={index === 0} disabled={item.disabled}>
+              {item.title}
+            </ContentSwitcherItem>
+          );
+        })}
+      </ContentSwitcher>
+      <Separator />
+      <T font="Body/Body 1 Long" as="div">
+        Dimension - m
+      </T>
+      <Separator />
+      <ContentSwitcher dimension={'m'}>
+        {list.map((item, index) => {
+          return (
+            <ContentSwitcherItem key={index} active={index === 0} disabled={item.disabled}>
+              {item.title}
+            </ContentSwitcherItem>
+          );
+        })}
+      </ContentSwitcher>
+      <Separator />
+      <T font="Body/Body 1 Long" as="div">
+        Dimension - s
+      </T>
+      <Separator />
+      <ContentSwitcher dimension={'s'}>
+        {list.map((item, index) => {
+          return (
+            <ContentSwitcherItem key={index} active={index === 0} disabled={item.disabled}>
+              {item.title}
+            </ContentSwitcherItem>
+          );
+        })}
+      </ContentSwitcher>
+      <Separator />
+      <T font="Body/Body 1 Long" as="div">
+        Mobile Adaptive
+      </T>
+      <MobileWrapper>
+        <SwitcherWrapper>
+          <ContentSwitcher dimension={'m'}>
+            {list.map((item, index) => {
+              return (
+                <ContentSwitcherItem key={index} active={index === 0} disabled={item.disabled}>
+                  {item.title}
+                </ContentSwitcherItem>
+              );
+            })}
+          </ContentSwitcher>
+        </SwitcherWrapper>
+      </MobileWrapper>
+    </>
+  );
+};
---
+++ packages/react-ui/src/components/ContentSwitcher/Stories/Templates/ContentSwitcherPlayground.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+
+import { Badge, ContentSwitcher, ContentSwitcherItem } from '@admiral-ds/react-ui';
+import type { ContentSwitcherProps } from '@admiral-ds/react-ui';
+
+import { ReactComponent as StarSolid } from '@admiral-ds/icons/build/system/StarSolid.svg';
+
+const list = [
+  {
+    title: (
+      <>
+        <StarSolid width={17} style={{ marginRight: 8 }} />
+        Active button
+      </>
+    ),
+  },
+  {
+    title: (
+      <>
+        <StarSolid width={17} style={{ marginRight: 8 }} />
+        Default button <Badge style={{ marginLeft: 8 }}>6</Badge>
+      </>
+    ),
+  },
+  { title: 'Disabled button', disabled: true },
+];
+
+export const ContentSwitcherPlaygroundTemplate = ({ dimension, className, ...props }: ContentSwitcherProps) => {
+  const [active, setActive] = React.useState(0);
+
+  return (
+    <>
+      <ContentSwitcher dimension={dimension} className={className} {...props}>
+        {list.map((item, index) => (
+          <ContentSwitcherItem
+            key={index}
+            active={index === active}
+            onClick={() => setActive(index)}
+            disabled={item.disabled}
+          >
+            {item.title}
+          </ContentSwitcherItem>
+        ))}
+      </ContentSwitcher>
+    </>
+  );
+};
