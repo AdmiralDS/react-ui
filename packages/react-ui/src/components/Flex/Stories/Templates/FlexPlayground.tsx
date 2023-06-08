7cdd1af186e473d647bb136afbbccc78a5827f4f
Larisa Markova <lovemirror@rambler.ru>
2023-06-08 13:57:49 +0300

    Fix type conversion

--- packages/react-ui/src/components/Flex/Stories/Templates/FlexPlayground.tsx
+++ packages/react-ui/src/components/Flex/Stories/Templates/FlexPlayground.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { Flex, FLEX_CELL_MAX_SIZE, InputField, LIGHT_THEME, TextField } from '@admiral-ds/react-ui';
-import type { FlexContainerProps } from '@admiral-ds/react-ui';
+import type { FlexContainerProps, FlexCellSizes } from '@admiral-ds/react-ui';
 
 export const FlexPlaygroundTemplate = ({ rowGap = 20, columnGap = 10, ...props }: FlexContainerProps) => {
   return (
@@ -10,7 +10,12 @@ export const FlexPlaygroundTemplate = ({ rowGap = 20, columnGap = 10, ...props }
       <Flex.Container {...props} rowGap={rowGap} columnGap={columnGap}>
         <Flex.Row {...props} rowGap={rowGap} columnGap={columnGap}>
           {Array.from({ length: FLEX_CELL_MAX_SIZE }, (_, i) => (
-            <Flex.Cell {...props} columnGap={columnGap} key={FLEX_CELL_MAX_SIZE - i} col={FLEX_CELL_MAX_SIZE - i}>
+            <Flex.Cell
+              {...props}
+              columnGap={columnGap}
+              key={FLEX_CELL_MAX_SIZE - i}
+              col={(FLEX_CELL_MAX_SIZE - i) as FlexCellSizes}
+            >
               <InputField label={`Инпут шириной ${FLEX_CELL_MAX_SIZE - i}`} />
             </Flex.Cell>
           ))}
