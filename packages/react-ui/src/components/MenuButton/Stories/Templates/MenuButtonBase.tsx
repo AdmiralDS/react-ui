1c17393accf1c75509d17287d0a66f2ae7bc3280
Larisa Markova <lovemirror@rambler.ru>
2023-06-08 17:32:18 +0300

    docs(Storybook): refactor story in MenuButton component

---
+++ packages/react-ui/src/components/MenuButton/Stories/Templates/MenuButtonBase.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { MenuButton, MenuItem } from '@admiral-ds/react-ui';
+import type { MenuButtonProps, RenderOptionProps, Theme } from '@admiral-ds/react-ui';
+
+const handleVisibilityChange = (isVisible: boolean) => {
+  if (isVisible) {
+    console.log('Menu opened');
+  } else {
+    console.log('Menu closed');
+  }
+};
+
+const logSelectedId = (id: string) => {
+  console.log(`selected: ${id}`);
+};
+
+const items = [
+  {
+    id: '1',
+    display: 'Option one',
+  },
+  {
+    id: '2',
+    display: 'Option two',
+  },
+  {
+    id: '3',
+    display: 'Option three',
+  },
+  {
+    id: '4',
+    display: 'Option four',
+  },
+  {
+    id: '5',
+    display: 'Option five',
+    disabled: true,
+  },
+  {
+    id: '6',
+    display: 'Option six',
+  },
+  {
+    id: '7',
+    display: 'Option seven',
+  },
+];
+
+export const MenuButtonBaseTemplate = (props: MenuButtonProps) => {
+  const [selected, setSelected] = React.useState<string | undefined>(undefined);
+
+  function swapBorder(theme: Theme): Theme {
+    theme.shape.borderRadiusKind = (props as any).themeBorderKind || theme.shape.borderRadiusKind;
+    return theme;
+  }
+
+  const model = React.useMemo(() => {
+    return items.map((item) => ({
+      id: item.id,
+      render: (items: RenderOptionProps) => (
+        <MenuItem dimension={props.dimension === 'xl' ? 'l' : props.dimension} {...items} key={item.id}>
+          {item.display}
+        </MenuItem>
+      ),
+      disabled: item.disabled,
+    }));
+  }, [props.dimension]);
+
+  return (
+    <ThemeProvider theme={swapBorder}>
+      <MenuButton
+        {...props}
+        selected={selected}
+        onSelectItem={(id) => {
+          logSelectedId(id);
+          setSelected(id);
+        }}
+        items={model}
+        onVisibilityChange={handleVisibilityChange}
+        data-dropdown-container-id="menu-button-with-dropdown"
+        className="menu-button-class"
+        dropContainerClassName="dropContainerClass"
+      >
+        test
+      </MenuButton>
+    </ThemeProvider>
+  );
+};
