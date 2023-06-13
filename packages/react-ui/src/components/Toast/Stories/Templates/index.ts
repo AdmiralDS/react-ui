439bfb73eefa8cab8b61e89ead0f0b1c883dc785
Larisa Markova <lovemirror@rambler.ru>
2023-06-13 20:32:22 +0300

    docs(Storybook): refactor stories in Toast component

---
+++ packages/react-ui/src/components/Toast/Stories/Templates/index.ts
@@ -0,0 +1,6 @@
+export * from './ToastCustomComponent';
+export * from './ToastLineNotification';
+export * from './ToastBackwardCompatibility';
+export * from './ToastNotification';
+export * from './ToastProgressComponent';
+export * from './ToastNotificationBase';
