1467d3ae4b9f5554f08cb633098e9749046c1d24
Larisa Markova <lovemirror@rambler.ru>
2023-06-15 14:03:11 +0300

    docs(Storybook): refactor stories in SuggestInput component

---
+++ packages/react-ui/src/components/input/SuggestInput/Stories/Templates/SuggestInputFilter.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import type { ChangeEvent } from 'react';
+
+import { getTextHighlightMeta, SuggestInput } from '@admiral-ds/react-ui';
+import type { SuggestInputProps } from '@admiral-ds/react-ui';
+
+const OPTIONS = [
+  'text 1',
+  'text 2 text text 2 text text 2 text text 2 text text 2 text text 2 text text 2 text ',
+  'text 3',
+  'text 4',
+  'text 5',
+  'text 6',
+  'text 7',
+  'text 8 text text 2 text text 2 text text 2 text text 2 text text 2 text text 2 text ',
+  'text 9',
+  'text 10',
+  'text 11',
+  'text 12',
+];
+
+export const SuggestInputFilterTemplate = (props: SuggestInputProps) => {
+  const [localValue, setValue] = React.useState<string>(props.value ? String(props.value) : '');
+  const [options, setOptions] = React.useState<string[] | undefined>([...OPTIONS]);
+
+  const handleSelectOption = (option: string) => {
+    setValue(option);
+    console.log(`Selected option - ${option}`);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.currentTarget.value;
+
+    setValue(inputValue);
+    props.onChange?.(e);
+  };
+
+  React.useEffect(() => {
+    const filteredOptions: string[] = OPTIONS.filter(
+      (option) => getTextHighlightMeta(option, localValue, 'wholly').shouldHighlight,
+    );
+    setOptions(filteredOptions);
+  }, [localValue]);
+
+  return (
+    <SuggestInput
+      className="suggest"
+      {...props}
+      value={localValue}
+      onInput={handleChange}
+      onOptionSelect={handleSelectOption}
+      options={options}
+      onSearchButtonClick={() => {
+        console.log('search button click');
+      }}
+      displayClearIcon
+    />
+  );
+};
---
+++ packages/react-ui/src/components/input/SuggestInput/Stories/Templates/SuggestInputPlayground.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import type { ChangeEvent } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { SuggestInput } from '@admiral-ds/react-ui';
+import type { SuggestInputProps, Theme } from '@admiral-ds/react-ui';
+
+const OPTIONS = [
+  'text 1',
+  'text 2 text text 2 text text 2 text text 2 text text 2 text text 2 text text 2 text ',
+  'text 3',
+  'text 4',
+  'text 5',
+  'text 6',
+  'text 7',
+  'text 8 text text 2 text text 2 text text 2 text text 2 text text 2 text text 2 text ',
+  'text 9',
+  'text 10',
+  'text 11',
+  'text 12',
+];
+
+export const SuggestInputPlaygroundTemplate = ({
+  placeholder = 'Начните набирать text',
+  ...props
+}: SuggestInputProps) => {
+  const [localValue, setValue] = React.useState<string>(props.value ? String(props.value) : '');
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [options, setOptions] = React.useState<string[] | undefined>();
+
+  const handleSelectOption = (option: string) => {
+    setValue(option);
+    console.log(`Selected option - ${option}`);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.currentTarget.value;
+
+    // Если в инпуте больше 3х символов производим запрос на поиск совпадений
+    if (localValue?.length < 3 && inputValue?.length > 2) {
+      setIsLoading(true);
+      setOptions([]);
+    }
+    setValue(inputValue);
+    props.onChange?.(e);
+  };
+
+  // Имитация запросов на бакэнд
+  React.useEffect(() => {
+    if (isLoading) {
+      const timeout = window.setTimeout(() => {
+        setIsLoading(false);
+        setOptions([...OPTIONS]);
+      }, 3000);
+      return () => {
+        window.clearTimeout(timeout);
+      };
+    }
+  }, [isLoading]);
+
+  function swapBorder(theme: Theme): Theme {
+    theme.shape.borderRadiusKind = (props as any).themeBorderKind || theme.shape.borderRadiusKind;
+    return theme;
+  }
+
+  return (
+    <ThemeProvider theme={swapBorder}>
+      <SuggestInput
+        className="suggest"
+        {...props}
+        value={localValue}
+        onInput={handleChange}
+        onOptionSelect={handleSelectOption}
+        options={options}
+        isLoading={isLoading}
+        onSearchButtonClick={() => {
+          console.log('search button click');
+        }}
+        displayClearIcon
+        dropContainerClassName="dropContainerClass"
+        placeholder={placeholder}
+      />
+    </ThemeProvider>
+  );
+};
