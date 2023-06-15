3e294781d4f1cefc823e3c06f53e474d48044b29
Larisa Markova <lovemirror@rambler.ru>
2023-06-15 15:14:54 +0300

    docs(Storybook): refactor stories in TimeInput component

---
+++ packages/react-ui/src/components/input/TimeInput/Stories/Templates/index.ts
@@ -0,0 +1,5 @@
+export * from './TimeInputRange';
+export * from './TimeInputRangeDisabled';
+export * from './TimeInputIconAlternative';
+export * from './TimeInputSimple';
+export * from './TimeInputSimpleDisabled';
