2a58a7c67db72dbbc58b4d147c652525ca64d292
Larisa Markova <lovemirror@rambler.ru>
2023-06-16 06:02:48 +0300

    docs(Storybook): refactor stories in PhoneInputField component

--- packages/react-ui/src/components/form/PhoneInputField/Stories/PhoneInputField.stories.tsx
+++ packages/react-ui/src/components/form/PhoneInputField/Stories/PhoneInputField.stories.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import type { ChangeEvent } from 'react';
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import { withDesign } from 'storybook-addon-designs';
-import { ThemeProvider } from 'styled-components';
+
 import { PhoneInputField, INPUT_DIMENSIONS_VALUES, ALL_BORDER_RADIUS_VALUES } from '@admiral-ds/react-ui';
-import type { Theme } from '@admiral-ds/react-ui';
+
+import { PhoneInputFieldExampleTemplate } from '#src/components/form/PhoneInputField/Stories/Templates';
+import { cleanUpProps } from '#src/components/common/utils/cleanUpStoriesProps';
 import { DataAttributesDescription } from '#src/components/form/common';
+
+// Imports of text sources
+import PhoneInputFieldExampleRaw from '!!raw-loader!./Templates/PhoneInputFieldExample';
 
 export default {
   title: 'Admiral-2.1/Form Field/PhoneInputField',
@@ -72,50 +76,18 @@ export default {
   },
 } as ComponentMeta<typeof PhoneInputField>;
 
-const Template: ComponentStory<typeof PhoneInputField> = (props) => {
-  const cleanProps = (Object.keys(props) as Array<keyof typeof props>).reduce((acc, key) => {
-    if (props[key] !== undefined) acc[key] = props[key];
+//<editor-fold desc="PhoneInputField example">
+const PhoneInputFieldExampleStory: ComponentStory<typeof PhoneInputField> = (props) => (
+  <PhoneInputFieldExampleTemplate {...cleanUpProps(props)} />
+);
 
-    return acc;
-  }, {} as Record<any, any>);
-
-  const [localValue, setValue] = React.useState<string>(props.value ?? '');
-
-  React.useEffect(() => {
-    if (props.value !== undefined) {
-      setValue(String(props.value));
-    }
-  }, [props.value]);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.currentTarget.value;
-    setValue(inputValue);
-    props.onChange?.(e);
-  };
-
-  function swapBorder(theme: Theme): Theme {
-    theme.shape.borderRadiusKind = (props as any).themeBorderKind || theme.shape.borderRadiusKind;
-    return theme;
-  }
-
-  return (
-    <ThemeProvider theme={swapBorder}>
-      <PhoneInputField
-        data-container-id="phoneInputFieldIdOne"
-        {...cleanProps}
-        value={localValue}
-        defaultCountry="RUS"
-        onChange={handleChange}
-        dropContainerClassName="dropContainerClass"
-      />
-    </ThemeProvider>
-  );
+export const PhoneInputFieldExample = PhoneInputFieldExampleStory.bind({});
+PhoneInputFieldExample.parameters = {
+  docs: {
+    source: {
+      code: PhoneInputFieldExampleRaw,
+    },
+  },
 };
-
-export const InputFieldInput = Template.bind({});
-
-InputFieldInput.args = {
-  label: 'Введите номер телефона',
-};
-
-InputFieldInput.storyName = 'PhoneInputField example';
+PhoneInputFieldExample.storyName = 'PhoneInputField example';
+//</editor-fold>
