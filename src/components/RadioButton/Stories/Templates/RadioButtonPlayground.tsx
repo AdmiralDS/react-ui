3a201ec3b10ec6dce530fab869573c43601b5b5a
Larisa Markova <lovemirror@rambler.ru>
2023-07-11 17:30:41 +0300

    docs(RadioButton): fix story

--- src/components/RadioButton/Stories/Templates/RadioButtonPlayground.tsx
+++ src/components/RadioButton/Stories/Templates/RadioButtonPlayground.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 import { Button, RadioButton } from '@admiral-ds/react-ui';
 import type { RadioButtonProps } from '@admiral-ds/react-ui';
+import { useEffect } from 'react';
 
 const Separator = styled.div`
   height: 20px;
@@ -11,6 +12,11 @@ const Separator = styled.div`
 
 export const RadioButtonPlaygroundTemplate = ({ checked: argChecked, ...props }: RadioButtonProps) => {
   const [checked, setChecked] = React.useState(argChecked || false);
+
+  useEffect(() => {
+    setChecked(!!argChecked);
+  }, [argChecked]);
+
   return (
     <>
       <RadioButton value={1} checked={checked} onChange={(event) => setChecked(event.currentTarget.checked)} {...props}>
