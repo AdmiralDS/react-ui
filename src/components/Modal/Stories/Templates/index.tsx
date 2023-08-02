0275ba855e8e27ce6df459052170ed94190cf258
Larisa Markova <lovemirror@rambler.ru>
2023-08-02 17:10:32 +0300

    Fix stories for Modal

--- src/components/Modal/Stories/Templates/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from './ModalTwoButtons';
-export * from './ModalOneButton';
-export * from './ModalWithoutButtons';
-export * from './ModalScroll';
-export * from './ModalCustomContent';
-export * from './ModalCustomOverlay';
-export * from './ModalStatusIcon';
