df4a0cea7600855d05a10a4faf8fe8e318df9a31
Andrey Litvinov <drune.email@gmail.com>
2023-09-27 12:19:30 +0300

    chore(storybook): remove icons storyies

--- src/icons/IconComponents-bank.ts
+++
@@ -1,4 +0,0 @@
-export { ReactComponent as AlphaBank } from '@admiral-ds/icons/build/bank/AlphaBank.svg';
-export { ReactComponent as Raif } from '@admiral-ds/icons/build/bank/Raif.svg';
-export { ReactComponent as SBP } from '@admiral-ds/icons/build/bank/SBP.svg';
-export { ReactComponent as Sber } from '@admiral-ds/icons/build/bank/Sber.svg';
--- src/icons/IconComponents-location.ts
+++
@@ -1,4 +0,0 @@
-export { ReactComponent as GPSOutline } from '@admiral-ds/icons/build/location/GPSOutline.svg';
-export { ReactComponent as GPSSolid } from '@admiral-ds/icons/build/location/GPSSolid.svg';
-export { ReactComponent as LocationOutline } from '@admiral-ds/icons/build/location/LocationOutline.svg';
-export { ReactComponent as LocationSolid } from '@admiral-ds/icons/build/location/LocationSolid.svg';
