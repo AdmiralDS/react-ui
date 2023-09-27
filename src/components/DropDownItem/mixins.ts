12d5d5f41c444b5e86b64adf36cf79ee23ab01d5
Larisa Markova <lovemirror@rambler.ru>
2023-09-27 15:24:17 +0300

    Deprecated Dropdown: fix passing props to styled

--- src/components/DropDownItem/mixins.ts
+++ src/components/DropDownItem/mixins.ts
@@ -3,8 +3,8 @@ import { typography } from '../Typography';
 
 export type DropDownItemDimension = 'l' | 'm' | 's';
 
-export const styleTextMixin = css<{ dimension: DropDownItemDimension }>`
-  ${({ dimension }) => (dimension === 's' ? typography['Body/Body 2 Long'] : typography['Body/Body 1 Long'])}
+export const styleTextMixin = css<{ $dimension: DropDownItemDimension }>`
+  ${({ $dimension }) => ($dimension === 's' ? typography['Body/Body 2 Long'] : typography['Body/Body 1 Long'])}
   ul[data-dimension='s'] & {
     ${typography['Body/Body 2 Long']}
   }
@@ -21,9 +21,9 @@ export const backgroundColor = css<{ selected?: boolean; id?: string }>`
   ${({ id, theme }) => id && `ul[data-selectedid="${id}"] && {background: ${theme.color['Opacity/Focus']};}`}
 `;
 
-export const paddings = css<{ dimension?: DropDownItemDimension }>`
-  padding: ${({ dimension }) => {
-    switch (dimension) {
+export const paddings = css<{ $dimension?: DropDownItemDimension }>`
+  padding: ${({ $dimension }) => {
+    switch ($dimension) {
       case 'l':
         return '12px 16px';
       case 'm':
