b8392954a61a9e4c280ec6129103886392899994
Larisa Markova <lovemirror@rambler.ru>
2023-10-03 17:01:36 +0300

    docs(DateField, DateInput): pass placeholder to templates

--- src/components/input/DateInput/stories/DateInputAlternativeIcon.template.tsx
+++ src/components/input/DateInput/stories/DateInputAlternativeIcon.template.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { ChangeEvent } from 'react';
-import { ReactComponent as CalendarSolidSVG } from '@admiral-ds/icons/build/system/CalendarSolid.svg';
-import { DateInput } from '@admiral-ds/react-ui';
-import type { DateInputProps } from '@admiral-ds/react-ui';
+import { ThemeProvider } from 'styled-components';
 
-export const DateInputAlternativeIconTemplate = (props: DateInputProps) => {
+import { DateInput } from '@admiral-ds/react-ui';
+import type { DateInputProps, BorderRadiusType, Theme } from '@admiral-ds/react-ui';
+import { ReactComponent as CalendarSolidSVG } from '@admiral-ds/icons/build/system/CalendarSolid.svg';
+
+export const DateInputAlternativeIconTemplate = ({
+  placeholder,
+  ...props
+}: DateInputProps & { themeBorderKind?: BorderRadiusType }) => {
+  const [placeholderValue, setPlaceholderValue] = useState<string>(placeholder || 'Другая иконка ->');
   const [value, setValue] = useState<string>('');
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -12,15 +18,28 @@ export const DateInputAlternativeIconTemplate = (props: DateInputProps) => {
     setValue(inputValue);
   };
 
+  useEffect(() => {
+    if (placeholder) {
+      setPlaceholderValue(placeholder);
+    }
+  }, [placeholder]);
+
+  function swapBorder(theme: Theme): Theme {
+    theme.shape.borderRadiusKind = props.themeBorderKind || theme.shape.borderRadiusKind;
+    return theme;
+  }
+
   return (
-    <DateInput
-      {...props}
-      value={value}
-      onChange={handleChange}
-      placeholder={'Другая иконка ->'}
-      style={{ maxWidth: 300 }}
-      dropContainerClassName="dropContainerClass"
-      icon={CalendarSolidSVG}
-    />
+    <ThemeProvider theme={swapBorder}>
+      <DateInput
+        {...props}
+        value={value}
+        onChange={handleChange}
+        placeholder={placeholderValue}
+        style={{ maxWidth: 300 }}
+        dropContainerClassName="dropContainerClass"
+        icon={CalendarSolidSVG}
+      />
+    </ThemeProvider>
   );
 };
