6ef48e521b9c8844d5e485e51c31b25163c33101
Larisa Markova <lovemirror@rambler.ru>
2023-10-12 16:36:11 +0300

    Add wrapper

--- src/components/skeleton/PseudoText.tsx
+++ src/components/skeleton/PseudoText.tsx
@@ -1,5 +1,11 @@
+import { forwardRef } from 'react';
 import styled, { css } from 'styled-components';
+import type { PolymorphicComponentProps, WebTarget } from 'styled-components';
+
 import type { ButtonAppearance, Dimension } from '#src/components/Button/types';
+
+export type PseudoTextBaseProps = { $dimension: Dimension; $appearance: ButtonAppearance };
+export type PseudoTextProps = PolymorphicComponentProps<'web', PseudoTextBaseProps, WebTarget, WebTarget>;
 
 export const backGroundColorMixin = css<{ $appearance: ButtonAppearance }>`
   background: ${({ theme, $appearance }) =>
@@ -10,9 +16,15 @@ export const backGroundColorMixin = css<{ $appearance: ButtonAppearance }>`
       : theme.color['Neutral/Neutral 05']};
 `;
 
-export const PseudoText = styled.div<{ $dimension: Dimension; $appearance: ButtonAppearance }>`
+const PseudoTextDiv = styled.div<{ $dimension: Dimension; $appearance: ButtonAppearance }>`
   border-radius: 4px;
   width: ${({ $dimension }) => ($dimension === 's' ? 65 : 74)}px;
   height: ${({ $dimension }) => ($dimension === 's' ? 8 : 12)}px;
   ${backGroundColorMixin}
 `;
+
+export const PseudoText = forwardRef<typeof PseudoTextDiv, PseudoTextProps>(
+  ({ dimension, appearance, ...props }, ref) => {
+    return <PseudoTextDiv ref={ref} $dimension={dimension} $appearance={appearance} {...props} />;
+  },
+);
