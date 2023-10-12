dae95d6ded03cb87774340756da947ba793c6f7f
Kutepova Ekaterina <eovetelina@gmail.com>
2023-10-12 15:54:32 +0300

    feat(PseudoIcon, PseudoText): поддержка styled-components v6

--- src/components/skeleton/PseudoIcon.ts
+++ src/components/skeleton/PseudoIcon.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import type { ButtonAppearance, Dimension } from '#src/components/Button/types';
 import { backGroundColorMixin } from '#src/components/skeleton/PseudoText';
 
-export const PseudoIcon = styled.div<{ dimension: Dimension; appearance: ButtonAppearance }>`
+export const PseudoIcon = styled.div<{ $dimension: Dimension; $appearance: ButtonAppearance }>`
   border-radius: 50%;
-  width: ${({ dimension }) => (dimension === 's' ? 20 : 24)}px;
-  height: ${({ dimension }) => (dimension === 's' ? 20 : 24)}px;
+  width: ${({ $dimension }) => ($dimension === 's' ? 20 : 24)}px;
+  height: ${({ $dimension }) => ($dimension === 's' ? 20 : 24)}px;
   ${backGroundColorMixin}
 `;
--- src/components/skeleton/PseudoText.ts
+++ src/components/skeleton/PseudoText.ts
@@ -1,18 +1,18 @@
 import styled, { css } from 'styled-components';
 import type { ButtonAppearance, Dimension } from '#src/components/Button/types';
 
-export const backGroundColorMixin = css<{ appearance: ButtonAppearance }>`
-  background: ${({ theme, appearance }) =>
-    appearance === 'secondary' || appearance === 'ghost'
+export const backGroundColorMixin = css<{ $appearance: ButtonAppearance }>`
+  background: ${({ theme, $appearance }) =>
+    $appearance === 'secondary' || $appearance === 'ghost'
       ? theme.color['Neutral/Neutral 10']
-      : appearance === 'white'
+      : $appearance === 'white'
       ? theme.color['Neutral/Neutral 50']
       : theme.color['Neutral/Neutral 05']};
 `;
 
-export const PseudoText = styled.div<{ dimension: Dimension; appearance: ButtonAppearance }>`
+export const PseudoText = styled.div<{ $dimension: Dimension; $appearance: ButtonAppearance }>`
   border-radius: 4px;
-  width: ${({ dimension }) => (dimension === 's' ? 65 : 74)}px;
-  height: ${({ dimension }) => (dimension === 's' ? 8 : 12)}px;
+  width: ${({ $dimension }) => ($dimension === 's' ? 65 : 74)}px;
+  height: ${({ $dimension }) => ($dimension === 's' ? 8 : 12)}px;
   ${backGroundColorMixin}
 `;
