4d14be5ecc940754cf944c699114dcdcfd6d5d28
Andrey Litvinov <drune.email@gmail.com>
2023-12-13 14:20:46 +0300

    chore(Theme): обновлены значения токенов теней для темы

--- src/components/themes/light/shadow.tsx
+++ src/components/themes/light/shadow.tsx
@@ -2,21 +2,21 @@ import type { Shadow } from '#src/components/themes/common';
 
 export const shadow = {
   'Shadow 02': `
-  box-shadow: 0px 0.9px 2px rgba(0, 0, 0, 0.28);
+  box-shadow: 0px 0.9px 2px 0px rgba(0, 0, 0, 0.28);
 `,
   'Shadow 04': `
-  box-shadow: 0px 0.2px 0.9px 0.3px rgba(0, 0, 0, 0.12), 0px 1.6px 3.6px rgba(0, 0, 0, 0.14);
+  box-shadow: 0px 1.6px 3.6px 0px rgba(0, 0, 0, 0.14), 0px 0.2px 0.9px 0.3px rgba(0, 0, 0, 0.12);
 `,
   'Shadow 08': `
-  box-shadow: 0px -1.5px 6px rgba(0, 0, 0, 0.06), 0px 0.6px 1.8px rgba(0, 0, 0, 0.1), 0px 3.2px 9px rgba(0, 0, 0, 0.16);
+  box-shadow: 0px 3.2px 9px 0px rgba(0, 0, 0, 0.16), 0px 0.6px 1.8px 0px rgba(0, 0, 0, 0.10), 0px -1.5px 6px 0px rgba(0, 0, 0, 0.06);
 `,
   'Shadow 12': `
-  box-shadow: 0px 1.2px 12px rgba(0, 0, 0, 0.1), 0px 4px 18px rgba(0, 0, 0, 0.08), 0px 0.8px 1.8px rgba(0, 0, 0, 0.04);
+  box-shadow: 0px 0.8px 1.8px 0px rgba(0, 0, 0, 0.04), 0px 4px 18px 0px rgba(0, 0, 0, 0.08), 0px 1.2px 12px 0px rgba(0, 0, 0, 0.10);
 `,
   'Shadow 16': `
-  box-shadow: 0px 1.2px 18px rgba(0, 0, 0, 0.08), 0px 6.4px 28px rgba(0, 0, 0, 0.12);
+  box-shadow: 0px 6.4px 28px 0px rgba(0, 0, 0, 0.12), 0px 1.2px 18px 0px rgba(0, 0, 0, 0.08);
 `,
   'Shadow Stroke': `
-  box-shadow: inset 0px -1px 0px #D6D8DD;
+  box-shadow: 0px -1px 0px 0px #D6D8DD inset;
 `,
 } as Shadow;
