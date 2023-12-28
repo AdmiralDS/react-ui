cd403ee62e204918506220c0f926f784c2017b53
Kutepova Ekaterina <eovetelina@gmail.com>
2023-12-28 18:45:04 +0300

    add wrapper and content styled components

--- src/components/FloatingButton/index.tsx
+++ src/components/FloatingButton/index.tsx
@@ -1,5 +1,40 @@
 import { forwardRef } from 'react';
 import type { ButtonHTMLAttributes } from 'react';
+import styled, { css } from 'styled-components';
+
+const FloatingButtonWrapper = styled.button<{ $dimension?: Dimension; $appearance?: Appearance }>`
+  position: relative;
+  box-sizing: border-box;
+  border: none;
+  appearance: none;
+  flex: 0 0 auto;
+  margin: 0;
+  padding: ${(p) => (p.$dimension == 'm' ? 8 : 16)}px;
+  height: ${(p) => (p.$dimension == 'm' ? 40 : 56)}px;
+  width: ${(p) => (p.$dimension == 'm' ? 40 : 56)}px;
+  background-color: ${(p) =>
+    p.$appearance == 'primary' ? p.theme.color['Primary/Primary 60 Main'] : p.theme.color['Special/Elevated BG']};
+  ${(p) => p.theme.shadow['Shadow 08']}
+`;
+
+const IconColor = css<{ $appearance?: Appearance }>`
+  & *[fill^='#'] {
+    fill: ${(p) =>
+      p.$appearance == 'primary' ? p.theme.color['Special/Static White'] : p.theme.color['Primary/Primary 60 Main']};
+  }
+`;
+
+const FloatingButtonContent = styled.div<{
+  $dimension?: Dimension;
+  $appearance?: Appearance;
+}>`
+  ${IconColor}
+
+  & > svg {
+    height: 24px;
+    width: 24px;
+  }
+`;
 
 type Appearance = 'primary' | 'secondary';
 type Dimension = 'm' | 'xl';
@@ -15,6 +50,14 @@ export interface FloatingButtonProps extends ButtonHTMLAttributes<HTMLButtonElem
   skeleton?: boolean;
 }
 
-export const FloatingButton = forwardRef<HTMLButtonElement, FloatingButtonProps>((props, ref) => {
-  return <button ref={ref}>FloatingButton</button>;
-});
+// type = 'button' важно указывать, так как иначе по умолчанию будет submit
+
+export const FloatingButton = forwardRef<HTMLButtonElement, FloatingButtonProps>(
+  ({ type = 'button', appearance = 'primary', dimension = 'm', children, ...props }, ref) => {
+    return (
+      <FloatingButtonWrapper ref={ref} type={type}>
+        <FloatingButtonContent>{children}</FloatingButtonContent>
+      </FloatingButtonWrapper>
+    );
+  },
+);
