a701bf21c5648963b943fb29257e0e7d1d351531
Larisa Markova <lovemirror@rambler.ru>
2024-03-14 19:11:16 +0300

    Move styled to separate file

--- src/components/input/EditMode/index.tsx
+++ src/components/input/EditMode/index.tsx
@@ -1,220 +1,13 @@
 import type { ChangeEventHandler, MouseEvent } from 'react';
 import { forwardRef, useLayoutEffect, useRef, useState } from 'react';
-import styled, { css } from 'styled-components';
+import type { css } from 'styled-components';
 
-import { Button } from '#src/components/Button';
 import type { TextInputProps } from '#src/components/input/TextInput';
-import { TextInput } from '#src/components/input/TextInput';
-import { typography } from '#src/components/Typography';
 import { refSetter } from '#src/components/common/utils/refSetter';
 import { changeInputData } from '#src/components/common/dom/changeInputData';
-
-import { ReactComponent as EditSolid } from '@admiral-ds/icons/build/system/EditSolid.svg';
-import { ReactComponent as SentOutline } from '@admiral-ds/icons/build/service/SentOutline.svg';
-import { ReactComponent as CloseOutline } from '@admiral-ds/icons/build/service/CloseOutline.svg';
 import { Tooltip } from '#src/components/Tooltip';
 import { checkOverflow } from '#src/components/common/utils/checkOverflow';
-
-const TypographyMixin = css`
-  [data-dimension='s'] & {
-    ${typography['Body/Body 2 Long']}
-  }
-
-  [data-dimension='s-bold'] & {
-    ${typography['Subtitle/Subtitle 3']}
-  }
-
-  [data-dimension='m'] & {
-    ${typography['Body/Body 1 Long']}
-  }
-
-  [data-dimension='m-bold'] & {
-    ${typography['Subtitle/Subtitle 2']}
-  }
-
-  [data-dimension='xl'] & {
-    ${typography['Header/H5']}
-  }
-
-  [data-dimension='xxl'] & {
-    ${typography['Header/H4']}
-  }
-`;
-
-const EditInput = styled(TextInput)`
-  flex: 1 1 auto;
-  & input {
-    ${TypographyMixin}
-  }
-`;
-
-const EditButton = styled(Button)`
-  margin-left: 4px;
-  flex-shrink: 0;
-`;
-
-const multilineIcon = css`
-  align-self: flex-start;
-
-  [data-dimension='s-bold'] &&&,
-  [data-dimension='s'] &&& {
-    margin-top: 6px;
-  }
-
-  [data-dimension='m'] &&&,
-  [data-dimension='m-bold'] &&& {
-    margin-top: 8px;
-  }
-
-  [data-dimension='xl'] &&& {
-    margin-top: 14px;
-  }
-
-  [data-dimension='xxl'] &&& {
-    margin-top: 12px;
-  }
-`;
-
-const iconStyle = css`
-  display: flex;
-  flex-shrink: 0;
-  cursor: pointer;
-`;
-
-const EditIcon = styled(EditSolid)<{ $multiline: boolean }>`
-  ${iconStyle}
-  & *[fill^='#'] {
-    fill: ${({ theme }) => theme.color['Neutral/Neutral 50']};
-  }
-
-  [data-disabled='true'] & {
-    cursor: default;
-    pointer-events: none;
-    & *[fill^='#'] {
-      fill: ${({ theme }) => theme.color['Neutral/Neutral 30']};
-    }
-  }
-  [data-disabled='false'] & {
-    &:hover {
-      & *[fill^='#'] {
-        fill: ${({ theme }) => theme.color['Primary/Primary 70']};
-      }
-    }
-  }
-
-  ${(p) => p.$multiline && multilineIcon}
-`;
-
-const ConfirmIcon = styled(SentOutline)`
-  ${iconStyle}
-`;
-
-const CancelIcon = styled(CloseOutline)`
-  ${iconStyle}
-`;
-
-const Wrapper = styled.div<{ $cssMixin?: ReturnType<typeof css> }>`
-  display: flex;
-  align-items: center;
-  ${({ $cssMixin }) => $cssMixin || ''};
-`;
-
-const MultilineMixin = css`
-  word-wrap: break-word;
-  height: auto;
-  white-space: normal;
-
-  [data-dimension='s-bold'] &&&,
-  [data-dimension='s'] &&& {
-    padding-top: 6px;
-    padding-bottom: 6px;
-    padding-left: 12px;
-  }
-
-  [data-dimension='m'] &&&,
-  [data-dimension='m-bold'] &&& {
-    padding-top: 8px;
-    padding-bottom: 8px;
-  }
-
-  [data-dimension='xl'] &&& {
-    padding-top: 14px;
-    padding-bottom: 14px;
-  }
-
-  [data-dimension='xxl'] &&& {
-    padding-top: 12px;
-    padding-bottom: 12px;
-  }
-`;
-
-const SingleMixin = css`
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-
-  [data-dimension='s'] &&& {
-    height: 32px;
-    line-height: 32px;
-    padding-left: 12px;
-  }
-
-  [data-dimension='s-bold'] &&& {
-    height: 32px;
-    line-height: 32px;
-    padding-left: 12px;
-  }
-
-  [data-dimension='m'] &&& {
-    height: 40px;
-    line-height: 40px;
-  }
-
-  [data-dimension='m-bold'] &&& {
-    height: 40px;
-    line-height: 40px;
-  }
-
-  [data-dimension='xl'] &&& {
-    height: 56px;
-    line-height: 56px;
-  }
-
-  [data-dimension='xxl'] &&& {
-    height: 56px;
-    line-height: 56px;
-  }
-`;
-
-const Text = styled.div<{ $multiline?: boolean }>`
-  display: block;
-
-  align-items: center;
-  cursor: text;
-  margin-right: 12px;
-  color: ${({ theme }) => theme.color['Neutral/Neutral 90']};
-
-  ${TypographyMixin};
-  ${(p) => (p.$multiline ? MultilineMixin : SingleMixin)}
-
-  [data-disabled='true'] & {
-    cursor: default;
-    pointer-events: none;
-  }
-
-  [data-disabled='false'] & {
-    &:hover {
-      & + ${EditIcon} *[fill^='#'] {
-        fill: ${({ theme }) => theme.color['Primary/Primary 70']};
-      }
-    }
-  }
-
-  [data-disable-copying] & {
-    user-select: none;
-    pointer-events: none;
-  }
-`;
+import { CancelIcon, ConfirmIcon, EditButton, EditIcon, EditInput, Text, Wrapper } from './style';
 
 const stopEvent = (e: MouseEvent) => e.preventDefault();
 
@@ -247,7 +40,7 @@ export interface EditModeProps extends Omit<TextInputProps, 'dimension' | 'displ
   onClear?: () => void;
   /** Отображение тултипа, по умолчанию true */
   showTooltip?: boolean;
-  /** Отображение тултипа, по умолчанию true */
+  /** Многострочное отображение текста в режиме просмотра значения, по умолчанию false */
   multilineView?: boolean;
 }
 
@@ -356,6 +149,7 @@ export const EditMode = forwardRef<HTMLInputElement, EditModeProps>(
                 disabled={props.status === 'error'}
                 onClick={handleConfirm}
                 iconStart={<ConfirmIcon height={iconSize} width={iconSize} />}
+                $multiline={false}
               />
               <EditButton
                 appearance="secondary"
@@ -363,6 +157,7 @@ export const EditMode = forwardRef<HTMLInputElement, EditModeProps>(
                 displayAsSquare
                 onClick={handleCancel}
                 iconStart={<CancelIcon height={iconSize} width={iconSize} />}
+                $multiline={false}
               />
             </>
           )
--- src/components/input/EditModeArea/index.tsx
+++ src/components/input/EditModeArea/index.tsx
@@ -1,222 +1,14 @@
 import type { ChangeEventHandler, MouseEvent } from 'react';
 import { forwardRef, useLayoutEffect, useRef, useState } from 'react';
-import styled, { css } from 'styled-components';
+import type { css } from 'styled-components';
 
-import { Button } from '#src/components/Button';
 import type { TextAreaProps } from '#src/components/input/TextArea';
-import { TextArea } from '#src/components/input/TextArea';
-import { typography } from '#src/components/Typography';
 import { refSetter } from '#src/components/common/utils/refSetter';
 import { changeInputData } from '#src/components/common/dom/changeInputData';
 
-import { ReactComponent as EditSolid } from '@admiral-ds/icons/build/system/EditSolid.svg';
-import { ReactComponent as SentOutline } from '@admiral-ds/icons/build/service/SentOutline.svg';
-import { ReactComponent as CloseOutline } from '@admiral-ds/icons/build/service/CloseOutline.svg';
 import { Tooltip } from '#src/components/Tooltip';
 import { checkOverflow } from '#src/components/common/utils/checkOverflow';
-
-const TypographyMixin = css`
-  [data-dimension='s'] & {
-    ${typography['Body/Body 2 Long']}
-  }
-
-  [data-dimension='s-bold'] & {
-    ${typography['Subtitle/Subtitle 3']}
-  }
-
-  [data-dimension='m'] & {
-    ${typography['Body/Body 1 Long']}
-  }
-
-  [data-dimension='m-bold'] & {
-    ${typography['Subtitle/Subtitle 2']}
-  }
-
-  [data-dimension='xl'] & {
-    ${typography['Header/H5']}
-  }
-
-  [data-dimension='xxl'] & {
-    ${typography['Header/H4']}
-  }
-`;
-
-const EditArea = styled(TextArea)`
-  flex: 1 1 auto;
-  & textarea,
-  & :first-child {
-    ${TypographyMixin}
-  }
-`;
-
-const EditButton = styled(Button)<{ $multiline: boolean }>`
-  margin-left: 4px;
-  flex-shrink: 0;
-  ${(p) => p.$multiline && 'align-self: flex-start;'}
-`;
-
-const multilineIcon = css`
-  align-self: flex-start;
-
-  [data-dimension='s-bold'] &&&,
-  [data-dimension='s'] &&& {
-    margin-top: 6px;
-  }
-
-  [data-dimension='m'] &&&,
-  [data-dimension='m-bold'] &&& {
-    margin-top: 8px;
-  }
-
-  [data-dimension='xl'] &&& {
-    margin-top: 14px;
-  }
-
-  [data-dimension='xxl'] &&& {
-    margin-top: 12px;
-  }
-`;
-
-const iconStyle = css`
-  display: flex;
-  flex-shrink: 0;
-  cursor: pointer;
-`;
-
-const EditIcon = styled(EditSolid)<{ $multiline: boolean }>`
-  ${iconStyle}
-  & *[fill^='#'] {
-    fill: ${({ theme }) => theme.color['Neutral/Neutral 50']};
-  }
-
-  [data-disabled='true'] & {
-    cursor: default;
-    pointer-events: none;
-    & *[fill^='#'] {
-      fill: ${({ theme }) => theme.color['Neutral/Neutral 30']};
-    }
-  }
-  [data-disabled='false'] & {
-    &:hover {
-      & *[fill^='#'] {
-        fill: ${({ theme }) => theme.color['Primary/Primary 70']};
-      }
-    }
-  }
-
-  ${(p) => p.$multiline && multilineIcon}
-`;
-
-const ConfirmIcon = styled(SentOutline)`
-  ${iconStyle}
-`;
-
-const CancelIcon = styled(CloseOutline)`
-  ${iconStyle}
-`;
-
-const Wrapper = styled.div<{ $cssMixin?: ReturnType<typeof css> }>`
-  display: flex;
-  align-items: center;
-  ${({ $cssMixin }) => $cssMixin || ''};
-`;
-
-const MultilineMixin = css`
-  word-wrap: break-word;
-  height: auto;
-  white-space: normal;
-
-  [data-dimension='s-bold'] &&&,
-  [data-dimension='s'] &&& {
-    padding-top: 6px;
-    padding-bottom: 6px;
-    padding-left: 12px;
-  }
-
-  [data-dimension='m'] &&&,
-  [data-dimension='m-bold'] &&& {
-    padding-top: 8px;
-    padding-bottom: 8px;
-  }
-
-  [data-dimension='xl'] &&& {
-    padding-top: 14px;
-    padding-bottom: 14px;
-  }
-
-  [data-dimension='xxl'] &&& {
-    padding-top: 12px;
-    padding-bottom: 12px;
-  }
-`;
-
-const SinglelineMixin = css`
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-
-  [data-dimension='s'] &&& {
-    height: 32px;
-    line-height: 32px;
-    padding-left: 12px;
-  }
-
-  [data-dimension='s-bold'] &&& {
-    height: 32px;
-    line-height: 32px;
-    padding-left: 12px;
-  }
-
-  [data-dimension='m'] &&& {
-    height: 40px;
-    line-height: 40px;
-  }
-
-  [data-dimension='m-bold'] &&& {
-    height: 40px;
-    line-height: 40px;
-  }
-
-  [data-dimension='xl'] &&& {
-    height: 56px;
-    line-height: 56px;
-  }
-
-  [data-dimension='xxl'] &&& {
-    height: 56px;
-    line-height: 56px;
-  }
-`;
-
-const Text = styled.div<{ $multiline?: boolean }>`
-  display: block;
-
-  align-items: center;
-  cursor: text;
-  margin-right: 12px;
-  color: ${({ theme }) => theme.color['Neutral/Neutral 90']};
-
-  ${TypographyMixin};
-  ${(p) => (p.$multiline ? MultilineMixin : SinglelineMixin)}
-
-  [data-disabled='true'] & {
-    cursor: default;
-    pointer-events: none;
-  }
-
-  [data-disabled='false'] & {
-    &:hover {
-      & + ${EditIcon} *[fill^='#'] {
-        fill: ${({ theme }) => theme.color['Primary/Primary 70']};
-      }
-    }
-  }
-
-  [data-disable-copying] & {
-    user-select: none;
-    pointer-events: none;
-  }
-`;
+import { CancelIcon, ConfirmIcon, EditArea, EditButton, EditIcon, Text, Wrapper } from '../EditMode/style';
 
 const stopEvent = (e: MouseEvent) => e.preventDefault();
 
@@ -249,8 +41,6 @@ export interface EditModeAreaProps extends Omit<TextAreaProps, 'dimension' | 'di
   onClear?: () => void;
   /** Отображение тултипа, по умолчанию true */
   showTooltip?: boolean;
-  /** Отображение тултипа, по умолчанию true */
-  multilineView?: boolean;
 }
 
 export const EditModeArea = forwardRef<HTMLTextAreaElement, EditModeAreaProps>(
@@ -266,7 +56,6 @@ export const EditModeArea = forwardRef<HTMLTextAreaElement, EditModeAreaProps>(
       onClear,
       value,
       showTooltip = true,
-      multilineView = true,
       rows = 1,
       ...props
     },
@@ -350,7 +139,6 @@ export const EditModeArea = forwardRef<HTMLTextAreaElement, EditModeAreaProps>(
                 dimension={editDimension}
                 value={value}
                 containerRef={wrapperRef}
-                autoHeight={multilineView}
                 rows={rows}
                 {...props}
               />
@@ -361,7 +149,7 @@ export const EditModeArea = forwardRef<HTMLTextAreaElement, EditModeAreaProps>(
                 disabled={props.status === 'error'}
                 onClick={handleConfirm}
                 iconStart={<ConfirmIcon height={iconSize} width={iconSize} />}
-                $multiline={multilineView}
+                $multiline={true}
               />
               <EditButton
                 appearance="secondary"
@@ -369,25 +157,19 @@ export const EditModeArea = forwardRef<HTMLTextAreaElement, EditModeAreaProps>(
                 displayAsSquare
                 onClick={handleCancel}
                 iconStart={<CancelIcon height={iconSize} width={iconSize} />}
-                $multiline={multilineView}
+                $multiline={true}
               />
             </>
           )
         ) : (
           <>
-            <Text
-              ref={refSetter(textRef, setNode)}
-              $multiline={multilineView}
-              onClick={!props.readOnly ? enableEdit : undefined}
-            >
+            <Text ref={refSetter(textRef, setNode)} $multiline onClick={!props.readOnly ? enableEdit : undefined}>
               {value}
             </Text>
             {showTooltip && tooltipVisible && overflowActive && (
               <Tooltip renderContent={() => value} targetElement={textRef.current} />
             )}
-            {!props.readOnly && (
-              <EditIcon $multiline={multilineView} height={iconSize} width={iconSize} onClick={enableEdit} />
-            )}
+            {!props.readOnly && <EditIcon $multiline height={iconSize} width={iconSize} onClick={enableEdit} />}
           </>
         )}
       </Wrapper>
