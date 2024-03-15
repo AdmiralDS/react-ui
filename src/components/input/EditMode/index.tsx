82832febc130bd84d3fa098dfbc7b49b237a572e
Larisa Markova <lovemirror@rambler.ru>
2024-03-15 14:16:03 +0300

    Move interfaces to separate file

--- src/components/input/EditMode/index.tsx
+++ src/components/input/EditMode/index.tsx
@@ -16,32 +16,15 @@ import {
   Text,
   Wrapper,
 } from '#src/components/input/EditMode/style';
+import type { EditModeComponentProps } from '#src/components/input/EditMode/types';
 
 const stopEvent = (e: MouseEvent) => e.preventDefault();
 
-export type EditModeDimension = 's' | 'm' | 'xl' | 'xxl';
-type Dimension = EditModeDimension;
-
-export interface EditModeProps extends Omit<TextInputProps, 'dimension' | 'displayClearIcon'> {
-  /** Значение компонента */
-  value: string | number;
+export interface EditModeProps
+  extends EditModeComponentProps,
+    Omit<TextInputProps, 'dimension' | 'displayClearIcon' | 'value'> {
   /** Колбек на изменение значения компонента */
   onChange: ChangeEventHandler<HTMLInputElement>;
-  /** Размер компонента */
-  dimension?: Dimension;
-  /** Жирное начертание текста. В размерах xl и xxl текст всегда жирный */
-  bold?: boolean;
-  /** Позволяет добавлять миксин на контейнер компонента, созданный с помощью styled css. */
-  containerCssMixin?: ReturnType<typeof css>;
-  /** Функция обработчика события нажатия кнопки начала редактирования
-   * @param value - значение поля ввода для редактирования */
-  onEdit?: (value: string | number) => void;
-  /** Функция обработчика события нажатия кнопки подтверждения введенного значения
-   * @param value - отредактированное значение поля ввода */
-  onConfirm?: (value: string | number) => void;
-  /** Функция обработчика события нажатия кнопки отмены
-   * @param value - значение поля ввода до нажатия кнопки редактирования */
-  onCancel?: (value: string | number) => void;
   /**
    * @deprecated Используйте onCancel
    * Колбек на нажатие кнопки очистки инпута
--- src/components/input/EditModeArea/index.tsx
+++ src/components/input/EditModeArea/index.tsx
@@ -1,6 +1,5 @@
 import type { ChangeEventHandler, MouseEvent } from 'react';
 import { forwardRef, useRef, useState } from 'react';
-import type { css } from 'styled-components';
 
 import type { TextAreaProps } from '#src/components/input/TextArea';
 import { refSetter } from '#src/components/common/utils/refSetter';
@@ -15,32 +14,15 @@ import {
   Text,
   Wrapper,
 } from '#src/components/input/EditMode/style';
-import type { EditModeDimension } from '#src/components/input/EditMode';
+import type { EditModeComponentProps } from '#src/components/input/EditMode/types';
 
 const stopEvent = (e: MouseEvent) => e.preventDefault();
 
-type Dimension = EditModeDimension;
-
-export interface EditModeAreaProps extends Omit<TextAreaProps, 'dimension' | 'displayClearIcon'> {
-  /** Значение компонента */
-  value: string | number;
+export interface EditModeAreaProps
+  extends EditModeComponentProps,
+    Omit<TextAreaProps, 'dimension' | 'displayClearIcon' | 'value'> {
   /** Колбек на изменение значения компонента */
   onChange: ChangeEventHandler<HTMLTextAreaElement>;
-  /** Размер компонента */
-  dimension?: Dimension;
-  /** Жирное начертание текста. В размерах xl и xxl текст всегда жирный */
-  bold?: boolean;
-  /** Позволяет добавлять миксин на контейнер компонента, созданный с помощью styled css. */
-  containerCssMixin?: ReturnType<typeof css>;
-  /** Функция обработчика события нажатия кнопки начала редактирования
-   * @param value - значение поля ввода для редактирования */
-  onEdit?: (value: string | number) => void;
-  /** Функция обработчика события нажатия кнопки подтверждения введенного значения
-   * @param value - отредактированное значение поля ввода */
-  onConfirm?: (value: string | number) => void;
-  /** Функция обработчика события нажатия кнопки отмены
-   * @param value - значение поля ввода до нажатия кнопки редактирования */
-  onCancel?: (value: string | number) => void;
 }
 
 export const EditModeArea = forwardRef<HTMLTextAreaElement, EditModeAreaProps>(
