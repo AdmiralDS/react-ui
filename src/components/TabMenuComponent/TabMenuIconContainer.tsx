eefe8fc34a2dab05ddd818a97080620657413410
Larisa Markova <lovemirror@rambler.ru>
2024-05-29 16:11:49 +0300

    Исправлена константа

--- src/components/TabMenuComponent/TabMenuIconContainer.tsx
+++ src/components/TabMenuComponent/TabMenuIconContainer.tsx
@@ -6,5 +6,5 @@ export const TabMenuIconContainer = styled.div<{ $underline?: boolean }>`
   box-sizing: border-box;
   height: fit-content;
   display: flex;
-  box-shadow: inset 0 -${UNDERLINE_HEIGHT} 0 0 ${(p) => (p.$underline ? `var(--admiral-color-Neutral_Neutral20, ${p.theme.color['Neutral/Neutral 20']})` : 'transparent')};
+  box-shadow: inset 0 -${UNDERLINE_HEIGHT}px 0 0 ${(p) => (p.$underline ? `var(--admiral-color-Neutral_Neutral20, ${p.theme.color['Neutral/Neutral 20']})` : 'transparent')};
 `;
