f22c3e07aecf704087690f21ed06e569c05abba8
Larisa Markova <lovemirror@rambler.ru>
2024-06-26 17:49:55 +0300

    Добавлен компонент TabMenuHorizontal

--- src/components/TabMenuComponent/stories/HorizontalTabMenuWithOverflowTemplate.tsx
+++ src/components/TabMenuComponent/stories/HorizontalTabMenuWithOverflowTemplate.tsx
@@ -1,67 +1,56 @@
-import { forwardRef, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import { forwardRef, useMemo, useState } from 'react';
+import type { ReactNode } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 
-import type { BorderRadiusType, MenuModelItemProps, RenderOptionProps } from '@admiral-ds/react-ui';
-import { MenuItem, checkOverflow } from '@admiral-ds/react-ui';
+import type { BorderRadiusType, RenderOptionProps } from '@admiral-ds/react-ui';
+import { MenuItem } from '@admiral-ds/react-ui';
 import { createBorderRadiusSwapper } from '../../../../.storybook/createBorderRadiusSwapper';
 import { ReactComponent as MinusCircleOutline } from '@admiral-ds/icons/build/service/MinusCircleOutline.svg';
 
-import { ActiveHorizontalTabUnderline } from '#src/components/TabMenuComponent/containers/ActiveHorizontalTabUnderline';
-import type { HorizontalTabProps, TabProps } from '#src/components/TabMenuComponent/types';
+import type { HorizontalTabProps } from '#src/components/TabMenuComponent/types';
 import { HorizontalTab } from '#src/components/TabMenuComponent/tabs/HorizontalTab';
 import { TabIcon } from '#src/components/TabMenuComponent/tabs/TabIcon';
 import { TabBadge } from '#src/components/TabMenuComponent/tabs/TabBadge';
-import { HorizontalTabMenuContainer } from '#src/components/TabMenuComponent/containers/HorizontalTabMenuContainer';
-import { TabOverflowMenu } from '#src/components/TabMenuComponent/containers/TabOverflowMenu';
-import {
-  OVERFLOW_MENU_CONTAINER_SIZE_L,
-  OVERFLOW_MENU_CONTAINER_SIZE_M,
-} from '#src/components/TabMenuComponent/constants';
 import { TabText } from '#src/components/TabMenuComponent/tabs/TabText';
+import { TabMenuHorizontal } from '#src/components/TabMenuComponent/tabMenus/TabMenuHorizontal';
 
-interface TabContentProps extends TabProps {
+interface TabContentProps extends HorizontalTabProps {
   text: string;
+  badge?: number;
+  disabled?: boolean;
+  icon?: ReactNode;
 }
 
-type TabWidthMapProps = {
-  tabId: string;
-  width: number;
-};
-
-interface CustomHorizontalTabProps extends TabContentProps, HorizontalTabProps {}
+interface CustomHorizontalTabProps extends TabContentProps {}
 const CustomHorizontalTab = forwardRef<HTMLButtonElement, CustomHorizontalTabProps>(
-  ({ dimension = 'l', disabled, selected, onSelectTab, tabId, text, ...props }: CustomHorizontalTabProps, ref) => {
+  ({ dimension = 'l', disabled, selected, tabId, text, badge, icon, ...props }: CustomHorizontalTabProps, ref) => {
     return (
-      <HorizontalTab
-        {...props}
-        tabId={tabId}
-        ref={ref}
-        dimension={dimension}
-        disabled={disabled}
-        selected={selected}
-        onSelectTab={onSelectTab}
-      >
-        <TabIcon $dimension={dimension} $disabled={disabled}>
-          <MinusCircleOutline />
-        </TabIcon>
+      <HorizontalTab {...props} tabId={tabId} ref={ref} dimension={dimension} disabled={disabled} selected={selected}>
+        {icon && (
+          <TabIcon $dimension={dimension} $disabled={disabled}>
+            {icon}
+          </TabIcon>
+        )}
         <TabText>{text}</TabText>
-        <TabBadge disabled={disabled} selected={selected}>
-          5
-        </TabBadge>
+        {badge && (
+          <TabBadge disabled={disabled} selected={selected}>
+            {badge}
+          </TabBadge>
+        )}
       </HorizontalTab>
     );
   },
 );
 
 const tabs = [
-  { text: 'Text1', tabId: '1' },
-  { text: 'Text22', tabId: '2' },
+  { text: 'Text1', tabId: '1', badge: 1 },
+  { text: 'Text22', tabId: '2', icon: <MinusCircleOutline /> },
   { text: 'Text333', tabId: '3' },
-  { text: 'Text4444', tabId: '4' },
-  { text: 'Text55555', tabId: '5', disabled: true },
+  { text: 'Text4444', tabId: '4', badge: 4 },
+  { text: 'Text55555', tabId: '5', disabled: true, icon: <MinusCircleOutline /> },
   { text: 'Text66666', tabId: '6' },
   { text: 'Text7777 is very very very very long', tabId: '7' },
-  { text: 'Text888', tabId: '8' },
+  { text: 'Text888', tabId: '8', icon: <MinusCircleOutline /> },
   { text: 'Text99', tabId: '9' },
 ];
 
@@ -71,21 +60,6 @@ const Wrapper = styled.div`
   gap: 10px;
 `;
 
-const HiddenContainer = styled.div`
-  visibility: hidden;
-  display: flex;
-  flex: 1 1 auto;
-  flex-wrap: nowrap;
-  align-items: center;
-  width: 100%;
-  box-sizing: border-box;
-  overflow: hidden;
-`;
-const VisibleContainer = styled(HorizontalTabMenuContainer)`
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
 const MenuItemWrapper = styled.div`
   display: flex;
   overflow: hidden;
@@ -102,181 +76,60 @@ export const HorizontalTabMenuWithOverflowTemplate = ({
   CSSCustomProps?: boolean;
 }) => {
   const dimension = 'l';
-  //<editor-fold desc="Управление шириной контейнера">
-  const [containerWidth, setContainerWidth] = useState(0);
+  const tabsMap = useMemo(() => {
+    return tabs.map((tab) => tab.tabId);
+  }, [tabs]);
 
-  useLayoutEffect(() => {
-    if (visibleContainerRef.current) {
-      const resizeObserver = new ResizeObserver((entries) => {
-        entries.forEach((entry) => setContainerWidth(entry.contentRect.width || 0));
-      });
-      resizeObserver.observe(visibleContainerRef.current);
-      return () => {
-        resizeObserver.disconnect();
-      };
-    }
-  }, []);
-  //</editor-fold>
+  const [selectedTab, setSelectedTab] = useState<string | undefined>('3');
+  const handleSelectTab = (tabId: string) => setSelectedTab(tabId);
 
-  //<editor-fold desc="Создание табов для отрисовки">
-  const [activeTab, setActiveTab] = useState<string | undefined>('3');
-  const handleSelectTab = (tabId: string) => {
-    setActiveTab(tabId);
+  const tabIsDisabled = (tabId: string) => {
+    const currentTab = tabs.find((tab) => tab.tabId === tabId);
+    return !!currentTab?.disabled;
   };
-  const renderVisibleTab = (text: string, tabId: string, disabled?: boolean) => {
+  const renderTab = (tabId: string, selected?: boolean, onSelectTab?: (tabId: string) => void) => {
+    const currentTab = tabs.find((tab) => tab.tabId === tabId);
+    const text = currentTab?.text || '';
+    const disabled = !!currentTab?.disabled;
+    const badge = currentTab?.badge;
+    const icon = currentTab?.icon;
     return (
       <CustomHorizontalTab
         tabId={tabId}
         text={text}
+        badge={badge}
+        icon={icon}
         key={tabId}
-        selected={tabId === activeTab}
+        selected={selected}
         disabled={disabled}
-        onSelectTab={handleSelectTab}
+        onSelectTab={onSelectTab}
       />
     );
   };
-
-  const horizontalTabsL = tabs.map((tab) => {
-    return renderVisibleTab(tab.text, `${tab.tabId}-hidden`, tab.disabled);
-  });
-
-  const visibleContainerRef = useRef<HTMLDivElement>(null);
-  const hiddenContainerRef = useRef<HTMLDivElement>(null);
-  const [overflowL, setOverflowL] = useState(false);
-  const [tabWidthMap, setTabWidthMap] = useState<Array<TabWidthMapProps>>([]);
-  useEffect(() => {
-    const tabWidth: TabWidthMapProps[] = [];
-    if (hiddenContainerRef.current) {
-      const overflow = checkOverflow(hiddenContainerRef.current);
-      if (overflow !== overflowL) setOverflowL(overflow);
-      const renderedTabs = hiddenContainerRef.current.children;
-      for (let i = 0; i < renderedTabs.length; i++) {
-        const tab = renderedTabs[i];
-        const width = tab.getBoundingClientRect().width;
-        tabWidth.push({ tabId: tabs[i].tabId, width: width });
-      }
-    }
-    setTabWidthMap(tabWidth);
-  }, [hiddenContainerRef, containerWidth]);
-
-  const [visibleTabs, setVisibleTabs] = useState<string[]>([]);
-  const [hiddenTabs, setHiddenTabs] = useState<string[]>([]);
-  useEffect(() => {
-    const newVisibleTabs: string[] = [];
-    const newHiddenTabs: string[] = [];
-    if (visibleContainerRef.current && tabWidthMap.length > 0) {
-      const maxWidth = containerWidth;
-      const addToVisible = (tabId: string) => newVisibleTabs.push(tabId);
-      const addToHidden = (tabId: string) => newHiddenTabs.push(tabId);
-
-      const activeTabWidth = tabWidthMap.find((tab) => tab.tabId === activeTab)?.width;
-      let availableWidth = overflowL
-        ? maxWidth -
-          (dimension === 'l' ? OVERFLOW_MENU_CONTAINER_SIZE_L : OVERFLOW_MENU_CONTAINER_SIZE_M) -
-          (activeTabWidth || 0)
-        : maxWidth;
-      tabs.forEach((tab, index) => {
-        const tabIsActive = tab.tabId === activeTab;
-        const tabWidth = tabWidthMap[index].width;
-
-        if (availableWidth >= tabWidth || tabIsActive) {
-          addToVisible(tab.tabId);
-        }
-        if (availableWidth === 0 || (availableWidth < tabWidth && !tabIsActive)) {
-          addToHidden(tab.tabId);
-        }
-        if (availableWidth > 0) {
-          if (!tabIsActive) {
-            if (availableWidth >= tabWidth) {
-              availableWidth -= tabWidth;
-            } else {
-              availableWidth = 0;
-            }
-          }
-        }
-      });
-    }
-    setVisibleTabs(newVisibleTabs);
-    setHiddenTabs(newHiddenTabs);
-  }, [visibleContainerRef, containerWidth, tabWidthMap, overflowL, activeTab]);
-  const renderedVisibleTabs = useMemo(() => {
-    if (visibleTabs.length === 0) return [];
-    return visibleTabs.map((tabId) => {
-      const currentTab = tabs.findIndex((tab) => tab.tabId === tabId);
-      return renderVisibleTab(tabs[currentTab].text, tabId, tabs[currentTab].disabled);
-    });
-  }, [visibleTabs, dimension]);
-  const overflowMenuItems: MenuModelItemProps[] = useMemo(() => {
-    if (hiddenTabs.length === 0) return [];
-    return hiddenTabs.map((tabId) => {
-      const currentTab = tabs.findIndex((tab) => tab.tabId === tabId);
-      return {
-        id: tabId,
-        render: (options: RenderOptionProps) => (
-          <MenuItem dimension={dimension} {...options} key={tabId}>
-            <MenuItemWrapper>{tabs[currentTab].text}</MenuItemWrapper>
-          </MenuItem>
-        ),
-        disabled: tabs[currentTab].disabled,
-      };
-    });
-  }, [hiddenTabs, dimension]);
-  //</editor-fold>
-
-  //<editor-fold desc="Параметры для корректной отрисовки TabActiveUnderline">
-  const [underlineLeftL, setUnderlineLeftL] = useState(0);
-  const [underlineWidthL, setUnderlineWidthL] = useState(0);
-  const [underlineTransitionL, setUnderlineTransitionL] = useState(false);
-  const getActiveTabWidth = () => {
-    return tabWidthMap.find((tab) => tab.tabId === activeTab)?.width || 0;
+  const renderDropMenuItem = (tabId: string) => {
+    const currentTab = tabs.find((tab) => tab.tabId === tabId);
+    return (options: RenderOptionProps) => {
+      return (
+        <MenuItem dimension={dimension} {...options} key={tabId}>
+          <MenuItemWrapper>{currentTab?.text}</MenuItemWrapper>
+        </MenuItem>
+      );
+    };
   };
-  const getActiveTabLeft = () => {
-    const index = visibleTabs.findIndex((tab) => tab === activeTab);
-    if (index < 0) return 0;
-    let left = 0;
-    for (let i = 0; i < index; i++) {
-      const tabWidth = tabWidthMap.find((tab) => tab.tabId === visibleTabs[i])?.width || 0;
-      left += tabWidth;
-    }
-    return left;
-  };
-  const getUnderlinePosition = () => {
-    const width = getActiveTabWidth();
-    const left = getActiveTabLeft();
-    return { left: left, width: width };
-  };
-  const styleUnderlineL = (enableTransition: boolean) => {
-    const { left, width } = getUnderlinePosition();
-    setUnderlineTransitionL(enableTransition);
-    setUnderlineWidthL(width);
-    setUnderlineLeftL(left);
-  };
-  useEffect(() => {
-    styleUnderlineL(true);
-  }, [activeTab, renderedVisibleTabs]);
-  //</editor-fold>
 
   return (
     <ThemeProvider theme={createBorderRadiusSwapper(themeBorderKind, CSSCustomProps)}>
       <Wrapper>
-        <div style={{ position: 'relative', height: '70px' }}>
-          <HiddenContainer ref={hiddenContainerRef}>{horizontalTabsL}</HiddenContainer>
-          <VisibleContainer ref={visibleContainerRef} $underline>
-            {renderedVisibleTabs}
-            <TabOverflowMenu
-              items={overflowMenuItems}
-              isHidden={!overflowL}
-              onSelectItem={(tabId) => setActiveTab(tabId)}
-              selected={activeTab}
-              dimension={dimension}
-            />
-            <ActiveHorizontalTabUnderline
-              $left={`${underlineLeftL}px`}
-              $width={`${underlineWidthL}px`}
-              $transition={underlineTransitionL}
-            />
-          </VisibleContainer>
-        </div>
+        <TabMenuHorizontal
+          dimension={dimension}
+          selectedTabId={selectedTab}
+          defaultSelectedTabId="3"
+          onSelectTab={handleSelectTab}
+          tabsId={tabsMap}
+          renderDropMenuItem={renderDropMenuItem}
+          renderTab={renderTab}
+          tabIsDisabled={tabIsDisabled}
+        />
       </Wrapper>
     </ThemeProvider>
   );
---
+++ src/components/TabMenuComponent/tabMenus/TabMenuHorizontal.tsx
@@ -0,0 +1,224 @@
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import type { ReactNode } from 'react';
+import styled from 'styled-components';
+
+import { TabOverflowMenu } from '#src/components/TabMenuComponent/containers/TabOverflowMenu';
+import { checkOverflow } from '#src/components/common/utils/checkOverflow';
+import type { RenderOptionProps, MenuModelItemProps } from '#src/components/Menu/MenuItem';
+
+import { ActiveHorizontalTabUnderline } from '#src/components/TabMenuComponent/containers/ActiveHorizontalTabUnderline';
+import { HorizontalTabMenuContainer } from '#src/components/TabMenuComponent/containers/HorizontalTabMenuContainer';
+import type { TabDimension } from '#src/components/TabMenuComponent/types';
+import {
+  OVERFLOW_MENU_CONTAINER_SIZE_L,
+  OVERFLOW_MENU_CONTAINER_SIZE_M,
+} from '#src/components/TabMenuComponent/constants';
+
+type TabWidthMapProps = {
+  tabId: string;
+  width: number;
+};
+
+const HiddenContainer = styled.div`
+  visibility: hidden;
+  display: flex;
+  flex-wrap: nowrap;
+  box-sizing: border-box;
+  overflow: hidden;
+`;
+const VisibleContainer = styled(HorizontalTabMenuContainer)`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+export interface TabMenuHorizontalProps {
+  /** Размер компонента */
+  dimension?: TabDimension;
+  /** ID выбранной вкладки */
+  selectedTabId?: string;
+  /** ID выбранной по умолчанию вкладки */
+  defaultSelectedTabId?: string;
+  /** Коллбэк на изменение выбранной вкладки */
+  onSelectTab?: (tabId: string) => void;
+  /** Массив из уникальных ID вкладок */
+  tabsId: string[];
+  /** Рендер-функция для отрисовки вкладки */
+  renderTab: (tabId: string, selected: boolean, onSelectTab?: (tabId: string) => void) => ReactNode;
+  /** Рендер-функция для отрисовки элемента выпадающего списка */
+  renderDropMenuItem: (tabId: string) => ((options: RenderOptionProps) => ReactNode) | ReactNode;
+  /** Коллбэк для определения, является ли вкладка disabled */
+  tabIsDisabled: (tabId: string) => boolean;
+}
+
+export const TabMenuHorizontal = ({
+  dimension = 'l',
+  selectedTabId,
+  defaultSelectedTabId,
+  onSelectTab,
+  tabsId,
+  renderTab,
+  renderDropMenuItem,
+  tabIsDisabled,
+}: TabMenuHorizontalProps) => {
+  //<editor-fold desc="Управление шириной контейнера">
+  const [containerWidth, setContainerWidth] = useState(0);
+  const visibleContainerRef = useRef<HTMLDivElement>(null);
+  const hiddenContainerRef = useRef<HTMLDivElement>(null);
+  const [overflowState, setOverflowState] = useState(false);
+
+  useLayoutEffect(() => {
+    if (visibleContainerRef.current) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        entries.forEach((entry) => setContainerWidth(entry.contentRect.width || 0));
+      });
+      resizeObserver.observe(visibleContainerRef.current);
+      return () => {
+        resizeObserver.disconnect();
+      };
+    }
+  }, []);
+  //</editor-fold>
+
+  //<editor-fold desc="Создание табов для отрисовки">
+  const [selectedTabInner, setSelectedTabInner] = useState<string | undefined>(defaultSelectedTabId);
+  const selectedTab = selectedTabId || selectedTabInner;
+  const handleSelectTab = (tabId: string) => {
+    setSelectedTabInner(tabId);
+    onSelectTab?.(tabId);
+  };
+  const horizontalTabs = useMemo(() => {
+    return tabsId.map((tabId) => {
+      return renderTab(tabId, tabId === selectedTab, undefined);
+    });
+  }, [tabsId, renderTab]);
+  /*const horizontalTabs = tabsId.map((tabId) => {
+    return renderTab(tabId, tabId === selectedTab, undefined);
+  });*/
+  const [visibleTabs, setVisibleTabs] = useState<string[]>([]);
+  const [hiddenTabs, setHiddenTabs] = useState<string[]>([]);
+  const [tabWidthMap, setTabWidthMap] = useState<Array<TabWidthMapProps>>([]);
+
+  useLayoutEffect(() => {
+    const tabWidth: TabWidthMapProps[] = [];
+    if (hiddenContainerRef.current) {
+      const overflow = checkOverflow(hiddenContainerRef.current);
+      if (overflowState !== overflow) setOverflowState(overflow);
+      const renderedTabs = hiddenContainerRef.current.children;
+      for (let i = 0; i < renderedTabs.length; i++) {
+        const tab = renderedTabs[i];
+        const width = tab.getBoundingClientRect().width;
+        tabWidth.push({ tabId: tabsId[i], width: width });
+      }
+      setTabWidthMap(tabWidth);
+    }
+  }, [hiddenContainerRef, containerWidth, horizontalTabs]);
+
+  useEffect(() => {
+    const newVisibleTabs: string[] = [];
+    const newHiddenTabs: string[] = [];
+    if (visibleContainerRef.current && tabWidthMap.length > 0) {
+      const maxWidth = containerWidth;
+      const addToVisible = (tabId: string) => newVisibleTabs.push(tabId);
+      const addToHidden = (tabId: string) => newHiddenTabs.push(tabId);
+
+      const activeTabWidth = tabWidthMap.find((tab) => tab.tabId === selectedTabInner)?.width;
+      let availableWidth = overflowState
+        ? maxWidth -
+          (dimension === 'l' ? OVERFLOW_MENU_CONTAINER_SIZE_L : OVERFLOW_MENU_CONTAINER_SIZE_M) -
+          (activeTabWidth || 0)
+        : maxWidth;
+      tabsId.forEach((tabId, index) => {
+        const tabIsActive = tabId === selectedTabInner;
+        const tabWidth = tabWidthMap[index].width;
+
+        if (availableWidth >= tabWidth || tabIsActive) {
+          addToVisible(tabId);
+        }
+        if (availableWidth === 0 || (availableWidth < tabWidth && !tabIsActive)) {
+          addToHidden(tabId);
+        }
+        if (availableWidth > 0) {
+          if (!tabIsActive) {
+            if (availableWidth >= tabWidth) {
+              availableWidth -= tabWidth;
+            } else {
+              availableWidth = 0;
+            }
+          }
+        }
+      });
+    }
+    setVisibleTabs(newVisibleTabs);
+    setHiddenTabs(newHiddenTabs);
+  }, [visibleContainerRef, containerWidth, tabWidthMap, overflowState, selectedTabInner]);
+
+  const renderedVisibleTabs = useMemo(() => {
+    if (visibleTabs.length === 0) return [];
+    return visibleTabs.map((tabId) => {
+      return renderTab(tabId, tabId === selectedTab, handleSelectTab);
+    });
+  }, [visibleTabs, dimension]);
+  const overflowMenuItems: MenuModelItemProps[] = useMemo(() => {
+    if (hiddenTabs.length === 0) return [];
+    return hiddenTabs.map((tabId) => {
+      return {
+        id: tabId,
+        render: renderDropMenuItem(tabId),
+        disabled: tabIsDisabled(tabId),
+      };
+    });
+  }, [hiddenTabs, dimension]);
+  //</editor-fold>
+
+  //<editor-fold desc="Параметры для корректной отрисовки TabActiveUnderline">
+  const [underlineLeft, setUnderlineLeft] = useState(0);
+  const [underlineWidth, setUnderlineWidth] = useState(0);
+  const getActiveTabWidth = () => {
+    return tabWidthMap.find((tab) => tab.tabId === selectedTab)?.width || 0;
+  };
+  const getActiveTabLeft = () => {
+    const index = visibleTabs.findIndex((tab) => tab === selectedTab);
+    if (index < 0) return 0;
+    let left = 0;
+    for (let i = 0; i < index; i++) {
+      const tabWidth = tabWidthMap.find((tab) => tab.tabId === visibleTabs[i])?.width || 0;
+      left += tabWidth;
+    }
+    return left;
+  };
+  const getUnderlinePosition = () => {
+    const width = getActiveTabWidth();
+    const left = getActiveTabLeft();
+    return { left: left, width: width };
+  };
+  const styleUnderlineL = () => {
+    const { left, width } = getUnderlinePosition();
+    setUnderlineWidth(width);
+    setUnderlineLeft(left);
+  };
+  useEffect(() => {
+    styleUnderlineL();
+  }, [selectedTab, renderedVisibleTabs]);
+  //</editor-fold>
+
+  return (
+    <Wrapper>
+      <HiddenContainer ref={hiddenContainerRef}>{horizontalTabs}</HiddenContainer>
+      <VisibleContainer ref={visibleContainerRef} $underline>
+        {renderedVisibleTabs}
+        <TabOverflowMenu
+          items={overflowMenuItems}
+          isHidden={!overflowState}
+          onSelectItem={handleSelectTab}
+          selected={selectedTab}
+          dimension={dimension}
+        />
+        <ActiveHorizontalTabUnderline $left={`${underlineLeft}px`} $width={`${underlineWidth}px`} $transition={true} />
+      </VisibleContainer>
+    </Wrapper>
+  );
+};
