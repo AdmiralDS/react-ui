cbf4dd00016fac1c87dfab126b0f629c3d68f0e4
Larisa Markova <lovemirror@rambler.ru>
2024-07-15 17:27:54 +0300

    feat(CardTabMenuHorizontal): добавлен вариант CardTabMenuHorizontal

--- src/components/TabMenuComponent/containers/index.ts
+++ src/components/TabMenuComponent/containers/index.ts
@@ -2,3 +2,4 @@ export * from './HorizontalTabMenuContainer';
 export * from './IconTabMenuContainer';
 export * from './HorizontalTabsContainer';
 export * from './VerticalTabMenuContainer';
+export * from './HorizontalCardTabMenuContainer';
