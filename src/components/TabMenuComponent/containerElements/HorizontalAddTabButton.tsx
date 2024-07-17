701215d82edd03bbf955185a7f3c70760a35d3c8
Larisa Markova <lovemirror@rambler.ru>
2024-07-17 12:26:32 +0300

    Добавлен элемент HorizontalAddTabButton

---
+++ src/components/TabMenuComponent/containerElements/HorizontalAddTabButton.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+import type { TabDimension } from '#src/components/TabMenuComponent/types';
+import type { AddTabButtonProps } from '#src/components/TabMenuComponent/containerElements/types';
+import {
+  ADD_TAB_BUTTON_CONTAINER_PADDING_L,
+  ADD_TAB_BUTTON_CONTAINER_PADDING_M,
+  ADD_TAB_BUTTON_CONTAINER_SIZE_L,
+  ADD_TAB_BUTTON_CONTAINER_SIZE_M,
+} from '#src/components/TabMenuComponent/constants';
+import { IconPlacement } from '#src/components/IconPlacement';
+import { ReactComponent as PlusOutline } from '@admiral-ds/icons/build/service/PlusOutline.svg';
+
+const AddTabButtonContainer = styled.div<{ $isHidden?: boolean; $dimension?: TabDimension }>`
+  box-sizing: border-box;
+  visibility: ${(p) => (p.$isHidden ? 'hidden' : 'visible')};
+  padding: ${(p) => (p.$dimension === 'l' ? ADD_TAB_BUTTON_CONTAINER_PADDING_L : ADD_TAB_BUTTON_CONTAINER_PADDING_M)}px;
+  ${(p) => `
+    width: ${p.$dimension === 'l' ? ADD_TAB_BUTTON_CONTAINER_SIZE_L : ADD_TAB_BUTTON_CONTAINER_SIZE_M}px;
+    height: ${p.$dimension === 'l' ? ADD_TAB_BUTTON_CONTAINER_SIZE_L : ADD_TAB_BUTTON_CONTAINER_SIZE_M}px;
+  `}
+`;
+
+export const HorizontalAddTabButton = ({ dimension = 'l', isHidden = false, ...props }: AddTabButtonProps) => {
+  const buttonDimension = dimension === 'l' ? 'lBig' : 'mBig';
+  return (
+    <AddTabButtonContainer $dimension={dimension} $isHidden={isHidden}>
+      <IconPlacement {...props} dimension={buttonDimension}>
+        <PlusOutline />
+      </IconPlacement>
+    </AddTabButtonContainer>
+  );
+};
+HorizontalAddTabButton.displayName = 'AddTabButton';
