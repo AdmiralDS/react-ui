9383e9e30f23ebf1c7b3c6799930071132777e37
Larisa Markova <lovemirror@rambler.ru>
2024-08-01 19:41:04 +0300

    Update stories

--- src/components/AvatarActivity/stories/AvatarActivity.stories.tsx
+++ src/components/AvatarActivity/stories/AvatarActivity.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryFn } from '@storybook/react';
-import styled from 'styled-components';
 
+import type { AvatarActivityProps } from '@admiral-ds/react-ui';
 import { AvatarActivity } from '@admiral-ds/react-ui';
 
 import { AvatarActivitySingleTemplate } from './AvatarActivitySingle.template';
@@ -9,12 +9,6 @@ import { AvatarActivityPlaygroundTemplate } from './AvatarActivityPlayground.tem
 // Imports of text sources
 import AvatarActivitySingleRaw from './AvatarActivitySingle.template?raw';
 import AvatarActivityPlaygroundRaw from './AvatarActivityPlayground.template?raw';
-
-const Desc = styled.div`
-  font-family: 'VTB Group UI';
-  font-size: 16px;
-  line-height: 24px;
-`;
 
 export default {
   title: 'Admiral-2.1/AvatarActivity',
@@ -26,14 +20,6 @@ export default {
         code: null,
       },
     },
-    componentSubtitle: (
-      <Desc>
-        Вариант аватара с Activity Ring. В этом случае появляется пустое пространство вокруг компонента под круг
-        фокусировки, что бы он (круг) при включении входил в общий размер компонента и не обрезался во фреймах. Таким
-        образом, в AvatarActivity добавляется по 4 px с каждой стороны компонента. Используйте только один тип Аватаров
-        одновременно.
-      </Desc>
-    ),
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/EGEGZsx8WhdxpmFKu8J41G/Admiral-2.1-UI-Kit?node-id=37%3A32367',
@@ -98,21 +84,27 @@ export const AvatarPlayground = {
       source: {
         code: AvatarActivityPlaygroundRaw,
       },
-      description: {
-        story: `Вариант аватара с Activity Ring.  В этом случае появляется пустое пространство вокруг компонента 
-        под круг фокусировки, что бы он (круг) при включении входил в общий размер компонента и не обрезался во 
-        фреймах.\n\nТаким образом, в AvatarActivity добавляется по 4 px с каждой стороны компонента.\n\nИспользуйте 
-        только один тип Аватаров одновременно.`,
-      },
     },
   },
 
-  name: 'Playground',
+  name: 'AvatarActivity. Playground',
 };
 //</editor-fold>
 
 //<editor-fold desc="Single AvatarActivity">
-const AvatarActivitySingleStory: StoryFn<typeof AvatarActivity> = () => <AvatarActivitySingleTemplate />;
+const AvatarActivitySingleStory: StoryFn<typeof AvatarActivity> = ({
+  userName,
+  appearance,
+  showTooltip,
+  showActivityRing,
+}: AvatarActivityProps) => (
+  <AvatarActivitySingleTemplate
+    userName={userName}
+    appearance={appearance}
+    showTooltip={showTooltip}
+    showActivityRing={showActivityRing}
+  />
+);
 
 export const SingleAvatar = {
   render: AvatarActivitySingleStory,
@@ -122,17 +114,29 @@ export const SingleAvatar = {
       source: {
         code: AvatarActivitySingleRaw,
       },
-      description: {
-        story: `Компонент используется для отображения фотографии пользователя, его инициалов или иконки. Присутствует 
-        пять размеров компонента.\n\nДопускается использование одной или двух букв для обозначения пользователя (в размере XS 
-        только одна буква). Компоненту и символам в нем можно присваивать любые цвета из палитры.\n\nКомпонент может быть 
-        кликабельным, например, вести в личный кабинет или показывать выпадающее меню с опциями пользователя. При ховере 
-        над аватаром показывается Tooltip с именем пользователя (опционально можно отключить).\n\nМожет отображать статус 
-        пользователя с помощью статусного Бейджа.`,
-      },
+    },
+  },
+  argTypes: {
+    dimension: {
+      control: false,
+    },
+    status: {
+      control: false,
+    },
+    href: {
+      control: false,
+    },
+    userInitials: {
+      control: false,
+    },
+    userName: {
+      control: false,
+    },
+    svgMaskId: {
+      control: false,
     },
   },
 
-  name: 'AvatarActivity',
+  name: 'AvatarActivity. Типы и размеры',
 };
 //</editor-fold>
