6b7ade9dc5e0d3fa9b47e7b261f1da29a2e30755
Larisa Markova <lovemirror@rambler.ru>
2024-08-21 12:26:18 +0300

    Update Carousel API

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -46,25 +46,66 @@ const innerSliderPositionMixin = css`
   position: absolute;
   bottom: 0;
 `;
-const CarouselSliderWrapper = styled.div<{ $sliderPosition: CarouselSliderPosition }>`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const StyledCarouselSlider = styled(CarouselSlider)<{ $sliderPosition: CarouselSliderPosition }>`
+  align-self: center;
   ${(p) => p.$sliderPosition === 'inner' && innerSliderPositionMixin};
 `;
 
+const getNextItemFinite = (currentItem: number, maxItems: number) => {
+  if (currentItem < maxItems - 1) {
+    return currentItem + 1;
+  }
+  return currentItem;
+};
+const getPrevItemFinite = (currentItem: number, maxItems: number) => {
+  if (currentItem > 0) {
+    return currentItem - 1;
+  }
+  return currentItem;
+};
+const getNextItemInfinite = (currentItem: number, maxItems: number) => {
+  return (currentItem + 1) % maxItems;
+};
+const getPrevItemInfinite = (currentItem: number, maxItems: number) => {
+  if (currentItem > 0) {
+    return (currentItem - 1) % maxItems;
+  }
+  return maxItems - 1;
+};
+
 type CarouselSliderPosition = 'inner' | 'outer';
 export interface CarouselProps extends HTMLAttributes<HTMLDivElement> {
+  /** Номер выбранной секции по умолчанию (нумерация с 0) */
+  defaultItem?: number;
+  /** Номер выбранной секции (нумерация с 0) */
+  currentItem?: number;
+  /** Коллбэк на изменение выбранной секции */
+  onCurrentItemChange?: (newItem: number) => void;
+  /** Отображение кнопок вперед/назад */
   showButtons?: boolean;
+  /** Внешний вид кнопок вперед/назад */
   buttonAppearance?: CarouselButtonAppearance;
+  /** Бесконечная прокрутка секций (при showButtons кнопки будут видны всегда) */
+  infiniteScroll?: boolean;
+  /** Расчет номера предыдущей секции при нажатии на кнопку назад */
+  getPrevItem?: (currentItem: number, maxItems: number) => number;
+  /** Расчет номера следующей секции при нажатии на кнопку вперед */
+  getNextItem?: (currentItem: number, maxItems: number) => number;
+  /** Расположение слайдера внутри или снаружи компонента */
   sliderPosition?: CarouselSliderPosition;
+  /** Внешний вид компонента слайдера */
   sliderAppearance?: CarouselSliderAppearance;
 }
 
 export const Carousel = ({
+  defaultItem,
+  currentItem,
+  onCurrentItemChange,
   showButtons = true,
   buttonAppearance = 'default',
+  infiniteScroll = false,
+  getPrevItem: getPrevItemCustom,
+  getNextItem: getPrevNextCustom,
   sliderPosition = 'inner',
   sliderAppearance = 'default',
   children,
@@ -72,27 +113,31 @@ export const Carousel = ({
 }: CarouselProps) => {
   const items = useMemo(() => Children.toArray(children), [children]);
 
-  const [currentIndex, setCurrentIndex] = useState(0);
   const [length, setLength] = useState(items.length);
-
-  const next = () => {
-    if (currentIndex < length - 1) {
-      setCurrentIndex((prevState) => prevState + 1);
-    }
-  };
-
-  const prev = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex((prevState) => prevState - 1);
-    }
-  };
-  const showPrev = showButtons && currentIndex > 0;
-  const showNext = showButtons && currentIndex < length - 1;
-
-  // Set the length to match current children from props
   useEffect(() => {
     setLength(items.length);
   }, [items.length]);
+
+  const [currentIndex, setCurrentIndex] = useState<number>(defaultItem || 0);
+  const currenItemInner = currentItem || currentIndex;
+  const handleCurrentItemChange = (newItem: number) => {
+    setCurrentIndex(newItem);
+    onCurrentItemChange?.(newItem);
+  };
+
+  const getPrevItem = getPrevItemCustom || (infiniteScroll ? getPrevItemInfinite : getPrevItemFinite);
+  const getNextItem = getPrevNextCustom || (infiniteScroll ? getNextItemInfinite : getNextItemFinite);
+
+  const handlePrevClick = () => {
+    const newItem = getPrevItem(currenItemInner, length);
+    handleCurrentItemChange(newItem);
+  };
+  const handleNextClick = () => {
+    const newItem = getNextItem(currenItemInner, length);
+    handleCurrentItemChange(newItem);
+  };
+  const showPrev = showButtons ? (infiniteScroll ? true : currentIndex > 0) : false;
+  const showNext = showButtons ? (infiniteScroll ? true : currentIndex < length - 1) : false;
 
   return (
     <Container {...props}>
@@ -103,21 +148,19 @@ export const Carousel = ({
         {showPrev && <CarouselButton appearance={buttonAppearance} direction="left" onClick={handlePrevClick} />}
         {showNext && <CarouselButton appearance={buttonAppearance} direction="right" onClick={handleNextClick} />}
       </Wrapper>
-      <CarouselSliderWrapper $sliderPosition={sliderPosition}>
-        <CarouselSlider>
-          {[...Array(length).keys()].map((item) => {
-            return (
-              <CarouselSliderItem
-                aria-label={`Item ${item}`}
-                key={item}
-                appearance={sliderAppearance}
-                isCurrent={item === currentIndex}
-                onClick={() => setCurrentIndex(item)}
-              />
-            );
-          })}
-        </CarouselSlider>
-      </CarouselSliderWrapper>
+      <StyledCarouselSlider $sliderPosition={sliderPosition}>
+        {[...Array(length).keys()].map((item) => {
+          return (
+            <CarouselSliderItem
+              aria-label={`Item ${item}`}
+              key={item}
+              appearance={sliderAppearance}
+              isCurrent={item === currentIndex}
+              onClick={() => handleCurrentItemChange(item)}
+            />
+          );
+        })}
+      </StyledCarouselSlider>
     </Container>
   );
 };
