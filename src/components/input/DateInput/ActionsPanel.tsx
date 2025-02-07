b97c83aa900197ce23e9b69d3a43e995d5b55c3e
Larisa Markova <lovemirror@rambler.ru>
2025-02-07 15:23:25 +0300

    Удалены лишние примеры реализации

--- src/components/input/DateInput/ActionsPanel.tsx
+++ src/components/input/DateInput/ActionsPanel.tsx
@@ -1,7 +1,6 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import type { css } from 'styled-components';
 import { Divider } from '#src/components/Divider';
-import { TextButton } from '#src/components/TextButton';
-import { Button } from '#src/components/Button';
 
 const ActionsPanelDiv = styled.div`
   box-sizing: border-box;
@@ -28,60 +27,5 @@ export const ActionsPanel = ({ actionsPanelCssMixin: actionsPanelCssMixin, child
       <Divider dimension="s" />
       <PanelContent $actionsPanelCssMixin={actionsPanelCssMixin}>{children}</PanelContent>
     </ActionsPanelDiv>
-  );
-};
-
-const TodayTextButton = styled(TextButton)`
-  margin-top: 21px;
-`;
-
-export interface PanelWithTodayButtonProps {
-  onTodayButtonMouseDown: React.MouseEventHandler;
-}
-
-export const PanelWithTodayButton = ({ onTodayButtonMouseDown }: PanelWithTodayButtonProps) => {
-  const handleTodayButtonMouseDown: React.MouseEventHandler = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    onTodayButtonMouseDown(e);
-  };
-  return (
-    <ActionsPanel>
-      <TodayTextButton dimension="s" text="Сегодня" onMouseDown={handleTodayButtonMouseDown} />
-    </ActionsPanel>
-  );
-};
-
-const ClearTextButton = styled(TextButton)`
-  margin-top: 21px;
-`;
-
-const mixin = css`
-  width: 100%;
-  justify-content: space-between;
-`;
-
-const RightButtonsBlock = styled.div`
-  display: flex;
-`;
-
-const CancelTextButton = styled(TextButton)`
-  margin-top: 21px;
-  margin-right: 16px;
-`;
-
-const StyledButton = styled(Button)`
-  margin-top: 15px;
-`;
-
-export const PanelWithOkButton = () => {
-  return (
-    <ActionsPanel actionsPanelCssMixin={mixin}>
-      <ClearTextButton dimension="s" text="Очистить" />
-      <RightButtonsBlock>
-        <CancelTextButton dimension="s" text="Отменить" />
-        <StyledButton dimension="s">Ok</StyledButton>
-      </RightButtonsBlock>
-    </ActionsPanel>
   );
 };
