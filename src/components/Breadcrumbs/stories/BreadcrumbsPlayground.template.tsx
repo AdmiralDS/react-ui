a8ab9baea055bd99e75b055fa9a262ff391f791d
Andrey Litvinov <i@drune.ru>
2025-02-24 15:26:40 +0300

    chore(BreadCrumbs):  #1667 add custom props passing on container

--- src/components/Breadcrumbs/stories/BreadcrumbsPlayground.template.tsx
+++ src/components/Breadcrumbs/stories/BreadcrumbsPlayground.template.tsx
@@ -3,10 +3,18 @@ import { Breadcrumbs } from '@admiral-ds/react-ui';
 import type { BreadcrumbsProps } from '@admiral-ds/react-ui';
 
 const items = [
-  { url: '#', text: 'page 1' },
+  { url: '#', text: 'page 1', 'data-testid': 'testid1' },
   { url: '#', text: 'page 2222222222222222222222222222222222222222' },
-  { url: '#', text: 'page 3' },
-  { url: '#', text: 'page 4' },
+  {
+    url: '#',
+    text: 'page 3',
+    'data-testid': 'testid1',
+    containerPropsConfig: (props) => {
+      console.log(JSON.stringify(props, null, 2));
+      return { 'data-testid': 'testid3' };
+    },
+  },
+  { url: '#', text: 'page 4', linkProps: { 'data-testid': 'linkid' } },
   { url: '#', text: 'page 5' },
   { url: '#', text: 'page 6' },
   { url: '#', text: 'page 7' },
@@ -15,7 +23,7 @@ const items = [
   { url: '#', text: 'page 10' },
   { url: '#', text: 'page 11' },
   { text: 'current page' },
-];
+] satisfies BreadcrumbsProps['items'];
 
 export const BreadcrumbsPlaygroundTemplate = (args: BreadcrumbsProps) => {
   return (
