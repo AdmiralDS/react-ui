a60be5a81e0703bffcc3b5fd91ff1b06bf202ee1
babaev_adil <babaev.adil8950@gmail.com>
2025-05-05 16:55:38 +0300

    fix(TabMenuHorizontalAdaptiveFill): width underline active tab fixed

--- src/components/TabMenuComponent/tabMenus/TabMenuHorizontalAdaptiveFill.tsx
+++ src/components/TabMenuComponent/tabMenus/TabMenuHorizontalAdaptiveFill.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import { ActiveHorizontalTabSelector } from '#src/components/TabMenuComponent/containerElements';
 
 import { HorizontalTabMenuContainer } from '#src/components/TabMenuComponent/containers';
-import type { TabMenuHorizontalAdaptiveFillProps } from '#src/components/TabMenuComponent/types';
+import type { TabMenuHorizontalAdaptiveFillProps, TabWidthMapProps } from '#src/components/TabMenuComponent/types';
+import { getActiveTabWidth, getTabWidthMap } from '../utils';
+import { debounce } from '#src/components/common/utils/debounce';
 
 const Wrapper = styled.div`
   position: relative;
@@ -23,13 +25,14 @@ export const TabMenuHorizontalAdaptiveFill = ({
   showActiveTabSelector,
   ...props
 }: TabMenuHorizontalAdaptiveFillProps) => {
-  const [underlineLeft, setUnderlineLeft] = useState('');
-  const [underlineWidth, setUnderlineWidth] = useState('');
   const [selectedTabInner, setSelectedTabInner] = useState<string | undefined>(defaultSelectedTabId);
+  const [underlineLeft, setUnderlineLeft] = useState(0);
+  const [underlineWidth, setUnderlineWidth] = useState(0);
+  const [tabWidthMap, setTabWidthMap] = useState<Array<TabWidthMapProps>>([]);
 
   const selectedTab = selectedTabId || selectedTabInner;
 
-  const hiddenContainerRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const handleSelectTab = (tabId: string) => {
     if (!tabIsDisabled(tabId)) {
@@ -45,24 +48,57 @@ export const TabMenuHorizontalAdaptiveFill = ({
   }, [tabsId, renderTab]);
 
   //<editor-fold desc="Параметры для корректной отрисовки TabActiveUnderline">
+  useLayoutEffect(() => {
+    const setTabWidth = () => {
+      if (containerRef.current) {
+        const tabWidth = getTabWidthMap(tabsId, containerRef.current.children);
+        setTabWidthMap(tabWidth);
+      }
+    };
+
+    if (containerRef.current) {
+      const resizeObserver = new ResizeObserver(debounce(setTabWidth, 100));
+      resizeObserver.observe(containerRef.current);
+      return () => {
+        resizeObserver.disconnect();
+      };
+    }
+  }, []);
+
+  const getActiveTabLeft = () => {
+    const index = tabsId.findIndex((tab) => tab === selectedTab);
+    if (index < 0) return 0;
+    let left = 0;
+    for (let i = 0; i < index; i++) {
+      const tabWidth = tabWidthMap.find((tab) => tab.tabId === tabsId[i])?.width || 0;
+      left += tabWidth;
+    }
+    return left;
+  };
+
+  const getUnderlinePosition = () => {
+    const width = selectedTab ? getActiveTabWidth(tabWidthMap, selectedTab) : 0;
+    const left = getActiveTabLeft();
+    return { left: left, width: width };
+  };
+
+  const styleUnderline = () => {
+    const { left, width } = getUnderlinePosition();
+    setUnderlineWidth(width);
+    setUnderlineLeft(left);
+  };
 
   useEffect(() => {
     if (!showActiveTabSelector) return;
 
-    const length = tabsId.length;
-    const indexSelectedTab = tabsId.findIndex((tab) => tab === selectedTab);
-
-    const width = selectedTab ? `calc(100% / ${length})` : '0';
-    const left = indexSelectedTab < 0 ? '0' : `calc(100% / ${length} * ${indexSelectedTab})`;
-    setUnderlineWidth(width);
-    setUnderlineLeft(left);
-  }, [selectedTab, horizontalTabs, showActiveTabSelector]);
+    styleUnderline();
+  }, [selectedTab, horizontalTabs, tabWidthMap]);
   //</editor-fold>
 
   return (
     <Wrapper {...props}>
       <HorizontalTabMenuContainer
-        ref={hiddenContainerRef}
+        ref={containerRef}
         $showUnderline={showUnderline}
         $showActiveTabSelector={showActiveTabSelector}
       >
@@ -71,8 +107,8 @@ export const TabMenuHorizontalAdaptiveFill = ({
       {showActiveTabSelector && (
         <ActiveHorizontalTabSelector
           $appearance={appearance}
-          $left={underlineLeft}
-          $width={underlineWidth}
+          $left={`${underlineLeft}px`}
+          $width={`${underlineWidth}px`}
           $transition={true}
         />
       )}
