695840270192fe86ffd3381964bd7d53d6edc54a
Kutepova Ekaterina <eovetelina@gmail.com>
2025-06-03 02:26:06 +0300

    add keydown handler and rewrite types usage

--- src/components/Accordion/AccordionItem.tsx
+++ src/components/Accordion/AccordionItem.tsx
@@ -1,26 +1,26 @@
-import type { HTMLAttributes, ReactNode, MouseEvent, FC } from 'react';
-import { useState, useMemo, useCallback } from 'react';
-
+import { useState, useMemo, useCallback, useRef } from 'react';
 import { uid } from '#src/components/common/uid';
+import { keyboardKey } from '#src/components/common/keyboardKey';
+
 import { Collapse } from './Collapse';
 import { ItemWrapper, ItemTitle, ItemTitleContent, ItemContent, TitleContent, Chevron } from './style';
 
-export interface AccordionItemProps extends Omit<HTMLAttributes<HTMLDivElement>, 'onClick' | 'title'> {
+export interface AccordionItemProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onClick' | 'title'> {
   /** Заголовок компонента */
-  title: ReactNode;
+  title: React.ReactNode;
   /** дефолтное (изначальное) состояние компонента (раскрыт/свернут) при неконтролируемом режиме работы */
   defaultExpanded?: boolean;
   /** состояние компонента (раскрыт/свернут) при контролируемом режиме работы */
   expanded?: boolean;
   /** Колбек на клик по компоненту */
-  onClick?: (title: ReactNode, expanded: boolean, event: MouseEvent<HTMLDivElement>) => void;
+  onClick?: (title: React.ReactNode, expanded: boolean, event: React.MouseEvent<HTMLDivElement>) => void;
   /**  Устанавливает максимальную высоту, на которую открывается контент до появления вертикального скрола */
   contentMaxHeight?: number | string;
   /** Отключение компонента */
   disabled?: boolean;
 }
 
-export const AccordionItem: FC<AccordionItemProps> = ({
+export const AccordionItem: React.FC<AccordionItemProps> = ({
   children,
   title,
   id,
@@ -33,13 +33,14 @@ export const AccordionItem: FC<AccordionItemProps> = ({
 }) => {
   const [expanded, setExpanded] = useState(defaultExpanded);
   const collapseOpened = userExpanded === undefined ? expanded : userExpanded;
+  const titleRef = useRef<HTMLDivElement>(null);
 
   const itemId = useMemo(() => id || uid(), [id]);
   const ITEM_TITLE_ID = `accordion_title_${itemId}`;
   const ITEM_CONTENT_ID = `accordion_content_${itemId}`;
 
   const handleClick = useCallback(
-    (event: MouseEvent<HTMLDivElement>) => {
+    (event: React.MouseEvent<HTMLDivElement>) => {
       if (userExpanded === undefined) {
         onClick?.(title, !expanded, event);
         setExpanded(!expanded);
@@ -49,10 +50,20 @@ export const AccordionItem: FC<AccordionItemProps> = ({
     },
     [expanded, userExpanded, onClick, title],
   );
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    const code = keyboardKey.getCode(e);
+    if (code === keyboardKey.Enter || code === keyboardKey[' ']) {
+      titleRef.current?.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    }
+    props.onKeyDown?.(e);
+  };
   return (
     <ItemWrapper $opened={collapseOpened} data-disabled={disabled}>
       <ItemTitle
+        ref={titleRef}
         onClick={disabled ? undefined : handleClick}
+        onKeyDown={disabled ? undefined : handleKeyDown}
         role="button"
         aria-expanded={collapseOpened}
         aria-controls={ITEM_CONTENT_ID}
