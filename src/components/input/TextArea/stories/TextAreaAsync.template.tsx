d83d6bd1bd221e3d3787118428b7031b1b439fe6
Larisa Markova <lovemirror@rambler.ru>
2025-09-12 11:18:45 +0300

    Добавлен пример для отлавливания ошибки

--- src/components/input/TextArea/stories/TextAreaAsync.template.tsx
+++ src/components/input/TextArea/stories/TextAreaAsync.template.tsx
@@ -1,7 +1,7 @@
-import { useLayoutEffect, useState } from 'react';
+import { useLayoutEffect, useState, useId } from 'react';
 import styled from 'styled-components';
 
-import { TextArea } from '@admiral-ds/react-ui';
+import { Button, TextArea } from '@admiral-ds/react-ui';
 
 const Form = styled.form`
   display: flex;
@@ -20,34 +20,41 @@ const TEXT =
   'grid, and multiples of mini units map to fluid grid column widths and row heights.';
 
 export const TextAreaAsyncTemplate = () => {
+  const copyIconProps = { 'data-testid': useId() };
   const [textValue, setTextValue] = useState<string>('' /*TEXT*/);
 
-  useLayoutEffect(() => {
-    const timeout = setTimeout(() => {
-      setTextValue(TEXT);
-    }, 1000);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [setTextValue]);
+  // useLayoutEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     setTextValue(TEXT);
+  //   }, 1000);
+  //   return () => {
+  //     clearTimeout(timeout);
+  //   };
+  // }, [setTextValue]);
 
   const handleChangeTextValue = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const inputValue = e.target.value;
     setTextValue(inputValue);
   };
+  const handleClearButtonClick = () => {
+    setTextValue('');
+  };
+  const handleFillButtonClick = () => {
+    setTextValue(TEXT);
+  };
 
   return (
     <Form>
+      <Button onClick={handleClearButtonClick}>Clear area</Button>
+      <Button onClick={handleFillButtonClick}>Fill area</Button>
       <TextArea
         data-container-id="textField-1"
         autoHeight
-        rows={3}
-        maxRows={6}
         value={textValue}
         onChange={handleChangeTextValue}
+        displayCopyIcon
+        copyIconPropsConfig={() => copyIconProps}
       />
-
-      <TextArea data-testid="textField-2" autoHeight rows={3} maxRows={6} required />
     </Form>
   );
 };
