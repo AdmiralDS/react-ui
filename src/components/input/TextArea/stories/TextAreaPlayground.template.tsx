3236014f2b67e0252c3bfe2edaa81e3b49018b91
Larisa Markova <lovemirror@rambler.ru>
2025-09-25 20:13:57 +0300

    Add TextArea autoheight test

--- src/components/input/TextArea/stories/TextAreaPlayground.template.tsx
+++ src/components/input/TextArea/stories/TextAreaPlayground.template.tsx
@@ -33,6 +33,7 @@ export const TextAreaPlaygroundTemplate = ({
         placeholder={placeholder}
         clearIconPropsConfig={() => clearIconProps}
         copyIconPropsConfig={() => copyIconProps}
+        data-testid="textAreaPlayground"
       />
     </ThemeProvider>
   );
---
+++ tests/TextArea/textarea.spec.ts
@@ -0,0 +1,66 @@
+import { test, expect } from '@playwright/test';
+
+test('basic render', async ({ page }) => {
+  await page.goto('/?path=/story/admiral-2-1-input-textarea--text-area-playground');
+  const frame = page.frameLocator('#storybook-preview-iframe');
+
+  const component = frame.getByTestId('textAreaPlayground');
+  await expect(component).toHaveText('Привет!');
+});
+
+test('autoheight with value undefined', async ({ page }) => {
+  await page.goto('/?path=/story/admiral-2-1-input-textarea--text-area-playground');
+  const frame = page.frameLocator('#storybook-preview-iframe');
+
+  const component = frame.getByTestId('textAreaPlayground');
+  await component.fill('1\n2');
+
+  const boxFirst = await component.boundingBox();
+  await component.fill('');
+  const boxSecond = await component.boundingBox();
+  expect(boxSecond?.height).toBeCloseTo(boxFirst!.height, 0);
+
+  await component.fill('1\n2\n3\n4\n5\n6');
+  const boxThird = await component.boundingBox();
+  expect(boxThird?.height).toBeCloseTo(boxFirst!.height, 0);
+});
+
+test('autoheight with value true', async ({ page }) => {
+  await page.goto('/?path=/story/admiral-2-1-input-textarea--text-area-playground&args=autoHeightBoolean:!true');
+  const frame = page.frameLocator('#storybook-preview-iframe');
+
+  const component = frame.getByTestId('textAreaPlayground');
+  await component.fill('1\n2');
+
+  const boxFirst = await component.boundingBox();
+  await component.fill('');
+  const boxSecond = await component.boundingBox();
+  expect(boxSecond?.height).toBeCloseTo(boxFirst!.height, 0);
+
+  await component.fill('1\n2\n3\n4\n5\n6');
+  const boxThird = await component.boundingBox();
+  expect(boxThird?.height).toBeGreaterThan(boxSecond!.height);
+});
+
+test('autoheight with min and max rows', async ({ page }) => {
+  await page.goto(
+    '/?path=/story/admiral-2-1-input-textarea--text-area-playground&args=autoHeightBoolean:!true;autoHeightMinRows:3;autoHeightMaxRows:5',
+  );
+  const frame = page.frameLocator('#storybook-preview-iframe');
+
+  const component = frame.getByTestId('textAreaPlayground');
+
+  await component.fill('1\n2');
+  const boxFirst = await component.boundingBox();
+  await component.fill('1\n2\n3\n4\n5');
+  const boxSecond = await component.boundingBox();
+  expect(boxSecond?.height).toBeGreaterThan(boxFirst!.height);
+
+  await component.fill('1\n2\n3\n4\n5\n6');
+  const boxThird = await component.boundingBox();
+  expect(boxThird?.height).toBeCloseTo(boxSecond!.height, 0);
+
+  await component.fill('Привет!');
+  const boxFourth = await component.boundingBox();
+  expect(boxFourth?.height).toBeCloseTo(boxFirst!.height, 0);
+});
