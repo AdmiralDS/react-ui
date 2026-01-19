96c35f368dc09dacf16cda8794deebdcb72e2366
Larisa Markova <lovemirror@rambler.ru>
2026-01-19 16:09:15 +0300

    Use 1 worker

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 4 : parseInt(process.env.JOBS || '1'),
+  workers: process.env.CI ? 1 : parseInt(process.env.JOBS || '1'),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
