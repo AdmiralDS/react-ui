505c34f4881b7817bb565f43aa62f9ffaf272fea
Larisa Markova <lovemirror@rambler.ru>
2026-02-27 17:00:46 +0300

    Update eslint for soft migration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,18 +3,25 @@ import { defineConfig } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import importPlugin from 'eslint-plugin-import';
 
+// Временный блок для мягкого перехода.
+// Для новых проектов: все правила из этого блока должны быть выставлены в `error`.
+const SOFT_MIGRATION_RULES = {
+  '@typescript-eslint/no-explicit-any': 'warn',
+  '@typescript-eslint/no-empty-object-type': 'warn',
+  // Note: you must disable the base rule as it can report incorrect errors
+  'no-unused-expressions': 'off',
+  '@typescript-eslint/no-unused-expressions': 'warn',
+  '@typescript-eslint/no-non-null-assertion': 'warn',
+  '@typescript-eslint/no-dynamic-delete': 'warn',
+};
+
 export default defineConfig([
   ...configs.strict,
   importPlugin.flatConfigs.recommended,
   {
     rules: {
       'import/no-named-as-default': 'off', // для совместимости с styled-components v5
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-empty-object-type': 'warn',
-      'no-unused-expressions': 'off',
-      '@typescript-eslint/no-unused-expressions': 'warn',
-      '@typescript-eslint/no-non-null-assertion': 'warn',
-      '@typescript-eslint/no-dynamic-delete': 'warn',
+      '@typescript-eslint/no-explicit-any': 'error',
       'no-console': 'warn',
       'prettier/prettier': 'error',
       'import/no-cycle': ['error', { ignoreExternal: true }],
@@ -39,6 +46,7 @@ export default defineConfig([
           ignoreRestSiblings: true,
         },
       ],
+      ...SOFT_MIGRATION_RULES
     },
     plugins: {},
     settings: {
